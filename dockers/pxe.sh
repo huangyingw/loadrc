01b6cbafd144aba72f9cb2cd8f85c6d22b14201e
huangyingw@linux_programming <huangyingw@gmail.com>
2015-04-21 21:51:08 -0700

    n

--- dockers/pxe.sh
+++ dockers/pxe.sh
@@ -2,8 +2,10 @@
 docker run  \
   -v /root:/root \
   -v /media/volgrp/pxe/ubuntu/:/var/www/ubuntu/ \
-  -v /media/volgrp/pxe/tftpboot/:/var/lib/tftpboot/ \
+  -v /media/volgrp/ubuntu/dists/utopic-updates/main/installer-amd64/current/images/netboot/:/var/lib/tftpboot/ \
   -h pxe \
   -p 69:69/udp \
+  -p 80:80 \
   -ti huangyingw/pxe bash
+
 
