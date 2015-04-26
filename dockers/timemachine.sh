15f85df8cd3e01adb481bf9390bdb0132352bb0f
huangyingw@linux_programming <huangyingw@gmail.com>
2015-04-26 10:59:44 -0700

    n

--- dockers/timemachine.sh
+++ dockers/timemachine.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run -e PASSWORD=asdf -v /media/timemachine/:/backup -p 548:548 --net=host -d arve0/timemachine
+sudo docker run -e PASSWORD=asdf -v /media/volgrp/timemachine/:/backup -p 548:548 --net=host -d arve0/timemachine
