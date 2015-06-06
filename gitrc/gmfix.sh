58714448ffa81a8c56eaa32935f2bc75cc06a933
huangyingw@linux_programming <huangyingw@gmail.com>
2015-06-05 17:47:29 -0700

    n

--- gitrc/gmfix.sh
+++ gitrc/gmfix.sh
@@ -3,10 +3,10 @@ red='\033[0;31m'
 green='\033[0;32m'
 NC='\033[0;0m' # No Color
 fix_branch=`git branch |awk '/^\*/{print $2}'`
-previous_branch=`echo ${fix_branch} | sed -e "s|.fix$||"` 
+parent_branch=`echo ${fix_branch} | sed -e "s|.fix$||"` 
 ready_file="gfvd.ready"
 echo $fix_branch
-echo $previous_branch
+echo $parent_branch
 if [ ! -f ${ready_file} ];
 then
   echo -e "${red}gfvd must be run before gmfix first ... ${NC}"
@@ -17,7 +17,7 @@ then
   echo -e "${red}It must be run in fix branch ... ${NC}"
   exit 1
 fi 
-git checkout $previous_branch \
+git checkout $parent_branch \
   && git merge $fix_branch \
   && git branch -d $fix_branch \
   && for ss in `git remote -v |awk '/\(fetch\)$/{print $1}'`; do git push $ss :$fix_branch; done
