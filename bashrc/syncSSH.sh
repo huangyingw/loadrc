c4fe4f430e2ac3238e57cee4184363fdb7299ca7
huangyingw@linux_programming <huangyingw@gmail.com>
2015-07-08 09:55:47 -0700

    n

---
+++ bashrc/syncSSH.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+red='\033[0;31m'
+green='\033[0;32m'
+NC='\033[0;0m' # No Color
+if [ -z "$1" ];
+then
+  echo -e "${red}please provide the host name... ${NC}"
+  exit 1
+fi
+rsync -aH --force -e ssh ~/.ssh/ "$1":~/.ssh/ \
+  && scp -v ~/loadrc/bashrc/ssh_host_* root@"$1":/etc/ssh/
