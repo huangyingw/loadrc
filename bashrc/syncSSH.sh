430c44bd315f1f79f9bfc205714ab8d34599c2aa
huangyingw@linux_programming <huangyingw@gmail.com>
2015-07-08 18:11:49 -0700

    n

--- bashrc/syncSSH.sh
+++ bashrc/syncSSH.sh
@@ -7,5 +7,5 @@ then
   echo -e "${red}please provide the host name... ${NC}"
   exit 1
 fi
-rsync -aH --force -e ssh ~/.ssh/ "$1":~/.ssh/ \
+rsync -aH --force -e ssh ~/.ssh/ root@"$1":~/.ssh/ \
   && scp -v ~/loadrc/bashrc/ssh_host_* root@"$1":/etc/ssh/
