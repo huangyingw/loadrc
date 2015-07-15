be97780024607ed8bee725c2e59fc3e82cb437c1
huangyingw@linux_programming <huangyingw@gmail.com>
2015-07-15 12:07:46 -0700

    n

--- bashrc/envSync.sh
+++ bashrc/envSync.sh
@@ -10,9 +10,9 @@ fi
 ssh-keygen -R "$1" \
   && rsync -aH --force -e ssh ~/.ssh/ "$1":~/.ssh/ \
   && rsync -aH --force -e ssh ~/.git/ "$1":~/.git/ \
-  && rsync -aH --force -e ssh ~/loadrc/.git/ "$1":~/loadrc/.git/ \
+  && rsync -aH --force -e ssh ~/loadrc/ "$1":~/loadrc/ \
   && rsync -aH --force -e ssh ~/.ssh/ root@"$1":~/.ssh/ \
   && rsync -aH --force -e ssh ~/.git/ root@"$1":~/.git/ \
-  && rsync -aH --force -e ssh ~/loadrc/.git/ root@"$1":~/loadrc/.git/ \
+  && rsync -aH --force -e ssh ~/loadrc/ root@"$1":~/loadrc/ \
   && rsync -aHv --progress --delete --force -e ssh ~/loadrc/grub.d/ root@"$1":/etc/grub.d/ \
   && scp -v ~/loadrc/bashrc/ssh_host_* root@"$1":/etc/ssh/
