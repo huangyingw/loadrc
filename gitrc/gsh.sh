bc531c65ca4ea67f731d9a2a721fb7fcdefbcd8a
huangyingw@linux_programming <huangyingw@gmail.com>
2015-07-28 08:06:43 -0700

    n

---
+++ gitrc/gsh.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+red='\033[0;31m'
+green='\033[0;32m'
+NC='\033[0;0m' # No Color
+if [ -z "$1" ];
+then
+  echo -e "${red}please specify the file name ... ${NC}"
+  exit 0
+fi
+git log --follow "$1"|awk '/commit/{print $2}'|while read ss; do git difftool $ss^!; done
