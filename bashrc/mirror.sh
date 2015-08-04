c2dce62723163c6b9e4424ca95ec3ff6e9f2c88f
huangyingw@linux_programming <huangyingw@gmail.com>
2015-08-04 16:01:01 -0700

    n

--- bashrc/mirror.sh
+++ bashrc/mirror.sh
@@ -1,12 +1,21 @@
 #!/bin/bash 
+red='\033[0;31m'
+green='\033[0;32m'
+NC='\033[0;0m' # No Color
 SOURCE=$1
 TARGET=$2
 
+ready_file="tmirror.ready"
+if [ ! -f ${ready_file} ];
+then
+  echo -e "${red}tmirror must be run before mirror ... ${NC}"
+  exit 1
+fi
 if [ ! -d "${TARGET}" ];
 then   
-    mkdir -p "${TARGET}"
+  mkdir -p "${TARGET}"
 fi
 
 rsync -aH --delete-during --force \
-		"${SOURCE}" "${TARGET}"
-
+  "${SOURCE}" "${TARGET}" \
+  && rm ${ready_file}
