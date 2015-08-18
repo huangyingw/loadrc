798e7d401d403d2d9d287db6b865cdefe76c8f24
huangyingw@linux_programming <huangyingw@gmail.com>
2015-08-17 17:05:06 -0700

    n

--- bashrc/rsleep.sh
+++ bashrc/rsleep.sh
@@ -1,7 +1,8 @@
 #!/bin/bash 
 if [ -z "$1" ];
 then
-  /usr/bin/ssh timemachine "sudo pm-suspend"
+  /usr/bin/ssh timemachine "sudo pm-suspend" \
+    & ping timemachine
   exit 1
 fi
 ssh "$1" pm-suspend
