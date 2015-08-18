8162c6c1ffe8415b79a4f7e2cf920725437adda8
huangyingw@linux_programming <huangyingw@gmail.com>
2015-08-17 17:01:03 -0700

    n

--- bashrc/rsleep.sh
+++ bashrc/rsleep.sh
@@ -1,10 +1,7 @@
 #!/bin/bash 
-red='\033[0;31m'
-green='\033[0;32m'
-NC='\033[0;0m' # No Color
 if [ -z "$1" ];
 then
-  ssh ubuntu pm-suspend
+  /usr/bin/ssh timemachine "sudo pm-suspend"
   exit 1
 fi
 ssh "$1" pm-suspend
