8d9b137b4374875accb006a90ac0ad317ece51e6
huangyingw@linux_programming <huangyingw@gmail.com>
2015-08-21 17:31:47 -0700

    n

--- bashrc/fav.sh
+++ bashrc/fav.sh
@@ -3,8 +3,5 @@ if [ -f $HOME/.passwd ]; then
   FAV=`cat $HOME/.passwd |grep fav|awk '{print $3}'`
 fi
 rm ~/fav/*
-find "$FAV" -type f -mtime -30 -size +700M -exec ls -rt {} \+|tail -n 100|while read ss
-do 
-  ftemp=`basename "$ss"`
-  ln -s "$ss" ~/fav/"$ftemp"
-done
+find "$FAV" -type f -mtime -30 -size +700M -exec ls -t {} \+ > fav.findresult
+vi fav.findresult
