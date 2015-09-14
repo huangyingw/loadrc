3ab7245e9a337991e2ab6bdd76e03c63f4272c1f
huangyingw@linux_programming <huangyingw@gmail.com>
2015-09-13 17:30:42 -0700

    n

--- bashrc/jformat.sh
+++ bashrc/jformat.sh
@@ -2,6 +2,7 @@
 red='\033[0;31m'
 green='\033[0;32m'
 NC='\033[0;0m' # No Color
+ECLIPSEFORMAT=.eclipseformatrc
 if [ -n "$1" ];
 then
   folderForGit="$1"
@@ -11,7 +12,11 @@ fi
 if  ( git status "$folderForGit"|grep -q 'nothing to commit' )
 then
   LIST=( -name \*.java -o -name \*.vala )
-  find "$folderForGit" -type f \( "${LIST[@]}" \) -exec astyle --mode=java -nfxejoOU -y  --max-code-length=150 -s2 --style=allman -pcH {} \; -exec ~/bin/eclipse/eclipse -nosplash -application org.eclipse.jdt.core.JavaCodeFormatter -quiet -config ~/loadrc/vimrc/.vim/ftplugin/org.eclipse.jdt.core.prefs {} \;
+  find "$folderForGit" -type f \( "${LIST[@]}" \) -exec astyle --mode=java -nfxejoOU -y  --max-code-length=150 -s2 --style=allman -pcH {} \;
+  if [ -f "$ECLIPSEFORMAT" ]
+  then
+    find "$folderForGit" -type f \( "${LIST[@]}" \) -exec ~/bin/eclipse/eclipse -nosplash -application org.eclipse.jdt.core.JavaCodeFormatter -quiet -config ~/loadrc/vimrc/.vim/ftplugin/org.eclipse.jdt.core.prefs {} \;
+  fi
 else
   echo -e "${red}the git repository is unclean, please check it before continuing... ${NC}"
   exit 1
