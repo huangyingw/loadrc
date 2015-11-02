5896f0ed73cae874ccac7c791b68027962bb5e01
huangyingw@linux_programming <huangyingw@gmail.com>
2015-11-01 16:35:41 -0800

    n

--- kvmrc/vssc.sh
+++ kvmrc/vssc.sh
@@ -4,11 +4,5 @@ green='\033[0;32m'
 NC='\033[0;0m' # No Color
 DOMAIN=$1
 SNAPSHOT=$2
-if  ( virsh list|grep -q "$DOMAIN" )
-then
-  echo -e "${red} ${DOMAIN} is running, please shut it down before continuing... ${NC}"
-  exit 1
-fi 
 virsh snapshot-create-as "$DOMAIN" "$SNAPSHOT" \
-  && virsh start "$DOMAIN" \
   && virsh snapshot-list --tree "$DOMAIN"
