af6e5cf7de4f6f1d7ea808dc7c13733371c43ba8
huangyingw@linux_programming <huangyingw@gmail.com>
2015-11-01 17:24:19 -0800

    n

---
+++ bashrc/tnetsync.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+red='\033[0;31m'
+green='\033[0;32m'
+NC='\033[0;0m' # No Color
+if [ -z "$1" ];
+then
+  echo -e "${red}please specify the source server ... ${NC}"
+  exit 0
+fi
+SOURCE="$1"
+NETSYNC="$HOME/loadrc/bashrc/netsync_ex"
+exclude_params=();
+while read suf
+do
+  exclude_params+=( "--exclude=$suf" )
+done < "$NETSYNC"
+rsync -e ssh -aHinv --delete-during --force "${exclude_params[@]}" "${SOURCE}":/ / > gs.findresult
+vi gs.findresult
