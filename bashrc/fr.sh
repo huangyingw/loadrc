953ce073202affc4da2104088f74ff55e2364396
huangyingw@linux_programming <huangyingw@gmail.com>
2015-11-06 10:21:06 -0800

    n

--- bashrc/fr.sh
+++ bashrc/fr.sh
@@ -2,11 +2,11 @@
 red='\033[0;31m'
 green='\033[0;32m'
 NC='\033[0;0m' # No Color
-#if ! (git status|grep -q 'nothing to commit')
-#then
-#  echo -e "${red}the git repository is unclean, please check it before continuing... ${NC}"
-#  exit 1
-#fi
+if ! (git status|grep -q 'nothing to commit')
+then
+  echo -e "${red}the git repository is unclean, please check it before continuing... ${NC}"
+  exit 1
+fi
 FIND=$(echo $1 | sed 's/\//\\\//g')
 REPLACE=$(echo $2 | sed 's/\//\\\//g')
 cscope_db_file="/export/home1/username/cscope_db/""${PWD##*/}"
