4e35fe753e5b5a989d1b018c329c85893dcd11a0
huangyingw@linux_programming <huangyingw@gmail.com>
2015-11-24 13:27:24 -0800

    n

--- bashrc/wol.sh
+++ bashrc/wol.sh
@@ -2,9 +2,9 @@
 if [ -z "$1" ]; then
   wakeonlan 00:01:c0:12:94:05 #mini
   ping mini
-elif [ "$1" == "timemachine" ]; then
-  wakeonlan 00:01:c0:12:92:a2 #timemachine
-  ping timemachine
+elif [ "$1" == "mldonkey" ]; then
+  wakeonlan 00:01:c0:12:92:a2 #mldonkey
+  ping mldonkey
 elif [ "$1" == "thinkpad" ]; then
   wakeonlan 00:21:cc:c0:53:a9 #thinkpad
   ping thinkpad
