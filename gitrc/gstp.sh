33ecc23cef3150ee6e638c613ec10f2fb1955114
Ying Huang <yinghuang@tutor-group.com>
2016-01-15 12:16:18 -0800

    n

--- gitrc/gstp.sh
+++ gitrc/gstp.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 fix_branch=`git branch |awk '/^\*/{print $2}'`
-if [ -n "$1" ];
-then
-  git stash pop stash@{"$1"}
-  exit 0
-fi
 if ! (git status|grep -q 'nothing to commit')
 then
   echo -e "${red}the git repository is unclean, please check it before continuing... ${NC}"
@@ -19,4 +14,4 @@ top_branch=`git stash list|awk 'NR==v1 {print $4}' v1=$(($1+1)) |sed 's/://g'`
 echo $top_branch
 git branch |awk '/^\*/{print $2}'
 ~/loadrc/gitrc/gco.sh $top_branch
-git stash pop
+git stash pop stash@{"$1"}
