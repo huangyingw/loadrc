f849ba3615ef27702cc5010573eebc4999d769eb
Ying Huang <yinghuang@tutor-group.com>
2016-01-16 17:00:52 -0800

    n

---
+++ bashrc/mbak.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+TARGET=root@mini:/media/volgrp/media/`hostname`/
+MBAKEX="$HOME/loadrc/bashrc/mbak_ex"
+MBAKIN="$HOME/loadrc/bashrc/mbak_in"
+exclude_params=();
+while read suf
+do
+  exclude_params+=( "--exclude=$suf" )
+done < "$MBAKEX"
+include_params=();
+while read suf
+do
+  include_params+=( "--include=$suf" )
+done < "$MBAKIN"
+sudo rsync -aH --force --delete-during "${exclude_params[@]}" "${include_params[@]}" / "${TARGET}"
