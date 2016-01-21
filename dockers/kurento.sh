53ace8b1c603302ed0cb80d78c7b98bb7ce7e99e
Ying Huang <yinghuang@tutor-group.com>
2016-01-20 21:59:33 -0800

    n

--- dockers/kurento.sh
+++ dockers/kurento.sh
@@ -6,4 +6,4 @@ docker run  \
   -h kurento \
   -p 8888:8888 \
   -p 8089:8089 \
-  -d java /bin/sh -c "java -Dkms.ws.uri=ws://52.33.62.122:8888/kurento -Dapp.server.url=http://52.3475.3:8089/ -Dserver.port=8089 -jar /root/service/lib/tutorgroup-group-call.jar &>/dev/null &"
+  -ti java
