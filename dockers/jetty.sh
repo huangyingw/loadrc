7fd90660f49fb15af54adc13c5da401da91c5042
Ying Huang <yinghuang@tutor-group.com>
2016-01-26 16:57:54 -0800

    n

--- dockers/jetty.sh
+++ dockers/jetty.sh
@@ -4,6 +4,7 @@ docker run  \
   -v /Users/huangyingw/myproject/git/work/TutorGroup/jetty/jetty9/demo-base/:/var/lib/jetty/ \
   --env-file $SCRIPT_DIR/env.list \
   -h jetty \
+  --name jetty \
   -p 80:8080 \
   -p 443:8443 \
   -d jetty
