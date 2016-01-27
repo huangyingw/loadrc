cfa484979ce3f07f4678a72a03f086b21327e00b
Ying Huang <yinghuang@tutor-group.com>
2016-01-27 11:29:06 -0800

    n

--- gitrc/gcl.sh
+++ gitrc/gcl.sh
@@ -4,15 +4,13 @@ if [ -z "$1" ];
 then
 	echo "Please provide parameter"
 else
-	x=`echo $1 | sed -e "s|/media/smb|$HOME|"`
+	x=`echo $1 | sed -e "s|Dropbox/||"`
 	echo ${x}
 fi
 
-
-if [ -d ${x} ];
+if [ -d "$x" ];
 then
 	echo local repository exist!!!
 else
-	#echo "git clone -v "$1" ${x}"
-	git clone -v "$1" ${x}
+	git clone -v "$1" "$x"
 fi
