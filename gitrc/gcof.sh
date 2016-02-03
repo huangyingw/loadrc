2aa749f9d8e0e8cccd64c438137493007d0bd385
Ying Huang <yinghuang@tutor-group.com>
2016-02-03 12:17:07 -0800

    n

--- gitrc/gcof.sh
+++ gitrc/gcof.sh
@@ -4,10 +4,8 @@ commit=`git rev-list --branches -n 1 HEAD -- "$1"`
 if [ -z "$1" ];
 then
   git st|awk '/modified:/{print $2}'|while read ss \
-    ; do cp -fv "$ss" "$ss".bak \
-    && git checkout -- "$ss" \
+    ; do git show HEAD^:"$ss" > "$ss".bak \
     ; done
 else
-  cp -fv "$1" "$1".bak \
-    && git checkout -- "$1"
+  git show HEAD^:"$1" > "$1".bak
 fi
