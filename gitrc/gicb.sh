2fac9b10d22f527f8833fbaac0a03ffa4a1edfa9
Ying Huang <yinghuang@tutor-group.com>
2016-02-18 10:09:04 -0800

    n

--- gitrc/gicb.sh
+++ gitrc/gicb.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-git init
-git add .
-~/loadrc/gitrc/gci.sh
-$HOME/loadrc/gitrc/gclb.sh
+git init \
+  && $HOME/loadrc/gitrc/gcob.sh dev \
+  && touch gci.default \
+  && touch gps.default \
+  && touch gme.default \
+  && $HOME/loadrc/bashrc/cscope.sh \
+  && git add . \
+  && $HOME/loadrc/gitrc/gci.sh
