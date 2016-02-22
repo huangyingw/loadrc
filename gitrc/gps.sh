95e15af0be98d561fb6ec1c9f225f63663408785
Ying Huang <yinghuang@tutor-group.com>
2016-02-22 11:19:45 -0800

    n

--- gitrc/gps.sh
+++ gitrc/gps.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-GPSDEFAULT=gps.default
-if [ ! -f "$GPSDEFAULT" ];
-then
-  echo -e "${red}By default git push is disabled, please manually do it ... ${NC}"
-  exit 0
-fi
 for ss in `git remote -v |awk '/\(fetch\)$/{print $1}'`; do git push -v $ss && git push -v --tag $ss; done
