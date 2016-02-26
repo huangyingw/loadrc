54c4bfdde8f36d96ac8c3411259f90f3faf95502
Ying Huang <yinghuang@tutor-group.com>
2016-02-26 10:57:24 -0800

    n

--- sqlrc/mysqlback.sh
+++ sqlrc/mysqlback.sh
@@ -9,4 +9,4 @@ user=`cat ${loginFile} |awk -F'=' '/user/{print $2}'`
 password=`cat ${loginFile} |awk -F'=' '/password/{print $2}'`
 host=`cat ${loginFile} |awk -F'=' '/host/{print $2}'`
 dbinstance=`cat ${loginFile} |awk -F'=' '/dbinstance/{print $2}'`
-mysqldump -u"$user" -p"$password" -h${host} ${dbinstance} > ${file}
+mysqldump -R -v -u"$user" -p"$password" -h${host} ${dbinstance} > ${file}
