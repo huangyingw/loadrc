aeb642d417f01116386ac91ae0ac6cea682d28ac
Ying Huang <yinghuang@tutor-group.com>
2016-03-07 21:35:38 -0800

    n

--- gitrc/gmup.sh
+++ gitrc/gmup.sh
@@ -3,11 +3,6 @@ current_branch=`git branch |awk '/^\*/{print $2}'`
 up_branch=`echo ${current_branch} | sed 's/\(\.[^.]*\)$//'`
 echo $current_branch
 echo $up_branch
-#if  ( echo $current_branch|grep -q 'fix$' )
-#then
-#  echo -e "${red}Please use gmfix to merge fix branch ... ${NC}"
-#  exit 1
-#fi 
 if [ $current_branch == $up_branch ];
 then
   echo -e "${red}It is already top branch ... ${NC}"
