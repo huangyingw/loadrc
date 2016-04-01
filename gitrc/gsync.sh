da6a666ed74abb731641dc1cd15d31f79674e0b4
Ying Huang <yinghuang@tutor-group.com>
2016-04-01 11:21:37 -0700

    n

--- gitrc/gsync.sh
+++ gitrc/gsync.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-~/loadrc/gitrc/gmet.sh "$1" \
- && ~/loadrc/gitrc/gme.sh "$1"
+if [ -z "$1" ];
+then
+  echo -e "${red}Must provide the target branch ... ${NC}"
+  exit 1
+fi
+CURRENT_BRANCH="`git branch |awk '/^\*/{print $2}'`"
+TARGET_BRANCH="$1"
+git checkout "$TARGET_BRANCH" \
+  && ~/loadrc/gitrc/gpl.sh \
+  && git checkout "$CURRENT_BRANCH" \
+  && ~/loadrc/gitrc/gme.sh "$TARGET_BRANCH"
