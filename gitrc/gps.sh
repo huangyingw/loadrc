6d762b9a759ba6de5c149c7e60f3bc81a03266c3
Ying Huang <yinghuang@tutor-group.com>
2016-04-16 17:03:17 -0700

    n

--- gitrc/gps.sh
+++ gitrc/gps.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 ~/loadrc/gitrc/gpl.sh
+if [ -f dropbox.only ];
+then
+  git push -v dropbox && git push -v --tag dropbox
+  exit 0
+fi
 for ss in `git remote -v |awk '/\(fetch\)$/{print $1}'`; do git push -v $ss && git push -v --tag $ss; done
