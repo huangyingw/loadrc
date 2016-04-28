5ec2a689d692c36bff0b3c92ea286624a5476551
Ying Huang <yinghuang@itutorgroup.com>
2016-04-28 11:15:36 -0700

    n

--- gitrc/gicb.sh
+++ gitrc/gicb.sh
@@ -3,6 +3,7 @@ git init \
   && $HOME/loadrc/gitrc/gcob.sh dev \
   && echo master > branch.default \
   && touch gci.default \
+  && touch dropbox.only \
   && touch gme.default \
   && $HOME/loadrc/bashrc/cscope.sh \
   && git add . \
--- gitrc/gpl.sh
+++ gitrc/gpl.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 branch=`git branch |awk '/\*/{print $2}'`
+if [ -f dropbox.only ];
+then
+  git pull dropbox $branch
+  exit 0
+fi
 for ss in `git remote -v |awk '/\(fetch\)$/{print $1}'`
 do 
   git pull $ss $branch
