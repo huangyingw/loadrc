f4ce1fea892d5db615b1e96efdd6a73a554c3b4b
Ying Huang <yinghuang@itutorgroup.com>
2016-04-29 10:27:03 -0700

    n

--- bashrc/updatecsc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-PROPERTIES=~/loadrc/bashrc/`hostname`.csc.properties
-while read ss
-do
-  ~/loadrc/bashrc/cscope.sh "$ss"
-done < "$PROPERTIES"
-read -p "press any key to continue...." update
