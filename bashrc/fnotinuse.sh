b02302b81337f8a07ce887dc3c19ac6d85cf390a
Ying Huang <yinghuang@itutorgroup.com>
2016-04-29 11:39:03 -0700

    n

--- bashrc/fnotinuse.sh
+++ bashrc/fnotinuse.sh
@@ -18,12 +18,13 @@
 #===============================================================================
 
 set -o nounset                              # Treat unset variables as an error
+find_result=fnotinuse.findresult
 cscope_db_file="/export/home1/username/cscope_db/""${PWD##*/}"
 while read fullfile
 do
   filename=$(basename "$fullfile")
   filename="${filename%.*}"
   if [ $(xargs fgrep -wnH "$filename" < "$cscope_db_file" |wc -l) -eq 0 ]; then
-    echo "$fullfile"
+    echo "$fullfile" >> "$find_result"
   fi
 done < '/export/home1/username/cscope_db/'`pwd |sed -e "s/^.*\///g"` 
