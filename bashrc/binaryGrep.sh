6b27aba0c22a12b9033ffeecd8635e99dcad7fd8
Ying Huang <yinghuang@itutorgroup.com>
2016-05-03 16:31:54 -0700

    n

--- bashrc/binaryGrep.sh
+++ bashrc/binaryGrep.sh
@@ -19,7 +19,24 @@
 
 set -o nounset                              # Treat unset variables as an error
 
-grep -rnH --include *.tar \
+find_result="`echo "$1".binaryGrep.findresult |sed  -e "s/\//\_/g;s/\ /\_/g"`"
+if [ -f "$find_result" ]; then
+  read -p "the search is already done, if you want to update, press u --> " update
+  case $update in
+    u)
+      ;;
+    "")
+      exit 1
+      ;;
+    ?)
+      exit 1
+      ;;
+  esac
+fi
+grep -rnH \
   --include *.jar \
+  --include *.class \
+  --include *.tar \
+  --include *.war \
   "$1" \
-  "$2"
+  . > "$find_result" 
