ac81bf6cc44c62402499b69d69145242867cf690
Ying Huang <yinghuang@itutorgroup.com>
2016-05-04 11:53:13 -0700

    n

--- gitrc/gsync.sh
+++ gitrc/gsync.sh
@@ -6,6 +6,11 @@ then
   exit 1
 fi
 TARGET_BRANCH=$(cat "$TARGET_BRANCH_DEFAULT")
+if [ -z "$TARGET_BRANCH" ]
+then
+  echo -e "${red}Please put the default sync branch in branch.default file... ${NC}"
+  exit 1
+fi
 echo "$TARGET_BRANCH"
 CURRENT_BRANCH="`git branch |awk '/^\*/{print $2}'`"
 git checkout "$TARGET_BRANCH" \
