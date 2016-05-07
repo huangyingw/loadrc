ad99e870146e68dca3d82c60d79aa6308f7edf34
Ying Huang <yinghuang@tutor-group.com>
2016-05-07 12:59:34 -0700

    n

--- kvmrc/vmount.sh
+++ kvmrc/vmount.sh
@@ -17,11 +17,13 @@
 #      REVISION:  ---
 #===============================================================================
 
-if [ -z "$1" ] || [ ! -f "$1" ];
-then
-  echo -e "${red}Please provide the full path to qcow2 file... ${NC}"
-  exit 1
-fi
-qcow2File="$1"
-modprobe nbd max_part=8
-qemu-nbd --connect=/dev/nbd0 "$qcow2File"
+#if [ -z "$1" ] || [ ! -f "$1" ];
+#then
+#  echo -e "${red}Please provide the full path to qcow2 file... ${NC}"
+#  exit 1
+#fi
+nbdDevice=$(~/loadrc/bashrc/find-free-nbd-device.sh)
+echo "$nbdDevice" 
+#qcow2File="$1"
+#modprobe nbd max_part=8
+#qemu-nbd --connect=/dev/nbd0 "$qcow2File"
