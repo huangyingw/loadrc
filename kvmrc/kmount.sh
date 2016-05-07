0e3109ac353ff34ff430a4dc3143f1d28ddd6f32
Ying Huang <yinghuang@tutor-group.com>
2016-05-07 12:28:14 -0700

    n

---
+++ kvmrc/kmount.sh
@@ -0,0 +1,27 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: qmount.sh
+# 
+#         USAGE: ./qmount.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 05/07/2016 12:01
+#      REVISION:  ---
+#===============================================================================
+
+if [ -z "$1" ] || [ ! -f "$1" ];
+then
+  echo -e "${red}Please provide the full path to qcow2 file... ${NC}"
+  exit 1
+fi
+qcow2File="$1"
+modprobe nbd max_part=8
+qemu-nbd --connect=/dev/nbd0 "$qcow2File"
