bb5450e150fbddd2382857f181070ac5f420ff60
Ying Huang <yinghuang@itutorgroup.com>
2016-05-10 15:45:27 -0700

    n

--- bashrc/smount.sh
+++ bashrc/smount.sh
@@ -4,8 +4,16 @@ then
   echo -e "${red}please provide the host name... ${NC}"
   exit 1
 fi
-diskutil umount force ~/samba \
-  ; sshfs root@"$1":/ ~/samba \
-  && df \
+diskutil umount force ~/samba
+SERVER="$1"
+KEY=$(~/loadrc/keys/getKey.sh "$SERVER")
+
+if [ $KEY == "/Users/huangyingw/loadrc/keys/.pem" ];
+then
+  sshfs root@"$SERVER":/ ~/samba
+else
+  sshfs -o "IdentityFile=""$KEY" ubuntu@"$SERVER":/ ~/samba 
+fi
+df \
   && cd ~/samba \
   && echo -e "${green}$(pwd) ${NC}" "in"  "${green}$1 ${NC}"
