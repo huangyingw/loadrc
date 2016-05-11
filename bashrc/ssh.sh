ce391eda73e7589567f84825ef03eaf089e9274a
Ying Huang <yinghuang@itutorgroup.com>
2016-05-11 16:56:27 -0700

    n

--- bashrc/ssh.sh
+++ bashrc/ssh.sh
@@ -5,14 +5,14 @@ then
   autossh -X "$1" -t -- ${tmuxAction}
 else
   SERVER="$1"
-  if [[ $SERVER != *"@"* ]]
-  then
-    SERVER=root@"$SERVER"
-  fi
   KEY=$(~/loadrc/keys/getKey.sh "$SERVER")
 
   if [ $KEY == "/Users/huangyingw/loadrc/keys/.pem" ];
   then
+    if [[ $SERVER != *"@"* ]]
+    then
+      SERVER=root@"$SERVER"
+    fi
     ssh -X "$SERVER" -t -- ${tmuxAction}
   else
     ssh -X -i "$KEY" ubuntu@"$SERVER" -t -- ${tmuxAction}
