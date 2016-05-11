cc8a0b7d25eab26b3cd47975f14db46c16282b8e
Ying Huang <yinghuang@itutorgroup.com>
2016-05-11 16:05:48 -0700

    n

--- bashrc/copy.sh
+++ bashrc/copy.sh
@@ -2,10 +2,31 @@
 SOURCE=$1
 TARGET=$2
 
-if [ ! -d "${TARGET}" ];
-then   
-  mkdir -p "${TARGET}"
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+. "$SCRIPTPATH"/getConDetails.sh "mini:/media/volgrp"
+
+echo $user
+echo $host
+echo $folder
+
+. "$SCRIPTPATH"/getConDetails.sh "huangyingw@mini:/media/volgrp"
+
+echo
+echo $user
+echo $host
+echo $folder
+
+if [[ $SOURCE == *":"* ]]
+then
+  DOMAIN=root@"$SOURCE"
+  DOMAIN=root@"$SOURCE"
 fi
 
-rsync -aH --force \
-  "${SOURCE}/" "${TARGET}/" \
+#if [ ! -d "${TARGET}" ];
+#then   
+#  mkdir -p "${TARGET}"
+#fi
+#
+#rsync -aH --force \
+  #  "${SOURCE}/" "${TARGET}/" \
