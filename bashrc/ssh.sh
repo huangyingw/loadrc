9e299475f3cad824065e4d20e0b11b5e2937fed4
Ying Huang <yinghuang@itutorgroup.com>
2016-05-12 13:03:02 -0700

    n

--- bashrc/ssh.sh
+++ bashrc/ssh.sh
@@ -6,14 +6,11 @@ then
 else
   SERVER="$1"
   . ~/loadrc/keys/getConDetails.sh "$SERVER"
-  echo "$host"
-  echo "$key"
-  echo "$user"
 
   if [ $key == "/Users/huangyingw/loadrc/keys/.pem" ];
   then
-    echo ssh -X "$user"@"$host" -t -- ${tmuxAction}
+    ssh -X "$user"@"$host" -t -- ${tmuxAction}
   else
-    echo ssh -X -i "$key" "$user"@"$host" -t -- ${tmuxAction}
+    ssh -X -i "$key" "$user"@"$host" -t -- ${tmuxAction}
   fi
 fi
