e20639a58eacd43ef42efba8602b4913f585d37f
Ying Huang <yinghuang@tutor-group.com>
2016-05-14 15:34:50 -0700

    n

--- bashrc/copy.sh
+++ bashrc/copy.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-#SOURCE=$1
-#TARGET=$2
+SOURCE=$1
+TARGET=$2
 
-SOURCE=$(~/loadrc/bashrc/wrapConStr.sh "mini:/media/volgrp/timemachine/")
-TARGET="/media/volgrp/timemachine/" 
+if [ ! -d "${TARGET}" ];
+then   
+  mkdir -p "${TARGET}"
+fi
 
-#if [ ! -d "${TARGET}" ];
-#then   
-#  mkdir -p "${TARGET}"
-#fi
-
-echo rsync -aH --force \
+rsync -aH --force \
   "${SOURCE}/" "${TARGET}/" \
