650cd712575f7036f6c251268f3d21c5af7aff77
Ying Huang <yinghuang@tutor-group.com>
2016-05-14 13:48:26 -0700

    n

--- gitrc/gclb.sh
+++ gitrc/gclb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 x=`pwd`
 x=`echo ${x} | sed -e "s|/.*/myproject|$HOME/BareReps/myproject|"`
+x=`echo ${x} | sed -e "s|/.*/loadrc|$HOME/BareReps/loadrc|"`
 echo ${x}
 
 if [ ! -d ${x} ];
