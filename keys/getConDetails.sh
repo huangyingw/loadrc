8fb8a44b182915d9d8e9a05c52034c7f3b21a8c4
Ying Huang <yinghuang@tutor-group.com>
2016-05-14 14:35:12 -0700

    n

--- keys/getConDetails.sh
+++ keys/getConDetails.sh
@@ -1,7 +1,16 @@
 #!/bin/bash - 
+user=
+host=
+key=
+folder=
+
 SOURCE="$1" 
 host=${SOURCE%:*}  
 folder=${SOURCE##*:}
+if [ "$folder" == "$host" ];
+then
+  folder=
+fi
 
 mapFILE="$HOME/loadrc/keys/keymap.csv"
 KEYFOLDER="$HOME/loadrc/keys/"
@@ -12,3 +21,8 @@ then
   chmod 400 "$key"
 fi
 user="$(cat ${mapFILE} |awk -F',' '/'"$host"'/{print $3}')"
+echo "origin --> ""$1"
+echo "user --> ""$user"
+echo "host --> ""$host"
+echo "key --> ""$key"
+echo "folder --> ""$folder"
