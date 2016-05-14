9ae604e86c8a5b68fcb28ab6c56584f5525ca66f
Ying Huang <yinghuang@tutor-group.com>
2016-05-14 14:05:52 -0700

    n

--- bashrc/getConDetails.sh
+++ bashrc/getConDetails.sh
@@ -3,5 +3,10 @@ host="$1"
 
 mapFILE="$HOME/loadrc/keys/keymap.csv"
 KEYFOLDER="$HOME/loadrc/keys/"
-key="$KEYFOLDER""$(cat ${mapFILE} |awk -F',' '/'"$host"'/{print $2}')"".pem"
+key="$(cat ${mapFILE} |awk -F',' '/'"$host"'/{print $2}')"
+if [ -n "$key" ];
+then
+  key="$KEYFOLDER""$key"".pem"
+  chmod 400 "$key"
+fi
 user="$(cat ${mapFILE} |awk -F',' '/'"$host"'/{print $3}')"
