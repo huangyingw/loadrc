3b54051797babed3404e0a5d1e65c5f162ca649f
Ying Huang <yinghuang@itutorgroup.com>
2016-05-19 18:02:38 -0700

    n

---
+++ bashrc/searchIP.sh
@@ -0,0 +1,22 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: searchIP.sh
+# 
+#         USAGE: ./searchIP.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 05/19/2016 17:57
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+xargs grep -oE -- "([0-9]{1,3}\.){3}[0-9]{1,3}" /dev/null < /export/home1/username/cscope_db/H2HSetup
