aae1fda76283cada4cb008f2f0444f8f61f5804e
Ying Huang <yinghuang@itutorgroup.com>
2016-05-22 19:36:28 -0700

    n

--- bashrc/selectCopy.sh
+++ bashrc/selectCopy.sh
@@ -1,1 +1,7 @@
 #!/bin/bash - 
+SOURCE="$1"
+TARGET="$2"
+INCLUDEFROM="includeFrom"
+rsync -aHinv --delete-during --force \
+  --include="*/" --include-from="$INCLUDEFROM"  --exclude="*" \
+  "${SOURCE}" "${TARGET}" | tee selectCopy.findresult
