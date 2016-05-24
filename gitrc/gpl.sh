bca29826b81726cb3d530ff22de59f084992ca73
Ying Huang <yinghuang@itutorgroup.com>
2016-05-24 10:53:35 -0700

    n

--- gitrc/gpl.sh
+++ gitrc/gpl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-branch=`git branch |awk '/\*/{print $2}'`
+. ~/loadrc/gitrc/getCurrentBranch.sh
 for ss in `git remote -v |awk '/\(fetch\)$/{print $1}'`
 do 
-  git pull $ss $branch
+  git pull $ss $currentBranch
 done
