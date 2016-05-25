023389daa44a0a1f9e1a29031b4ccc47e956bbd8
Ying Huang <yinghuang@itutorgroup.com>
2016-05-24 17:38:26 -0700

    n

--- gitrc/gps.sh
+++ gitrc/gps.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-#~/loadrc/gitrc/gpl.sh
-#for ss in `git remote -v |awk '/\(fetch\)$/{print $1}'`; do git push -v $ss && git push -v --tag $ss; done
+~/loadrc/gitrc/gpl.sh
 . ~/loadrc/gitrc/getCurrentBranch.sh
 remotes=($(git config --get-all branch."$currentBranch".remote))
-echo ${remotes[@]}
+if [ ${#remotes[@]} -gt 0 ]
+then
+  for remote in "${remotes[@]}"
+  do
+    git push -v $remote && git push -v --tag $remote
+  done
+fi 
+git push -v dropbox && git push -v --tag dropbox
