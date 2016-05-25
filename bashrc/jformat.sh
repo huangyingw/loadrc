5bd6dd8266757bf0e27ec5869bd0018ea9aa081b
Ying Huang <yinghuang@itutorgroup.com>
2016-05-25 16:30:01 -0700

    n

--- bashrc/jformat.sh
+++ bashrc/jformat.sh
@@ -1,16 +1,15 @@
 #! /bin/bash
 ECLIPSEFORMAT=.eclipseformatrc
 folderForGit=$(realpath .)
-if  ( git status |grep -q 'nothing to commit' )
+~/loadrc/gitrc/checkGitStatus.sh
+if [ $? -ne 0 ]
 then
-  LIST=( -name \*.java -o -name \*.vala )
-  find "$folderForGit" -type f \( "${LIST[@]}" \) -exec astyle --mode=java -nfxejoOU -y  --max-code-length=150 -s2 --style=allman -pcH {} \;
-  if [ -f "$ECLIPSEFORMAT" ]
-  then
-    find "$folderForGit" -type f \( "${LIST[@]}" \) -exec ~/bin/eclipse/eclipse -nosplash -application org.eclipse.jdt.core.JavaCodeFormatter -quiet -config ~/loadrc/vimrc/.vim/ftplugin/org.eclipse.jdt.core.prefs {} \;
-  fi
-else
-  echo -e "${red}the git repository is unclean, please check it before continuing... ${NC}"
   exit 1
+fi
+LIST=( -name \*.java -o -name \*.vala )
+find "$folderForGit" -type f \( "${LIST[@]}" \) -exec astyle --mode=java -nfxejoOU -y  --max-code-length=150 -s2 --style=allman -pcH {} \;
+if [ -f "$ECLIPSEFORMAT" ]
+then
+  find "$folderForGit" -type f \( "${LIST[@]}" \) -exec ~/bin/eclipse/eclipse -nosplash -application org.eclipse.jdt.core.JavaCodeFormatter -quiet -config ~/loadrc/vimrc/.vim/ftplugin/org.eclipse.jdt.core.prefs {} \;
 fi
 ~/loadrc/gitrc/gci.sh
