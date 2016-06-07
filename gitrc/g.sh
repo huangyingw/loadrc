7193edf75aafbfe59df717299b9a058e417278d8
Ying Huang <yinghuang@itutorgroup.com>
2016-06-06 17:05:35 -0700

    n

--- gitrc/g.sh
+++ gitrc/g.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 GCIDEFAULT=gci.default
 commit_message='n'
-if [ -z "$1" ];
+if [ -z "$1" ]
 then
-  if [ ! -f "$GCIDEFAULT" ] && [ -n "$(git status --porcelain)" ]
+  if [ ! -f .git/MERGE_MSG ] && [ ! -f "$GCIDEFAULT" ] && [ -n "$(git status --porcelain)" ]
   then
     echo -e "${red}Must provide the commit message ... ${NC}"
     exit 1
   fi
 else
   commit_message="$1"
+fi
+if [ -f .git/MERGE_MSG ]
+then
+  commit_message=$(cat .git/MERGE_MSG |awk 'NR==1')
 fi
 git commit  --no-verify -m "$commit_message"
 ~/loadrc/gitrc/gclb.sh
