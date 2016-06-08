0a905470752bd87b4492560844e04fe4dd44d937
Ying Huang <yinghuang@itutorgroup.com>
2016-06-08 15:38:15 -0700

    n

--- bashrc/selectMirror.sh
+++ bashrc/selectMirror.sh
@@ -1,1 +1,10 @@
 #!/bin/bash - 
+
+if [ "$#" -lt 2 ]
+then
+  echo -e "${red}Please specify enough parameter ... ${NC}"
+  exit 1
+fi
+SOURCE="$1"
+TARGET="$2"
+echo rsync -aHv --exclude-from=excludeFile "$SOURCE"/  "$TARGET"/ 
