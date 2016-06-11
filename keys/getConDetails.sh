7532c28947614c4f6853b16cd927d29cd419c46f
Ying Huang <yinghuang@tutor-group.com>
2016-06-11 16:25:07 -0700

    n

--- keys/getConDetails.sh
+++ keys/getConDetails.sh
@@ -3,7 +3,11 @@ host="$1"
 
 mapFILE="$HOME/loadrc/keys/keymap.csv"
 KEYFOLDER="$HOME/loadrc/keys/"
-host="$(cat ${mapFILE} |awk -F',' '/'"$host"'/{print $1}')"
+host="$(cat ${mapFILE} | awk -F',' -v pat="^$host" '$0 ~ pat { print $1}')"
+
+
+
+
 
 if [ -z "$host" ];
 then
@@ -11,10 +15,10 @@ then
   exit 1
 fi
 
-key="$(cat ${mapFILE} |awk -F',' '/'"$host"'/{print $2}')"
+key="$(cat ${mapFILE} | awk -F',' -v pat="^$host" '$0 ~ pat { print $2}')"
 if [ -n "$key" ];
 then
   key="$KEYFOLDER""$key"".pem"
   chmod 400 "$key"
 fi
-user="$(cat ${mapFILE} |awk -F',' '/'"$host"'/{print $3}')"
+user="$(cat ${mapFILE} | awk -F',' -v pat="^$host" '$0 ~ pat { print $3}')"
