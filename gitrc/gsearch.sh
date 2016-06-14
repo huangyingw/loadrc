2ec68210581d5b20ab3de94876ec6d9636d48688
Ying Huang <yinghuang@itutorgroup.com>
2016-06-14 14:26:12 -0700

    n

--- gitrc/gsearch.sh
+++ gitrc/gsearch.sh
@@ -3,4 +3,5 @@
 #git rev-list --all | xargs git grep -wnH "$1" > "$1".gsearch.findresult
 #vi "$1".gsearch.findresult
 find_result="`echo "$1".gsearch.findresult |sed  -e "s/\//\_/g;s/\ /\_/g"`"
-git log -S "$1" --source --all | tee "$find_result"
+#git log -S "$1" --source --all | tee "$find_result"
+git grep "$1" $(git rev-list --all) &> "$find_result" 
