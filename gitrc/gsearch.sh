3806ece16c483bb5aa73c68d8508970f87b2e08a
Ying Huang <yinghuang@tutor-group.com>
2016-06-14 22:48:24 -0700

    n

--- gitrc/gsearch.sh
+++ gitrc/gsearch.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-#don't know what it is used for.
-#git rev-list --all | xargs git grep -wnH "$1" > "$1".gsearch.findresult
-#vi "$1".gsearch.findresult
 find_result="`echo "$1".gsearch.findresult |sed  -e "s/\//\_/g;s/\ /\_/g"`"
-#git log -S "$1" --source --all | tee "$find_result"
 git grep "$1" $(git rev-list --all) &> "$find_result" 
