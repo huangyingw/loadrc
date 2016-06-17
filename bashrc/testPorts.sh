1fca33665e8d45ae8787f0dbe242e92dcefc064a
Ying Huang <yinghuang@itutorgroup.com>
2016-06-17 10:15:49 -0700

    n

--- bashrc/testPorts.sh
+++ bashrc/testPorts.sh
@@ -1,7 +1,8 @@
 #!/bin/bash - 
 host="$1"
-portList="$2"
+portList="$HOME/Dropbox/loadrc/bashrc/portList"
 while read port
 do
-  nc -zv "$host" "$port"
+  nc -zv "$host" "$port" | tee testPorts.findresult
+  nc -zuv "$host" "$port" | tee testPorts.findresult 
 done < "$portList"
