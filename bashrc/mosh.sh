75b16efe0d87998f2deba0cd1dc8ad5fbcf566b0
Ying Huang <yinghuang@itutorgroup.com>
2016-06-20 13:12:27 -0700

    n

--- bashrc/mosh.sh
+++ bashrc/mosh.sh
@@ -1,31 +1,3 @@
 #!/bin/bash - 
-#===============================================================================
-#
-#          FILE: mosh.sh
-# 
-#         USAGE: ./mosh.sh 
-# 
-#   DESCRIPTION: 
-# 
-#       OPTIONS: ---
-#  REQUIREMENTS: ---
-#          BUGS: ---
-#         NOTES: ---
-#        AUTHOR: YOUR NAME (), 
-#  ORGANIZATION: 
-#       CREATED: 04/06/2016 09:42
-#      REVISION:  ---
-#===============================================================================
-
-set -o nounset                              # Treat unset variables as an error
-
 ~/loadrc/bashrc/smount.sh "$1"
-SERVER="$1"
-. ~/loadrc/keys/getConDetails.sh "$SERVER"
-
-if [ -z "$key" ];
-then
-  mosh "$user"@"$host" -- ${tmuxAction}
-else
-  mosh --ssh='ssh -i '"$key" "$user"@"$host" -- ${tmuxAction}
-fi
+mosh "$1" -- ${tmuxAction}
