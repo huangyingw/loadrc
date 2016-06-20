6b0af12ecaf31510196211322fc20fd026c6dd5c
Ying Huang <yinghuang@itutorgroup.com>
2016-06-20 14:38:33 -0700

    n

--- gitrc/gbil.sh
+++ gitrc/gbil.sh
@@ -1,22 +1,6 @@
 #!/bin/bash - 
-#===============================================================================
-#
-#          FILE: gbil.sh
-# 
-#         USAGE: ./gbil.sh 
-# 
-#   DESCRIPTION: 
-# 
-#       OPTIONS: ---
-#  REQUIREMENTS: ---
-#          BUGS: ---
-#         NOTES: ---
-#        AUTHOR: YOUR NAME (), 
-#  ORGANIZATION: 
-#       CREATED: 03/29/2016 13:23
-#      REVISION:  ---
-#===============================================================================
-
-set -o nounset                              # Treat unset variables as an error
-
-git bisect log | tee gbil.findresult
+if [ ! -f gbil.findresult ];
+then
+  echo git bisect start > gbil.findresult
+  exit 0
+fi
