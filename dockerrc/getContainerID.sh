d006aaa6c100155c0f22e82a8bff0dd14536ddd3
Ying Huang <yinghuang@itutorgroup.com>
2016-06-30 16:04:31 -0700

    n

---
+++ dockerrc/getContainerID.sh
@@ -0,0 +1,17 @@
+#!/bin/bash - 
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd "$SCRIPTPATH"
+
+if [ $(docker ps -q|wc -l) -eq 1 ]
+then
+  CONTAINERID=$(docker ps -q)
+elif [ -n "$1" ]
+then
+  CONTAINERID=$(docker ps|awk -v pat="$1" '$0 ~ pat { print $1}')
+fi
+if [ -z "$CONTAINERID" ]
+then
+  echo -e "${red}Must provide the image name... ${NC}"
+  exit 1
+fi
