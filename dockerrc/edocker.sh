a1ba67668cbe776c99196bc16d0ee1ffc781ddac
Ying Huang <yinghuang@itutorgroup.com>
2016-06-30 15:48:26 -0700

    n

--- dockerrc/edocker.sh
+++ dockerrc/edocker.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-if [ $(docker ps -q|wc -l) -gt 1 ];
+CONTAINERID=$(docker ps|awk -v pat="$1" '$0 ~ pat { print $1}')
+if [ $(docker ps -q|wc -l) -eq 1 ];
 then
-  if [ -z "$1" ];
-  then
-    echo -e "${red}Must provide the containder ID... ${NC}"
-    exit 1
-  else
-    CONTAINERID="$1"
-  fi
-else
   CONTAINERID=$(docker ps -q)
+fi
+if [ -z "$CONTAINERID" ];
+then
+  echo -e "${red}Must provide the image name... ${NC}"
+  exit 1
 fi
 docker exec \
   -it "$CONTAINERID" bash
