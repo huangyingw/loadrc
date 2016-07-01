f6a53976bb5a9b77d1997b600d47d0a4215f1a48
Ying Huang <yinghuang@itutorgroup.com>
2016-07-01 14:55:20 -0700

    n

--- gitrc/gsync.sh
+++ gitrc/gsync.sh
@@ -12,7 +12,9 @@ then
   exit 1
 fi
 echo "$TARGET_BRANCH"
-for ss in `git remote -v |awk '/\(fetch\)$/{print $1}'`
-do 
-  git pull $ss $TARGET_BRANCH
-done
+CURRENT_BRANCH="`git branch |awk '/^\*/{print $2}'`"
+git checkout "$TARGET_BRANCH" \
+  && ~/loadrc/gitrc/gpl.sh \
+  ; git checkout "$CURRENT_BRANCH" \
+  && ~/loadrc/gitrc/gme.sh "$TARGET_BRANCH" \
+  && ~/loadrc/bashrc/cscope.sh
