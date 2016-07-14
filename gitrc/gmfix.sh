590503c9a3c21ab6d5360ecba384741c2f71ccd0
Ying Huang <yinghuang@itutorgroup.com>
2016-07-14 11:15:46 -0700

    n

--- gitrc/gmfix.sh
+++ gitrc/gmfix.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 fix_branch=`git branch |awk '/^\*/{print $2}'`
 parent_branch=`echo ${fix_branch} | sed -e "s|.fix$||"` 
-ready_file="gfvd.already"
-if [ ! -f ${ready_file} ];
-then
-  echo -e "${red}gfvd must be run before gmfix first ... ${NC}"
-  exit 1
-fi
 if  ( ! echo $fix_branch|grep -q 'fix$' )
 then
   echo -e "${red}It must be run in fix branch ... ${NC}"
@@ -26,4 +20,3 @@ else
     && git stash pop stash@{0} \
     && git reset HEAD . 
 fi 
-rm ${ready_file}
