6f3ca60b4be7fcd75f542fed2d63f11527372a7a
Ying Huang <yinghuang@itutorgroup.com>
2016-07-14 13:22:28 -0700

    refactor gci.sh g.sh done.

--- gitrc/gci.sh
+++ gitrc/gci.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 . ~/loadrc/gitrc/getCommit_message.sh "$1"
 git commit -am "$commit_message"
-> .git/COMMIT_EDITMSG
-~/loadrc/gitrc/gclb.sh
-~/loadrc/gitrc/gsync.sh              
-~/loadrc/gitrc/gps.sh              
+~/loadrc/gitrc/postCommit.sh
