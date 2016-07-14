2c43c6991e1b5885f7bcb74608b5eb3bbef49932
Ying Huang <yinghuang@itutorgroup.com>
2016-07-14 13:19:58 -0700

    n

--- gitrc/gci.sh
+++ gitrc/gci.sh
@@ -1,31 +1,5 @@
 #!/bin/bash
-current_branch=`git branch |awk '/^\*/{print $2}'`
-commit_message=""
-if  [ -z "$(git status --porcelain)" ]
-then
-  commit_message="n"
-fi 
-if ( echo $current_branch | grep -q 'fix$' )
-then
-  commit_message="n"
-fi 
-if [ -n "$1" ]
-then
-  commit_message="$1"
-fi
-if [ -n "$(cat .git/COMMIT_EDITMSG)" ]
-then
-  commit_message=$(cat .git/COMMIT_EDITMSG)
-fi
-if [ -f .git/MERGE_MSG ]
-then
-  commit_message=$(cat .git/MERGE_MSG |awk 'NR==1')
-fi
-if [ -z "$commit_message" ]
-then
-  echo -e "${red}Must provide the commit message ... ${NC}"
-  exit 1
-fi
+. ~/loadrc/gitrc/getCommit_message.sh "$1"
 git commit -am "$commit_message"
 > .git/COMMIT_EDITMSG
 ~/loadrc/gitrc/gclb.sh
