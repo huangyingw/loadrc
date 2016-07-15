e6e494c68ac80675c163a13f3ff5bbe472250611
Ying Huang <yinghuang@itutorgroup.com>
2016-07-14 21:19:00 -0700

    n

--- bashrc/jformat.sh
+++ bashrc/jformat.sh
@@ -7,9 +7,5 @@ then
   exit 1
 fi
 LIST=( -name \*.java -o -name \*.vala )
-find "$folderForGit" -type f \( "${LIST[@]}" \) -exec astyle --mode=java -nfxejoOU -y  --max-code-length=150 -s2 --style=allman -pcH {} \;
-if [ -f "$ECLIPSEFORMAT" ]
-then
-  find "$folderForGit" -type f \( "${LIST[@]}" \) -exec ~/bin/eclipse/eclipse -nosplash -application org.eclipse.jdt.core.JavaCodeFormatter -quiet -config ~/loadrc/vimrc/.vim/ftplugin/org.eclipse.jdt.core.prefs {} \;
-fi
+find "$folderForGit" -type f \( "${LIST[@]}" \) -exec astyle --mode=java -nfxejoU -y  --max-code-length=150 -s2 --style=allman -pcH {} \;
 ~/loadrc/gitrc/gci.sh
