393035dc95f2dbd0991ece8aea23fb82e70c7f5c
Ying Huang <yinghuang@itutorgroup.com>
2016-07-15 16:48:54 -0700

    n

--- bashrc/jformat.sh
+++ bashrc/jformat.sh
@@ -1,11 +1,4 @@
 #! /bin/bash
-ECLIPSEFORMAT=.eclipseformatrc
 folderForGit=$(realpath .)
-~/loadrc/gitrc/checkGitStatus.sh
-if [ $? -ne 0 ]
-then
-  exit 1
-fi
 LIST=( -name \*.java -o -name \*.vala )
 find "$folderForGit" -type f \( "${LIST[@]}" \) -exec astyle --mode=java -nfxejoU -y  --max-code-length=150 -s2 --style=allman -pcH {} \;
-~/loadrc/gitrc/gci.sh
