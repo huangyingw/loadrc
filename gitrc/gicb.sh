262bab1260a24817abbf6cfe2c86caa2ea718f71
Ying Huang <yinghuang@itutorgroup.com>
2016-07-26 15:28:31 -0700

    n

--- gitrc/gicb.sh
+++ gitrc/gicb.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 git init \
   && $HOME/loadrc/gitrc/gcob.sh dev.fix \
-  && echo master > branch.default \
-  && touch cscope.out \
-  && touch dropbox.only \
-  && touch gme.default \
+  && $HOME/loadrc/gitrc/gdev.sh \
   && $HOME/loadrc/bashrc/cscope.sh \
   && git add . \
   && $HOME/loadrc/gitrc/gci.sh n
