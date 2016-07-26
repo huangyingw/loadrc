463686814c6c9436180045ea1ade453281f1dd95
Ying Huang <yinghuang@itutorgroup.com>
2016-07-26 15:30:13 -0700

    n

--- gitrc/gcl.sh
+++ gitrc/gcl.sh
@@ -19,10 +19,7 @@ else
     && git clone "$1" \
     && cd ${target} \
     && $HOME/loadrc/gitrc/gcob.sh dev.fix \
-    && echo master > branch.default \
-    && touch cscope.out \
-    && touch dropbox.only \
-    && touch gme.default \
+    && $HOME/loadrc/gitrc/gdev.sh \
     && $HOME/loadrc/bashrc/cscope.sh \
     && git add . \
     && $HOME/loadrc/gitrc/gci.sh
