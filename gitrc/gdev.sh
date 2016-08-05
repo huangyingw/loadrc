6646d9a627e552eeb3e93b5f165c4811aab9db9c
Ying Huang <yinghuang@itutorgroup.com>
2016-08-05 15:47:38 -0700

    n

--- gitrc/gdev.sh
+++ gitrc/gdev.sh
@@ -1,5 +1,7 @@
 #!/bin/bash - 
-echo master > branch.default \
+if [ ! -f branch.default ]; then
+  echo master > branch.default
+fi \
   && touch cscope.out \
   && touch dropbox.only \
   && touch gme.default
