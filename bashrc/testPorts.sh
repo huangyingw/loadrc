3968d5ca33556e2ca00236719ea3e5061cbfe18f
Ying Huang <yinghuang@itutorgroup.com>
2016-08-26 15:50:48 -0700

    n

--- bashrc/testPorts.sh
+++ bashrc/testPorts.sh
@@ -3,10 +3,10 @@ portList="$HOME/Dropbox/loadrc/bashrc/portList"
 while read port
 do
   echo "port listening --> $port"
-  $HOME/loadrc/bashrc/rssh.sh "$1" "sudo nc -l $port" &
+  ssh -n "$1" "nohup nc -l $port &" &
 done < "$portList"
 while read port
 do
   echo "port requesting --> $port"
-  nc -zv "$1" "$port" &
+  nc -zv "$1" "$port"
 done < "$portList"
