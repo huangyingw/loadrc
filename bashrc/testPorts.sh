9c0c58b90d0a7bd89488c364f4543a8ef1c8d7b3
Ying Huang <yinghuang@itutorgroup.com>
2016-09-08 11:00:55 -0700

    n

--- bashrc/testPorts.sh
+++ bashrc/testPorts.sh
@@ -1,12 +1,14 @@
 #!/bin/bash - 
 portList="$HOME/Dropbox/loadrc/bashrc/portList"
-while read port
-do
-  echo "port listening --> $port"
-  ssh -n "$1" "nohup nc -l $port &" &
-done < "$portList"
-while read port
-do
-  echo "port requesting --> $port"
-  nc -zv "$1" "$port"
-done < "$portList"
+while read -r line || [[ -n "$line" ]]; do
+    while read port
+    do
+        echo "port listening --> $port"
+        ssh -n "$line" "nohup nc -l $port &" &
+    done < "$portList"
+    while read port
+    do
+        echo "port requesting --> $port"
+        nc -zv "$line" "$port"
+    done < "$portList"
+done < servers.list
