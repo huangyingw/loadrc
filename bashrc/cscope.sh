8c153fdd6e03d6fa455d55b596f056c62bebb257
Ying Huang <yinghuang@itutorgroup.com>
2016-09-28 13:04:39 -0700

    n

--- bashrc/cscope.sh
+++ bashrc/cscope.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 function upsearch () {
-  test / == "$PWD" && return || test -e "$1" && echo "found: " "$PWD" && return || cd .. && upsearch "$1"
+    test / == "$PWD" && return || test -e "$1" && echo "found: " "$PWD" && return || cd .. && upsearch "$1"
 }
 if [ -z "$1" ];
 then
-  TARGETEDIR=`realpath "$PWD"`
+    TARGETEDIR=`realpath "$PWD"`
 else
-  TARGETEDIR=`realpath "$1"`
+    TARGETEDIR=`realpath "$1"`
 fi
 cd "$TARGETEDIR"
 
 upsearch cscope.out
 
 if [ ! -f cscope.out ]; then
-  echo -e "${red}No cscope.out file here, will not build the index ... ${NC}"
-  exit 0
+    echo -e "${red}No cscope.out file here, will not build the index ... ${NC}"
+    exit 0
 fi
 TARGETEDIR=`realpath "$PWD"`
-cp -nv ~/loadrc/prunefi* ./
-cp -nv ~/loadrc/includefile.conf ./
+touch prunefile.conf
+touch prunefix.conf
+touch includefile.conf
 TARGET=`pwd |sed -e "s/^.*\///g"`
 if [ -z $TARGET ];
 then
-  TARGET='osroot'
+    TARGET='osroot'
 fi
 TARGET='/export/home1/username/cscope_db/'$TARGET
 echo $TARGET
@@ -33,23 +34,23 @@ INCLUDE_FILE=includefile.conf
 or="";
 while read suf
 do
-  prune_params+=( $or "-iname" "*.$suf" )
-  or="-o"
+    prune_params+=( $or "-iname" "*.$suf" )
+    or="-o"
 done < "$PRUNE_POSTFIX"
 while read suf
 do
-  prune_files+=( $or "-wholename" "$suf" )
-  or="-o"
+    prune_files+=( $or "-wholename" "$suf" )
+    or="-o"
 done < "$PRUNE_FILE"
 or="";
 while read suf
 do
-  include_params+=( $or "-wholename" "$suf" )
-  or="-o"
+    include_params+=( $or "-wholename" "$suf" )
+    or="-o"
 done < "$INCLUDE_FILE"
-find "$TARGETEDIR" "(" "${prune_params[@]}" "${prune_files[@]}" ")" -a -prune -o -type f -size -9000k -print -exec file {} \; | grep text | cut -d: -f1 | sed 's/\(["'\''\]\)/\\\1/g;s/.*/"&"/' > ${TARGET}
+find "$TARGETEDIR" "(" "${prune_params[@]}" "${prune_files[@]}" ")" -a -prune -o -type f -size -9000k -print | sed 's/\(["'\''\]\)/\\\1/g;s/.*/"&"/' > ${TARGET}
 if [ ${#include_params[@]} -gt 0 ]; then
-  find "$TARGETEDIR" "(" "${include_params[@]}" ")" -type f -size -9000k -print | sed 's/\(["'\''\]\)/\\\1/g;s/.*/"&"/' >> ${TARGET}
+    find "$TARGETEDIR" "(" "${include_params[@]}" ")" -type f -size -9000k -print | sed 's/\(["'\''\]\)/\\\1/g;s/.*/"&"/' >> ${TARGET}
 fi
 sort -u ${TARGET} -o ${TARGET}
 cscope $PARA -i ${TARGET} 
