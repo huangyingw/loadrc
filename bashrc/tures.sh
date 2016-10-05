a918c2bb9a79d55b2eb24e31ab80d4fc0f0baf15
Ying Huang <yinghuang@itutorgroup.com>
2016-10-04 17:01:55 -0700

    n

---
+++ bashrc/tures.sh
@@ -0,0 +1,27 @@
+#!/bin/bash 
+SOURCE=/media/volgrp/slink/media/`hostname`/  
+TARGET=/
+if [ -n "$1" ]
+then 
+  SOURCE="$1"
+fi
+if [ -n "$2" ]
+then 
+  TARGET="$2"
+fi
+echo $SOURCE
+echo $TARGET
+URESEX="$HOME/loadrc/bashrc/ures_ex"
+URESIN="$HOME/loadrc/bashrc/ures_in"
+exclude_params=();
+while read suf
+do
+  exclude_params+=( "--exclude=$suf" )
+done < "$URESEX"
+include_params=();
+while read suf
+do
+  include_params+=( "--include=$suf" )
+done < "$URESIN"
+rsync -aHinv --force --delete-during "${exclude_params[@]}" "${SOURCE}" "${TARGET}"> find.cc
+vi find.cc
---
+++ bashrc/ures.sh
@@ -0,0 +1,30 @@
+#!/bin/bash 
+SOURCE=/media/volgrp/slink/media/`hostname`/ 
+TARGET=/
+if [ -n "$1" ]
+then 
+  SOURCE="$1"
+fi
+if [ -n "$2" ]
+then 
+  TARGET="$2"
+fi
+echo $SOURCE
+echo $TARGET
+URESEX="$HOME/loadrc/bashrc/ures_ex"
+URESIN="$HOME/loadrc/bashrc/ures_in"
+exclude_params=();
+while read suf
+do
+  exclude_params+=( "--exclude=$suf" )
+done < "$URESEX"
+include_params=();
+while read suf
+do
+  include_params+=( "--include=$suf" )
+done < "$URESIN"
+rsync -aH --delete-during "${exclude_params[@]}" "${SOURCE}" "${TARGET}" \
+  && scp -v "${SOURCE}"boot/vmlinuz-* "${SOURCE}"boot/initrd.img-*   "${TARGET}"boot/ \
+  && update-initramfs -u \
+  && update-grub2 \
+  && reboot
