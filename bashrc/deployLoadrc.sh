51a003361ecf97c4458bb4353260c7de9e2f45b2
Ying Huang <yinghuang@itutorgroup.com>
2016-11-02 14:16:35 -0700

    n

--- bashrc/deployLoadrc.sh
+++ bashrc/deployLoadrc.sh
@@ -1,13 +1,24 @@
-#!/bin/bash - 
+#!/bin/bash -
 SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
+realDeploy () {
+    server="$1"
+    echo "Begin loadrc deploy on $server"
+    ./rsync.sh ~/loadrc/ "$server":~/loadrc/
+    ssh -n "$server" "apt-get install -y vim-gnome cscope"
+    ssh -n "$server" "ln -s ~/loadrc/vimrc/.vimrc ~/.vimrc"
+    ssh -n "$server" "ln -s ~/loadrc/vimrc/.vim ~/.vim"
+    ssh -n "$server" "mkdir -p /export/home1/username/cscope_db/"
+}
+
+if [ -n "$1" ]
+then
+    realDeploy "$1"
+    return
+fi
+
 while read -r line || [[ -n "$line" ]]; do
-    echo "Begin loadrc deploy on $line"
-    ./rsync.sh ~/loadrc/ "$line":~/loadrc/
-    ssh -n "$line" "apt-get install -y vim-gnome cscope"
-    ssh -n "$line" "ln -s ~/loadrc/vimrc/.vimrc ~/.vimrc"
-    ssh -n "$line" "ln -s ~/loadrc/vimrc/.vim ~/.vim"
-    ssh -n "$line" "mkdir -p /export/home1/username/cscope_db/"
+    realDeploy "$line"
 done < deployLoadrc.list
