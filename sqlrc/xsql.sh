a11081434dbfcbb5d2c132c6d55d1f2e1bf5b52d
Ying Huang <yinghuang@itutorgroup.com>
2016-12-21 11:56:29 -0800

    n

--- sqlrc/xsql.sh
+++ sqlrc/xsql.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 file=${1}
 loginFile=.login
+
 if [ -n "$2" ];
 then
-  loginFile=${2}/.login
+    loginFile=${2}/.login
 fi
+
 user=`cat ${loginFile} |awk -F'=' '/user/{print $2}'`
 password=`cat ${loginFile} |awk -F'=' '/password/{print $2}'`
 host=`cat ${loginFile} |awk -F'=' '/host/{print $2}'`
 dbinstance=`cat ${loginFile} |awk -F'=' '/dbinstance/{print $2}'`
+
+echo "loginFile --> $loginFile"
+echo "user --> $user"
+echo "password --> $password"
+echo "host --> $host"
+echo "dbinstance --> $dbinstance"
+echo "file --> $file"
+
 mysql -u"$user" -p"$password" -h${host} ${dbinstance} < ${file}
