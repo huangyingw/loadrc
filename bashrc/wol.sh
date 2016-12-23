fead08c529a910ad6f4b808c5156c47dec9fecac
Ying Huang <yinghuang@itutorgroup.com>
2016-12-23 10:13:35 -0800

    n

--- bashrc/wol.sh
+++ bashrc/wol.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 if [ -z "$1" ]; then
-  wakeonlan 00:01:c0:12:94:05 #mini
-  ping mini
+    wakeonlan 00:01:c0:12:94:05 #mini
+    ping mini
 elif [ "$1" == "mldonkey" ]; then
-  wakeonlan 00:01:c0:12:92:a2 #mldonkey
-  ping mldonkey
+    wakeonlan 00:01:c0:12:92:a2 #mldonkey
+    ping mldonkey
 elif [ "$1" == "thinkpad" ]; then
-  wakeonlan 00:21:cc:c0:53:a9 #thinkpad
-  ping thinkpad
+    wakeonlan 00:21:cc:c0:53:a9 #thinkpad
+    ping thinkpad
 elif [ "$1" == "toshiba" ]; then
-  wakeonlan b8:70:f4:ce:3d:f3 #toshiba
-  ping toshiba
+    wakeonlan b8:70:f4:ce:3d:f3 #toshiba
+    ping toshiba
 elif [ "$1" == "mini" ]; then
-  wakeonlan 00:01:c0:15:fa:ed #mini
-  ping mini
+    wakeonlan 00:01:c0:15:fa:ed #mini
+    ping mini
 elif [ "$1" == "server" ]; then
-  wakeonlan 00:0a:cd:1e:e9:be #server
-  ping server
+    wakeonlan 50:e5:49:ca:fd:04 #server
+    ping server
 elif [ "$1" == "mac" ]; then
-  wakeonlan 6C:40:08:AA:76:86 #mac
-  ping mac
+    wakeonlan 6C:40:08:AA:76:86 #mac
+    ping mac
 fi
