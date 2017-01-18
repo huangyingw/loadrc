d7ec04fb63f2a31e69500b75c53398ac1e9e7168
Ying Huang <yinghuang@itutorgroup.com>
2017-01-17 17:10:50 -0800

    n

---
+++ bashrc/catRSA.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd "$SCRIPTPATH"
+
+while read -r line || [[ -n "$line" ]]; do
+    cat ~/.ssh/id_rsa.pub | sshpass -p '1qaz@WSX' ssh huangyingw@"$line"  'cat >> .ssh/authorized_keys'
+done < servers.list
+
+cd -
