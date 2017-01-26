1d7d54ba0fc3a9432cbee99bed9b87e1647efc88
Ying Huang <yinghuang@itutorgroup.com>
2017-01-26 14:06:55 -0800

    n

--- dockerrc/dockerdev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash - 
-SCRIPT=$(realpath "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
-#cd "$SCRIPTPATH"
-cp -nv \
-    ~/myproject/git/docker/dbuntu/Dockerfile \
-    ~/myproject/git/docker/dbuntu/docker-compose.yml \
-    ~/myproject/git/docker/dbuntu/entrypoint.sh \
-    ./
