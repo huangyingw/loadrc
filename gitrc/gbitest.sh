bfe416748dd112b950d9a357c9374fde97a90ee7
Ying Huang <yinghuang@itutorgroup.com>
2017-01-31 14:07:51 -0800

    n

---
+++ gitrc/gbitest.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd "$SCRIPTPATH"
+
+h2h-dependecies/KMSFilter/SubtitlesFilter-31Aug2016/jar/1.5.0-SNAPSHOT/install.sh \
+    && h2h-dependecies/KMSFilter/speex/install.sh \
+    && mvn -Dmaven.test.skip=true clean install -f appserver/h2h-app-server/server/core/pom.xml \
+    && mvn -Dmaven.test.skip=true clean install -f appserver/h2h-app-server/server/extensions/xep0045-muc/pom.xml \
+    && mvn -Dmaven.test.skip=true clean install -f appserver/h2h-app-server/server/extensions/websockets/pom.xml \
+    && mvn -Dmaven.test.skip=true clean install -f appserver/h2h-app-server/examples/embedded-war/pom.xml
+
+if [ $? -ne 0 ]
+then
+    ~/loadrc/gitrc/gbib.sh
+else
+    ~/loadrc/gitrc/gbig.sh
+fi
