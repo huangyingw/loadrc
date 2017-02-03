30f876dfa47dfc4eac9f4278fd7dbce11dee514c
Ying Huang <yinghuang@itutorgroup.com>
2017-02-02 22:28:09 -0800

    n

--- bashrc/sleep.sh
+++ bashrc/sleep.sh
@@ -2,8 +2,9 @@
 OS=`uname`
 if [ $OS == "Darwin" ];
 then
-    #~/loadrc/dockerrc/killDockers.sh
+    ~/loadrc/dockerrc/killDockers.sh
     containerCounts=$(docker ps|wc -l)
+
     if [ $containerCounts -gt 1 ]
     then
         echo -e "${red}Please stop all the running containers first... ${NC}"
