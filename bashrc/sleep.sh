e0365a815484a2510fb40a16b97bf96e8b314d27
Ying Huang <yinghuang@itutorgroup.com>
2017-02-02 22:25:24 -0800

    n

--- bashrc/sleep.sh
+++ bashrc/sleep.sh
@@ -2,7 +2,15 @@
 OS=`uname`
 if [ $OS == "Darwin" ];
 then
-    ~/loadrc/dockerrc/killDockers.sh ; pmset sleepnow
+    #~/loadrc/dockerrc/killDockers.sh
+    containerCounts=$(docker ps|wc -l)
+    if [ $containerCounts -gt 1 ]
+    then
+        echo -e "${red}Please stop all the running containers first... ${NC}"
+        exit 1
+    else
+        pmset sleepnow
+    fi
 else
     sudo -u huangyingw gnome-screensaver-command --lock && sudo pm-suspend
 fi
