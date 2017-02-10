89d39877da0aafd2f51ca63b3bb906a66918d503
Ying Huang <yinghuang@itutorgroup.com>
2017-02-10 11:24:17 -0800

    n

--- gitrc/gcl.sh
+++ gitrc/gcl.sh
@@ -2,25 +2,25 @@
 
 if [ -z "$1" ];
 then
-  echo -e "${red}Please provide parameter ... ${NC}"
-  exit 1
+    echo -e "${red}Please provide parameter ... ${NC}"
+    exit 1
 fi
 
 if [[ "$1" == *"Dropbox"* ]]
 then
-  target=`echo $1 | sed -e "s|Dropbox/||"`
-  echo ${target}
-  git clone "$1" "$target" \
-    && cd ${target} \
-    && $HOME/loadrc/bashrc/cscope.sh
+    target=`echo $1 | sed -e "s|Dropbox/||"`
+    echo ${target}
+    git clone "$1" "$target" \
+        && cd ${target} \
+        && $HOME/loadrc/bashrc/cscope.sh
 else
-  target=`echo $1 | sed -e "s|.git$||;s|^.*\/||"` \
-    && echo ${target} \
-    && git clone "$1" \
-    && cd ${target} \
-    && $HOME/loadrc/gitrc/gcob.sh dev.fix \
-    && $HOME/loadrc/gitrc/gdev.sh \
-    && $HOME/loadrc/bashrc/cscope.sh \
-    && git add . \
-    && $HOME/loadrc/gitrc/gci.sh
+    target=`echo $1 | sed -e "s|.git$||;s|^.*github.com.||;s|\/|-|"` \
+        && echo ${target} \
+        && git clone "$1" ${target} \
+        && cd ${target} \
+        && $HOME/loadrc/gitrc/gcob.sh dev.fix \
+        && $HOME/loadrc/gitrc/gdev.sh \
+        && $HOME/loadrc/bashrc/cscope.sh \
+        && git add . \
+        && $HOME/loadrc/gitrc/gci.sh
 fi
