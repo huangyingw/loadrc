d0edd5f50f5f98d0f8c9b236e8e082456c6fd7c0
Ying Huang <yinghuang@itutorgroup.com>
2017-02-17 15:18:25 -0800

    n

--- bashrc/dof.sh
+++ bashrc/dof.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-for ss in `$1`; do $2 $ss; done
+for ss in `$1`
+do
+    $2 $ss
+done
+
+for ss in \
+    str1 \
+    str2 \
+    str3
+do
+    echo $ss
+done
+
 #echo "for ss in `$1`; do echo $ss; done"
 for (( c=1; c<=5; c++ ))
 do
