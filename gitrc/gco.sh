a418d2d3e41474baac1b7b42660be3f2047c0bc5
Ying Huang <yinghuang@itutorgroup.com>
2017-03-24 10:08:27 -0700

    n

--- gitrc/gco.sh
+++ gitrc/gco.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-git branch \
-  |awk '/^\*/{print $2}' \
-  && git checkout "$1" \
-  && $HOME/loadrc/bashrc/cscope.sh
+branch="$1"
+git remote update \
+    && remoteBranch=$(git branch -a | grep remotes/.*"$branch"$ | head -1) \
+    && localBranch=$(git branch -a | grep "$branch" | sed 's/.*\///')
+echo "remoteBranch --> $remoteBranch"
+echo "localBranch --> $localBranch"
+echo "git checkout -b $localBranch $remoteBranch"
+
+#git checkout -b "$localBranch" "$remoteBranch"
+#current_branch=`git branch |awk '/^\*/{print $2}'`
+#echo "current_branch --> $current_branch"
