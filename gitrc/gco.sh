8860e5c7993d084a53b12683ece22b9d38dc3750
Ying Huang <yinghuang@itutorgroup.com>
2017-03-24 10:30:46 -0700

    n

--- gitrc/gco.sh
+++ gitrc/gco.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 branch="$1"
-git remote update \
-    && remoteBranch=$(git branch -a | grep remotes/.*"$branch"$ | head -1) \
-    && localBranch=$(git branch -a | grep "$branch" | sed 's/.*\///')
+
+git remote update
+remoteBranch=$(git branch -a | grep remotes/.*"$branch"$ | head -1 |  sed -e 's/^[ \t]*//')
+localBranch=$(git branch -a | grep "$branch" | sed 's/.*\///;s/^\*[ \t]*//' | head -1)
 echo "remoteBranch --> $remoteBranch"
 echo "localBranch --> $localBranch"
 echo "git checkout -b $localBranch $remoteBranch"
 
-#git checkout -b "$localBranch" "$remoteBranch"
-#current_branch=`git branch |awk '/^\*/{print $2}'`
-#echo "current_branch --> $current_branch"
+git checkout -b "$localBranch" "$remoteBranch"
+current_branch=`git branch |awk '/^\*/{print $2}'`
+echo "current_branch --> $current_branch"
