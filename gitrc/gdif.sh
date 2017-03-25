07568b1a3523331029d927d559147d5ef5e19b4a
Ying Huang <yinghuang@itutorgroup.com>
2017-03-25 10:46:46 -0700

    n

--- gitrc/gdif.sh
+++ gitrc/gdif.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 if [ -n "$1" ];
 then
-  if [ -n "$2" ];
-  then
-    git diff --name-status "$1" "$2" | tee gdif.findresult 
-  else
-    git diff --name-status HEAD "$1" | tee gdif.findresult  
-  fi
+    if [ -n "$2" ];
+    then
+        git diff --name-status "$1" "$2" | tee gdif.findresult
+    else
+        git diff --name-status HEAD "$1" | tee gdif.findresult
+    fi
 else
-  if [ -z "$(git status --porcelain)" ]
-  then
-    git diff --name-status `git log --oneline|awk 'NR==2{print $1}'` | tee gdif.findresult  
-  else
-    git diff --name-status | tee gdif.findresult  
-  fi
+    if [ -z "$(git status --porcelain)" ]
+    then
+        git diff --name-status `git log --oneline|awk 'NR==2{print $1}'` | tee gdif.findresult
+    else
+        git diff --name-status | tee gdif.findresult
+    fi
 fi
 OS=`uname`
 if [ "$OS" == "Linux" ]
 then
-  xargs sed -i"" "s/\bA\b/git\srm/g" < gdif.findresult
+    xargs sed -i"" "s/\bA\b/git\srm/g" < gdif.findresult
 else
-  sed -i.bak "s/[[:<:]]D[[:>:]]/git rm/g" gdif.findresult 
-  sed -i.bak "s/[[:<:]][AMT][[:>:]]/git checkout $1 -- /g" gdif.findresult 
-  sed -i.bak "s/^R[0-9]*/git mv/g" gdif.findresult 
-  for ss in $(git config --get-all gdif.ignore)
-  do 
-    sed -i.bak "/$ss/d" gdif.findresult 
-  done
+    sed -i.bak "s/[[:<:]]D[[:>:]]/git rm/g" gdif.findresult
+    sed -i.bak "s/[[:<:]][AMT][[:>:]]/git checkout $1 -- /g" gdif.findresult
+    sed -i.bak "s/^R[0-9]*/git mv/g" gdif.findresult
+    for ss in $(git config --get-all gdif.ignore)
+    do
+        sed -i.bak "/$ss/d" gdif.findresult
+    done
 fi
 sort -u gdif.findresult -o gdif.findresult
