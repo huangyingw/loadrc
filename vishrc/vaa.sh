85bcd0e14c0d2b24e781ef511bba9695b9869d15
Ying Huang <yinghuang@itutorgroup.com>
2017-03-29 12:14:58 -0700

    n

--- vishrc/vaa.sh
+++ vishrc/vaa.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 find_result="$1/""`echo "$2".vaa.findresult |sed  -e "s/\//\_/g;s/\ /\_/g"`"
 if [ -f "$find_result" ]; then
-  read -p "the search is already done, if you want to update, press u --> " update
-  case $update in
-    u)
-      ;;
-    "")
-      exit 1
-      ;;
-    ?)
-      exit 1
-      ;;
-  esac
+    read -p "the search is already done, if you want to update, press u --> " update
+    case $update in
+        u)
+            ;;
+        "")
+            exit 1
+            ;;
+        ?)
+            exit 1
+            ;;
+    esac
 fi
 echo "search in " "$1"
 cd "$1"
 cscope_db_file="/export/home1/username/cscope_db/""${PWD##*/}"
-if [ "$1" == "/" ];   
+if [ "$1" == "/" ];
 then
-  cscope_db_file="/export/home1/username/cscope_db/osroot"
+    cscope_db_file="/export/home1/username/cscope_db/osroot"
 fi
 echo "cscope_db_file " "$cscope_db_file"
 echo "result in " "$find_result"
