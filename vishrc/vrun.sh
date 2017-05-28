fecc9d3da2f39664568bf640427ef83a365b30f3
Ying Huang <yinghuang@itutorgroup.com>
2017-05-28 16:01:52 -0700

    n

--- vishrc/vrun.sh
+++ vishrc/vrun.sh
@@ -2,38 +2,38 @@
 file=${1}
 if [ $(basename "$file") == "gbil.findresult" ];
 then
-  ~/loadrc/gitrc/gbir.sh
-  exit 0
+    ~/loadrc/gitrc/gbir.sh
+    exit 0
 fi
 extension=${file##*.}
 case $extension in
-  sql)
-    ~/loadrc/sqlrc/xsql.sh ${1} ${2} 2>&1 | tee ${1}.findresult 
-    read -p "press any key to continue...." update
-    ;;
-  findresult)
-    sh ${1} 2>&1 | tee ${1}.findresult
-    read -p "press any key to continue...." update
-    ;;
-  sh)
-    sh ${1} 2>&1 | tee ${1}.findresult 
-    read -p "press any key to continue...." update
-    ;;
-  py)
-    python ${1} 2>&1 | tee ${1}.findresult 
-    read -p "press any key to continue...." update
-    ;;
-  vim)
-    source ${1}
-    read -p "press any key to continue...." update
-    ;;
-  yml)
-    docker-compose -f "$file" up -d 2>&1 | tee "$file".findresult
-    docker-compose -f "$file" restart 2>&1 | tee "$file".findresult
-    read -p "press any key to continue...." update
-    ;;
-  ymldebug)
-    docker-compose -f "$file" up --build --force-recreate 2>&1 | tee "$file".findresult
-    read -p "press any key to continue...." update
-    ;;
+    sql)
+        ~/loadrc/sqlrc/xsql.sh ${1} ${2} 2>&1 | tee ${1}.findresult
+        read -p "press any key to continue...." update
+        ;;
+    findresult)
+        sh ${1} 2>&1 | tee ${1}.findresult
+        read -p "press any key to continue...." update
+        ;;
+    sh)
+        sh ${1} 2>&1 | tee ${1}.findresult
+        read -p "press any key to continue...." update
+        ;;
+    py)
+        python ${1} 2>&1 | tee ${1}.findresult
+        read -p "press any key to continue...." update
+        ;;
+    vim)
+        source ${1}
+        read -p "press any key to continue...." update
+        ;;
+    yml)
+        docker-compose -f "$file" up -d 2>&1 | tee "$file".findresult
+        docker-compose -f "$file" restart 2>&1 | tee "$file".findresult
+        read -p "press any key to continue...." update
+        ;;
+    ymldebug)
+        docker-compose -f "$file" up --build --force-recreate 2>&1 | tee "$file".findresult
+        read -p "press any key to continue...." update
+        ;;
 esac
