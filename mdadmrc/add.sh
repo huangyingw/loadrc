0118ac39ab21f2f73dbbd6a9257b169a83ee42c8
Ying Huang <yinghuang@itutorgroup.com>
2017-05-29 13:49:30 -0700

    n

--- mdadmrc/add.sh
+++ mdadmrc/add.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -
+mdadm --create --verbose /dev/md0 --level=5 --raid-devices=3 /dev/sdb1 /dev/sdc1 /dev/sdd1 --spare-devices=1 /dev/sde1
 mdadm --manage /dev/md0 --run
-mdadm --manage /dev/md0 --add /dev/sdc
+mdadm --manage /dev/md0 --add /dev/sd[b-d]
+mdadm --grow /dev/md0 --size=max
+reboot
