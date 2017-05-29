25e64bd3c8f3923f459de08405ebe60783082176
Ying Huang <yinghuang@itutorgroup.com>
2017-05-29 14:41:19 -0700

    n

--- mdadmrc/add.sh
+++ mdadmrc/add.sh
@@ -3,6 +3,8 @@
 mdadm --create --verbose /dev/md0 --level=6 --raid-devices=4 /dev/sd[b-d] --spare-devices=1 /dev/sde1
 cryptsetup luksAddKey /dev/md0 /root/loadrc/keys/keyfile
 mdadm --manage /dev/md0 --run
-mdadm --manage /dev/md0 --add /dev/sd[b-d]
+mdadm --manage /dev/md0 --add /dev/sd[c-d]
 mdadm --grow /dev/md0 --size=max
 reboot
+e2fsck -f /dev/mapper/md0_crypt
+resize2fs /dev/mapper/md0_crypt
