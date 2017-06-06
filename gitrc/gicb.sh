14a0dc900e3587c437d8d8f0b28bb4052aa7e3e5
Ying Huang <yhuang@imvu.com>
2017-06-05 23:06:46 -0700

    n

--- gitrc/gicb.sh
+++ gitrc/gicb.sh
@@ -2,7 +2,7 @@
 
 git init \
     && $HOME/loadrc/gitrc/gcob.sh dev.fix \
-    && $HOME/loadrc/gitrc/gdev.sh \
+    ; $HOME/loadrc/gitrc/gdev.sh \
     && $HOME/loadrc/bashrc/cscope.sh \
     && xargs git add < files.proj \
     && $HOME/loadrc/gitrc/gci.sh n
