e27e0c9e93c95dafa93b174ff391d014b0ae97ae
Ying Huang <yhuang@imvu.com>
2017-06-07 13:18:55 -0700

    n

--- gitrc/gdif.sh
+++ gitrc/gdif.sh
@@ -19,6 +19,13 @@ OS=`uname`
 if [ "$OS" == "Linux" ]
 then
     sed -i.bak "s/\b[AMT]\b/git checkout $1 -- /g" gdif.findresult
+    sed -i.bak "s/\bD\b/git rm/g" gdif.findresult
+    sed -i.bak "s/^R[0-9]*/git mv/g" gdif.findresult
+    sed -i.bak "s/^R[0-9]*/git mv/g" gdif.findresult
+    for ss in $(git config --get-all gdif.ignore)
+    do
+        sed -i.bak "/$ss/d" gdif.findresult
+    done
 else
     sed -i.bak "s/[[:<:]]D[[:>:]]/git rm/g" gdif.findresult
     sed -i.bak "s/[[:<:]][AMT][[:>:]]/git checkout $1 -- /g" gdif.findresult
