fa9f6696304c9ba3f3618468d20b686e57f7c7ec
Ying Huang <yhuang@imvu.com>
2017-06-07 13:27:46 -0700

    n

--- gitrc/gdif.sh
+++ gitrc/gdif.sh
@@ -21,7 +21,6 @@ then
     sed -i.bak "s/\b[AMT]\b/git checkout $1 -- /g" gdif.findresult
     sed -i.bak "s/\bD\b/git rm/g" gdif.findresult
     sed -i.bak "s/^R[0-9]*/git mv/g" gdif.findresult
-    sed -i.bak "s/^R[0-9]*/git mv/g" gdif.findresult
     for ss in $(git config --get-all gdif.ignore)
     do
         sed -i.bak "/$ss/d" gdif.findresult
