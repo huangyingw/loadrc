620b919cd1b0763b2f5cc2197782ef1494da3da8
Ying Huang <yhuang@imvu.com>
2017-06-08 09:56:40 -0700

    n

--- svnrc/svnst.sh
+++ svnrc/svnst.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-svn st > svn.findresult
-vi svn.findresult
+svn st | tee svnst.findresult
+
+OS=`uname`
+if [ "$OS" == "Linux" ]
+then
+    sed -i.bak "/^?/d" svnst.findresult
+    sed -i.bak "/^$/d" svnst.findresult
+else
+    sed -i.bak "s/[[:<:]]D[[:>:]]/git rm/g" svnst.findresult
+    sed -i.bak "s/[[:<:]][AMT][[:>:]]/git checkout $1 -- /g" svnst.findresult
+    sed -i.bak "s/^R[0-9]*/git mv/g" svnst.findresult
+fi
