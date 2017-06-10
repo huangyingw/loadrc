45644a56131439a85e594d7a8422e46229088f1e
Ying Huang <yhuang@imvu.com>
2017-06-10 12:07:22 -0700

    Revert "n"

--- bashrc/cscope.sh
+++ bashrc/cscope.sh
@@ -46,5 +46,6 @@ if [ ${#include_params[@]} -gt 0 ]; then
     find -L . "(" "${include_params[@]}" ")" -type f -size -9000k -print | sed 's/\(["'\''\]\)/\\\1/g;s/.*/"&"/' >> ${TARGET}
 fi
 sort -u ${TARGET} -o ${TARGET}
+cscope $PARA -i ${TARGET}
 echo "$TARGETEDIR"/${TARGET} | sed 's/\(["'\''\]\)/\\\1/g;s/.*/"&"/' >> ~/files.proj
 sort -u ~/files.proj -o ~/files.proj
