67aa3ac778f02b7d7198f9d423d67fd436250689
Ying Huang <yhuang@imvu.com>
2017-06-12 22:59:19 -0700

    n

--- bashrc/smount.sh
+++ bashrc/smount.sh
@@ -4,17 +4,17 @@ then
     echo -e "${red}please provide the host name... ${NC}"
     exit 1
 fi
-ps ax|awk '/sshfs/{print $1}'|while read ss; do kill -9 "$ss"; done
+
 OS=`uname`
 if [ "$OS" == "Linux" ]
 then
     umount /media/samba
-    sshfs "$1":/media /media/samba
+    mkdir -p /media/"$1"
+    sshfs "$1":/media /media/"$1"
+    df -TH
 else
     diskutil unmountDisk force ~/samba
-    sshfs "$1":/ ~/samba
+    mkdir -p ~/"$1"
+    sshfs "$1":/ ~/"$1"
+    df -H
 fi
-
-df \
-    && cd ~/samba \
-    && echo -e "${green}$(pwd) ${NC}" "in"  "${green}$1 ${NC}"
