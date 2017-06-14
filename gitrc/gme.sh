5c7f182da160e2e9ea0b097ba33480140c25ed94
Ying Huang <yhuang@imvu.com>
2017-06-13 17:00:13 -0700

    n

--- gitrc/gme.sh
+++ gitrc/gme.sh
@@ -2,8 +2,8 @@
 GMEDEFAULT=gme.default
 if [ ! -f "$GMEDEFAULT" ];
 then
-  echo -e "${red}git merge is not allowed, without tag file: gme.default ... ${NC}"
-  exit 1
+    echo -e "${red}git merge is not allowed, without tag file: gme.default ... ${NC}"
+    exit 1
 fi
 git merge "$1" \
-  && ~/loadrc/gitrc/gps.sh
+    && ~/loadrc/gitrc/gps.sh
