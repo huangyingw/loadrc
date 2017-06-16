1c799e73f86ba36ba2a6b0191555261f2c0d58eb
Ying Huang <yhuang@imvu.com>
2017-06-16 15:00:35 -0700

    n

--- svnrc/svnst.sh
+++ svnrc/svnst.sh
@@ -6,6 +6,7 @@ if [ "$OS" == "Linux" ]
 then
     sed -i.bak "/^?/d" svnst.findresult
     sed -i.bak "/^$/d" svnst.findresult
+    sed -i.bak "s/\b[M]\b/svn cl utest $1/g" svnst.findresult
 else
     sed -i.bak "s/[[:<:]]D[[:>:]]/git rm/g" svnst.findresult
     sed -i.bak "s/[[:<:]][AMT][[:>:]]/git checkout $1 -- /g" svnst.findresult
