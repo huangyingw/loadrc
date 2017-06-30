9757ec6609e997ff524c11ec74a79da7c05fb517
Ying Huang <yhuang@imvu.com>
2017-06-29 21:40:17 -0700

    n

--- gitrc/gclean.sh
+++ gitrc/gclean.sh
@@ -1,22 +1,2 @@
-#!/bin/bash - 
-#===============================================================================
-#
-#          FILE: gclean.sh
-# 
-#         USAGE: ./gclean.sh 
-# 
-#   DESCRIPTION: 
-# 
-#       OPTIONS: ---
-#  REQUIREMENTS: ---
-#          BUGS: ---
-#         NOTES: ---
-#        AUTHOR: YOUR NAME (), 
-#  ORGANIZATION: 
-#       CREATED: 04/12/2016 15:07
-#      REVISION:  ---
-#===============================================================================
-
-set -o nounset                              # Treat unset variables as an error
-
+#!/bin/bash -
 git clean -fd
