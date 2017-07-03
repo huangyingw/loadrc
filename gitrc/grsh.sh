69d187a50c24d089e1bcba7b27a29ec88748863d
Ying Huang <yhuang@imvu.com>
2017-07-02 18:23:46 -0700

    Revert "n"

--- gitrc/grsh.sh
+++ gitrc/grsh.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-git status | tee grsh.findresult
-
-gsed -i.bak "s/deleted:/git checkout -- /g" grsh.findresult
-gsed -i.bak "s/modified:/git checkout -- /g" grsh.findresult
-gsed -i.bak 's/^[ \t]*//;s/[ \t]*$//' grsh.findresult
-gsed -i.bak '/^git /!d' grsh.findresult
-sort -u grsh.findresult -o grsh.findresult
+git reset --hard $1
