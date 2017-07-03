174018c782cc16fa748217be127c907745185311
Ying Huang <yhuang@imvu.com>
2017-07-03 13:54:06 -0700

    n

--- gitrc/gstp.sh
+++ gitrc/gstp.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 fix_branch=`git branch |awk '/^\*/{print $2}'`
-if [ -n "$1" ];
+if [ -n "$1" ]
 then
-  git stash pop stash@{"$1"}
-  exit 0
+    git stash pop stash@{"$1"}
+    exit 0
 fi
 ~/loadrc/gitrc/checkGitStatus.sh
 if [ $? -ne 0 ]
 then
-  exit 1
+    exit 1
 fi
 top_branch=`git stash list|awk 'NR==v1 {print $4}' v1=$(($1+1)) |sed 's/://g'`
 echo $top_branch
