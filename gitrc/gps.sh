9bf6afabab8488ab35c866ed03d717b097d89847
Ying Huang <yhuang@imvu.com>
2017-07-12 23:15:40 -0700

    n

--- gitrc/gps.sh
+++ gitrc/gps.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 chmod 400 ~/.ssh/id_rsa
 ~/loadrc/gitrc/gpl.sh
-if [ -f default.only ];
+
+if [[ $(git config --get-all push.remote) ]]
 then
-  git push -v default && git push -v --tag default
-  exit 0
+    for ss in $(git config --get-all push.remote)
+    do
+        git push -v "$ss" && git push -v --tag "$ss"
+    done
+else
+    for ss in `git remote -v |awk '/\(fetch\)$/{print $1}'`; do git push -v $ss && git push -v --tag $ss; done
 fi
-for ss in `git remote -v |awk '/\(fetch\)$/{print $1}'`; do git push -v $ss && git push -v --tag $ss; done
