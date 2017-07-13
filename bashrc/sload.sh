cb2342d3c92f85aa009b599b5afad1803c3e223c
Ying Huang <yhuang@imvu.com>
2017-07-13 11:02:05 -0700

    n

--- bashrc/sload.sh
+++ bashrc/sload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash - 
 if ping -c 1 macpro &> /dev/null
 then
-  sshfs -o nonempty huangyingw@macpro:/Users/huangyingw/Dropbox/loadrc/ ~/loadrc/
+  sshfs -o nonempty huangyingw@macpro:~/Dropbox/loadrc/ ~/loadrc/
   df -TH
   . $HOME/loadrc/.loadrc
 else
