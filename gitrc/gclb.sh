c0092eb97b0875c43aa38d490acdc8ff0d154597
Ying Huang <yhuang@imvu.com>
2017-07-13 23:49:05 -0700

    n

--- gitrc/gclb.sh
+++ gitrc/gclb.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 x=`pwd`
-x=`echo ${x} | sed -e "s|/.*/myproject|$HOME/BareReps/myproject|"`
-x=`echo ${x} | sed -e "s|/.*/loadrc|$HOME/BareReps/loadrc|"`
+x=`echo ${x} | sed -e "s|/.*/myproject|~/BareReps/myproject|"`
+x=`echo ${x} | sed -e "s|/.*/loadrc|~/BareReps/loadrc|"`
 echo ${x}
 
 if [ ! -d ${x} ];
 then
-  git clone --bare . ${x}
+    git clone --bare . ${x}
 fi
+
 git remote add BareReps ${x}
