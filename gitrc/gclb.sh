b564d2bb2478ecfacd02f216ff35ddc963be9c9c
Ying Huang <yhuang@imvu.com>
2017-07-14 23:54:49 -0700

    n

--- gitrc/gclb.sh
+++ gitrc/gclb.sh
@@ -2,6 +2,7 @@
 x=`pwd`
 x=`echo "$x" | sed -e "s|myproject|BareReps/myproject|"`
 x=`echo "$x" | sed -e "s|loadrc|BareReps/loadrc|"`
+x=`echo "$x" | sed -e "s|imvu|BareReps/imvu|"`
 echo "BareReps location --> $x"
 
 if [ ! -d "$x" ];
