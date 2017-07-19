350509071952b3806697d65c9bc8625233743d71
Ying Huang <yhuang@imvu.com>
2017-07-18 22:22:12 -0700

    n

--- gitrc/gvd.sh
+++ gitrc/gvd.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+if [ ${PWD##*/} == ".git" ]
+then
+    cd ../
+fi
+
 if [ -n "$1" ];
 then
-  if [ -n "$2" ];
-  then
-    git difftool "$1" "$2"
-  else
-    git difftool "$1"
-  fi
+    if [ -n "$2" ];
+    then
+        git difftool "$1" "$2"
+    else
+        git difftool "$1"
+    fi
 else
-  git difftool --cached
-  git difftool
+    git difftool --cached
+    git difftool
 fi
