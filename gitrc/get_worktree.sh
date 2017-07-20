d7143a8da8a7728dc7305198eedda0f2594af83e
Ying Huang <yhuang@imvu.com>
2017-07-19 22:17:31 -0700

    n

--- gitrc/get_worktree.sh
+++ gitrc/get_worktree.sh
@@ -3,7 +3,8 @@ configFile=$(echo "$1" | sed 's/index$/config/')
 workTree=$(cat "$configFile" | awk '/worktree/{print $3}')
 if [ -z "$workTree" ];
 then
-    echo "$(realpath "$1"/../)"
+    workTreeRelative=$(echo "$1" | sed 's/\.git\/index$//')
+    echo "$(realpath "$workTreeRelative")"
 else
     workTreeRelative=$(echo "$workTree" | sed 's/index$//')
     echo "$(realpath $(echo "$1" | sed 's/index$//')"$workTreeRelative")"
