0f84ea77dc67ed335382d013193135f8f4672c22
Ying Huang <yhuang@imvu.com>
2017-07-20 15:27:26 -0700

    n

--- gitrc/gvd.sh
+++ gitrc/gvd.sh
@@ -3,11 +3,11 @@ if [ -n "$1" ];
 then
     if [ -n "$2" ];
     then
-        git difftool "$1" "$2"
+        git difftool -y "$1" "$2"
     else
-        git difftool "$1"
+        git difftool -y "$1"
     fi
 else
-    git difftool --cached
-    git difftool
+    git difftool -y --cached
+    git difftool -y
 fi
