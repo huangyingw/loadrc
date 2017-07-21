ccce0f8c4cf85ed52385f64285cf6d474e088513
Ying Huang <yhuang@imvu.com>
2017-07-21 13:39:25 -0700

    n

--- gitrc/gps.sh
+++ gitrc/gps.sh
@@ -11,3 +11,5 @@ then
 else
     for ss in `git remote -v |awk '/\(fetch\)$/{print $1}'`; do git push -v "$ss" && git push -v --tag "$ss"; done
 fi
+
+git push -v BareReps && git push -v --tag BareReps
