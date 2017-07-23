add6369b878bc4dabc70f995a890b01b267d70c1
Ying Huang <yhuang@imvu.com>
2017-07-22 19:56:33 -0700

    The commit message from COMMIT_EDITMSG should work now.

--- gitrc/getCommit_message.sh
+++ gitrc/getCommit_message.sh
@@ -1,28 +1,30 @@
-#!/bin/bash - 
+#!/bin/bash -
 current_branch=`git branch |awk '/^\*/{print $2}'`
 commit_message=""
 if  [ -z "$(git status --porcelain)" ]
 then
-  commit_message="n"
-fi 
+    commit_message="n"
+fi
 if ( echo $current_branch | grep -q 'fix$' )
 then
-  commit_message="n"
-fi 
+    commit_message="n"
+fi
 if [ -n "$1" ]
 then
-  commit_message="$1"
+    commit_message="$1"
 fi
-if [ -n "$(cat .git/COMMIT_EDITMSG)" ]
+cd $(~/loadrc/gitrc/get_index.sh $pwd)
+if [ -n "$(cat COMMIT_EDITMSG)" ]
 then
-  commit_message=$(cat .git/COMMIT_EDITMSG)
+    commit_message=$(cat COMMIT_EDITMSG)
 fi
 if [ -f .git/MERGE_MSG ]
 then
-  commit_message=$(cat .git/MERGE_MSG |awk 'NR==1')
+    commit_message=$(cat MERGE_MSG |awk 'NR==1')
 fi
 if [ -z "$commit_message" ]
 then
-  echo -e "${red}Must provide the commit message ... ${NC}"
-  exit 1
+    echo -e "${red}Must provide the commit message ... ${NC}"
+    exit 1
 fi
+cd -
--- gitrc/postCommit.sh
+++ gitrc/postCommit.sh
@@ -1,5 +1,7 @@
-#!/bin/bash - 
-> .git/COMMIT_EDITMSG
+#!/bin/bash -
+cd $(~/loadrc/gitrc/get_index.sh $pwd)
+> COMMIT_EDITMSG
+cd -
 ~/loadrc/gitrc/gclb.sh
-~/loadrc/gitrc/gsync.sh              
+~/loadrc/gitrc/gsync.sh
 ~/loadrc/gitrc/gps.sh
