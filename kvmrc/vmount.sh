859735e72c6646706f602d5bf0517e6c8408755c
Ying Huang <yhuang@imvu.com>
2017-08-05 10:23:00 -0700

    n

--- kvmrc/vmount.sh
+++ kvmrc/vmount.sh
@@ -1,13 +1,19 @@
-#!/bin/bash - 
+#!/bin/bash -
 if [ ! -f "$1" ] || [ -z "$1" ];
 then
-  echo -e "${red}Please provide the full path to qcow2 file... ${NC}"
-  exit 1
+    echo -e "${red}Please provide the full path to qcow2 file... ${NC}"
+    exit 1
 fi
 modprobe nbd max_part=8
 umount -fl /media/nbd0
 qemu-nbd --disconnect /dev/nbd0
 qcow2File="$1"
 qemu-nbd --connect=/dev/nbd0 "$qcow2File"
-mount $(fdisk /dev/nbd0 -l|awk '/nbd0/ {a=$1} END{print a}') /media/nbd0 
+
+for ss in $(fdisk /dev/nbd0 -l | awk '/nbd0p/{print $1}')
+do
+    echo $ss
+    mkdir -p ~/$ss
+    mount $ss ~/$ss
+done
 df -TH
