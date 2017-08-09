c22d431055b6f9bbf7a11b4f0ce33c4eca889204
Ying Huang <yhuang@imvu.com>
2017-08-09 14:18:37 -0700

    n

--- svnrc/svnst.sh
+++ svnrc/svnst.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-svn st | tee svnst.findresult
-
-sed -i.bak "/^?/d" svnst.findresult
+svn status | sed -e '/^.*.\(findresult\|orig\|sw.\|bak\|default\)$/d' |sed -e '/\(files.proj\|.gitignore\|includefile.conf\|prunefile.conf\|prunefix.conf\)/d' | tee svnst.findresult 
 sed -i.bak "/^$/d" svnst.findresult
 sed -i.bak "s/\b[M]\b/svn cl utest $1/g" svnst.findresult
