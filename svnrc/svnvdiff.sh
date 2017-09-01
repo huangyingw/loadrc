7152425e8b59b14505d8bbb7f1a134ac8257315f
Ying Huang <yhuang@imvu.com>
2017-08-31 17:10:33 -0700

    n

--- svnrc/svnvdiff.sh
+++ svnrc/svnvdiff.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-svn diff --cl utest --diff-cmd kdiff3
+svn status | tee svnvdiff.findresult 
+
+while read ss
+do
+    sed -i.bak "/^.*.$ss\$/d" svnvdiff.findresult
+done < svnpostfix.ignore
+
+while read ss
+do
+    ss=$(echo $ss | sed  -e "s/\//\\\\\//g")
+    sed -i.bak "/$ss/d" svnvdiff.findresult
+done < svnfiles.ignore
+
+sed -i.bak "/^$/d" svnvdiff.findresult
+sed -i.bak "/Changelist/d" svnvdiff.findresult
+sed -i.bak "s/\b[M]\b/svn diff --diff-cmd kdiff3 $1/g" svnvdiff.findresult
