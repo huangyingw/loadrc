2b99dbcad216d05cc38526fea5beae7fc5af62a7
Ying Huang <yhuang@imvu.com>
2017-09-10 23:34:10 -0700

    n

--- setup.sh
+++ setup.sh
@@ -9,3 +9,8 @@ ln -fs ./gitrc/.gitconfig ~/.gitconfig
 ln -fs ./gitrc/.globalgitignore ~/.globalgitignore
 ln -fs ./vimrc/.vim ~/.vim
 ln -fs ./vimrc/.vimrc ~/.vimrc
+git submodule init
+git submodule sync
+git submodule update
+./macvim/build.sh
+./vimrc/.vim/bundle/YouCompleteMe/install.sh
