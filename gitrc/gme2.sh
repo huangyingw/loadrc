3b035c3cb345710a694093e48c22d6488a8b7645
Ying Huang <yhuang@imvu.com>
2017-09-22 14:22:07 -0700

    n

--- gitrc/gme2.sh
+++ gitrc/gme2.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 CURRENT_BRANCH="`git branch |awk '/^\*/{print $2}'`"
-TARGET_BRANCH=`echo ${CURRENT_BRANCH} | sed -e "s|.fix$||"` 
+TARGET_BRANCH=`echo ${CURRENT_BRANCH} | sed -e "s|.fix$||"`
 if [ -n "$1" ];
 then
-  TARGET_BRANCH="$1"
+    TARGET_BRANCH="$1"
 fi
 echo "$TARGET_BRANCH"
 if [ -z "$(git status --porcelain)" ]
 then
-  git checkout "$TARGET_BRANCH" \
-    && ~/loadrc/gitrc/gme.sh "$CURRENT_BRANCH" \
-    && git checkout "$CURRENT_BRANCH"
+    git checkout "$TARGET_BRANCH" \
+        && ~/loadrc/gitrc/gme.sh "$CURRENT_BRANCH" \
+        ; git checkout "$CURRENT_BRANCH"
 else
-  git add . \
-    && git stash|grep -q "HEAD is now at" \
-    && git checkout "$TARGET_BRANCH" \
-    && ~/loadrc/gitrc/gme.sh "$CURRENT_BRANCH" \
-    && git checkout "$CURRENT_BRANCH" \
-    && git stash pop stash@{0} \
-    && git reset HEAD . 
-fi 
+    git add . \
+        && git stash|grep -q "HEAD is now at" \
+        && git checkout "$TARGET_BRANCH" \
+        && ~/loadrc/gitrc/gme.sh "$CURRENT_BRANCH" \
+        ; git checkout "$CURRENT_BRANCH" \
+        && git stash pop stash@{0} \
+        && git reset HEAD .
+fi
