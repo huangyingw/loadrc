8cf3dd9b3f9ef65cc9fa91f0482dc0018d0e9b2b
Ying Huang <yhuang@imvu.com>
2017-09-26 15:14:08 -0700

    n

--- gitrc/gpl.sh
+++ gitrc/gpl.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 . ~/loadrc/gitrc/getCurrentBranch.sh
 for ss in `git remote -v |awk '/\(fetch\)$/{print $1}'`
-do 
-  git pull $ss $currentBranch
+do
+    git pull $ss $currentBranch
 done
+echo "gpl done"
