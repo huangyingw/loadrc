b61a9dcd35bab69d0e313361bbd1ed3883bd00dc
Ying Huang <yhuang@imvu.com>
2017-11-08 11:56:22 -0800

    n

---
+++ pythonrc/remove-unuses.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -
+if [ -z "$1" ]
+then
+    echo -e "${red}Please provide TARGET ... ${NC}"
+    exit 1
+fi
+
+TARGET="$1"
+autoflake --in-place --remove-unused-variables --remove-all-unused-imports "$TARGET"
