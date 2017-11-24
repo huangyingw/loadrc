03649193169c6c362e8e65ee529e1e483b338d10
Ying Huang <yhuang@imvu.com>
2017-11-24 12:20:08 -0800

    n

--- bashrc/vi.sh
+++ bashrc/vi.sh
@@ -1,19 +1,2 @@
 #!/bin/bash
-
-function upsearch () {
-    test / == "$pwd" && return || test -e "$1" && echo "found: " "$pwd" && return || cd .. && upsearch "$1"
-}
-
-TARGET=files.proj
-
-if [ -z "$1" ];
-then
-    if [ -f "$TARGET" ]
-    then
-        vim $TARGET
-    fi
-else
-    vim "$1"
-fi
-
-upsearch "$TARGET"
+vi `~/loadrc/bashrc/find_up.sh "$PWD" files.proj` 
