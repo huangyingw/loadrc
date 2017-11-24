09e1be66a780901ea1c35d2f87940d9d4a950d83
Ying Huang <yhuang@imvu.com>
2017-11-24 12:26:38 -0800

    n

---
+++ bashrc/find_up_goto.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -
+function upsearch () {
+    test / == "$pwd" && return || test -e "$1" && return || cd .. && upsearch "$1"
+}
+
+upsearch "$1"
---
+++ bashrc/find_up_goto_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -
+. ~/loadrc/bashrc/find_up_goto.sh ".git"
+echo "pwd --> $PWD"
+cd ~/myproject/git/AI/MorvanZhou/tutorials/kerasTUT/
+. ~/loadrc/bashrc/find_up_goto.sh ".git"
+echo "pwd --> $PWD"
+. ~/loadrc/bashrc/find_up_goto.sh "files.proj"
+echo "pwd --> $PWD"
