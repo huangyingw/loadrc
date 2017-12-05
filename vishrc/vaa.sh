035a92c30bd5fd3a36579340795f4dee83ad5149
Ying Huang <yhuang@imvu.com>
2017-12-05 10:57:36 -0800

    n

--- vishrc/vaa.sh
+++ vishrc/vaa.sh
@@ -4,7 +4,7 @@ echo "search in " "$1"
 cd "$1"
 find_result="$3".vaa.findresult
 echo "find_result --> $find_result"
-if [ -f "$find_result" ] 
+if [ -f "$find_result" ]
 then
     read -p "the search is already done, if you want to update, press u --> " update
     case $update in
@@ -20,4 +20,4 @@ then
 fi
 cscope_db_file=files.proj
 echo "result in " "$1/$find_result"
-xargs fgrep -inH "$2" < "$cscope_db_file" > "$find_result"
+xargs fgrep -inH -- "$2" < "$cscope_db_file" > "$find_result"
