d15196c2514dd999dac321c632eab8a415c8c9f4
Ying Huang <yhuang@imvu.com>
2018-01-11 10:26:50 -0800

    n

--- svnrc/svndiff.sh
+++ svnrc/svndiff.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-svn diff --diff-cmd kdiff3 "$1"
+fullfile="$1"
+if [ $(basename "$fullfile") == "index" ]
+then
+    svn diff --cl utest > utest.diff
+else
+    svn diff --diff-cmd kdiff3 "$fullfile"
+fi
