8b78ba882f8bc7f63b37445702cab08f4c643ace
Ying Huang <yhuang@imvu.com>
2018-01-17 14:08:06 -0800

    n

---
+++ deploy_links.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd "$SCRIPTPATH"
+
+mv -fv ~/.config/git/gitk ~/.config/git/gitk.bak
+ln -fs "$SCRIPTPATH" ~/loadrc
+ln -fs ~/loadrc/."`hostname`".theanorc  ~/.theanorc
+ln -fs ~/loadrc/.config/git/gitk  ~/.config/git/gitk
+ln -fs ~/loadrc/.profile ~/.profile
+ln -fs ~/loadrc/.tmux.conf ~/.tmux.conf
+ln -fs ~/loadrc/gitrc/.gitconfig ~/.gitconfig
+ln -fs ~/loadrc/gitrc/.globalgitignore ~/.globalgitignore
+ln -fs ~/loadrc/pythonrc/setup.cfg  ~/setup.cfg
+ln -fs ~/loadrc/usr/NX/etc/node.cfg  /usr/NX/etc/node.cfg
+ln -fs ~/loadrc/vimrc/.vim ~/.vim
+ln -fs ~/loadrc/vimrc/.vimrc ~/.vimrc
--- setup.sh
+++ setup.sh
@@ -9,18 +9,7 @@ then
     ./install_basic.sh
     ln -fs ~/loadrc/.bashrc ~/.bashrc
 fi
-mv -fv ~/.config/git/gitk ~/.config/git/gitk.bak
-ln -fs "$SCRIPTPATH" ~/loadrc
-ln -fs ~/loadrc/."`hostname`".theanorc  ~/.theanorc
-ln -fs ~/loadrc/.config/git/gitk  ~/.config/git/gitk
-ln -fs ~/loadrc/.profile ~/.profile
-ln -fs ~/loadrc/.tmux.conf ~/.tmux.conf
-ln -fs ~/loadrc/gitrc/.gitconfig ~/.gitconfig
-ln -fs ~/loadrc/gitrc/.globalgitignore ~/.globalgitignore
-ln -fs ~/loadrc/pythonrc/setup.cfg  ~/setup.cfg
-ln -fs ~/loadrc/usr/NX/etc/node.cfg  /usr/NX/etc/node.cfg
-ln -fs ~/loadrc/vimrc/.vim ~/.vim
-ln -fs ~/loadrc/vimrc/.vimrc ~/.vimrc
+./deploy_links.sh
 git submodule init
 git submodule sync
 git submodule update
