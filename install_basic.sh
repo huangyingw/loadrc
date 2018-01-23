9a3b821eca8e9b071f3e228d1d3a34ec89de3807
Ying Huang <yhuang@imvu.com>
2018-01-22 18:23:33 -0800

    n

--- install_basic.sh
+++ install_basic.sh
@@ -3,40 +3,39 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-if [ $(uname) == "Darwin" ]
+if [ $(uname) != "Darwin" ]
 then
-    exit 0
+    apt-get update
+    apt-get upgrade -y
+    apt-get purge -y totem totem-plugins
+    apt-get install -y \
+        bcmwl-kernel-source \
+        binutils \
+        cryptsetup \
+        curl \
+        dkms \
+        git \
+        htop \
+        kdiff3 \
+        mdadm \
+        mosh \
+        net-tools \
+        pm-utils \
+        realpath  \
+        slurm \
+        smplayer \
+        ssh \
+        sshfs \
+        tmux \
+        vim-gnome
+    modprobe wl
+    cp -fv ./etc/NetworkManager/system-connections/dd-wrt /etc/NetworkManager/system-connections/dd-wrt
+    cp -fv ./etc/crypttab /etc/crypttab
+    cp -fv ./etc/mdadm/mdadm.conf /etc/mdadm/mdadm.conf
+    cp -fv ./etc/sudoers /etc/sudoers
+    update-initramfs -u
 fi
 
-apt-get update
-apt-get upgrade -y
-apt-get purge -y totem totem-plugins
-apt-get install -y \
-    bcmwl-kernel-source \
-    binutils \
-    cryptsetup \
-    curl \
-    dkms \
-    git \
-    htop \
-    kdiff3 \
-    mdadm \
-    mosh \
-    net-tools \
-    pm-utils \
-    realpath  \
-    slurm \
-    smplayer \
-    ssh \
-    sshfs \
-    tmux \
-    vim-gnome
-modprobe wl
-cp -fv ./etc/NetworkManager/system-connections/dd-wrt /etc/NetworkManager/system-connections/dd-wrt
-cp -fv ./etc/crypttab /etc/crypttab
-cp -fv ./etc/mdadm/mdadm.conf /etc/mdadm/mdadm.conf
-cp -fv ./etc/sudoers /etc/sudoers
-update-initramfs -u
 ./deploy_links.sh
 ./submodule_setup.sh
 ./zerotier_install.sh
