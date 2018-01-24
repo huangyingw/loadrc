0592089bd428028619e5fd4ef3c4ffe599b34c58
Ying Huang <yhuang@imvu.com>
2018-01-23 18:03:11 -0800

    n

--- gitrc/gci.sh
+++ gitrc/gci.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git config --local include.path $(realpath .gitconfig)
+~/loadrc/gitrc/include_gitconfig.sh
 . ~/loadrc/gitrc/getCommit_message.sh "$1"
 git commit -am "$commit_message"
 ~/loadrc/gitrc/postCommit.sh
