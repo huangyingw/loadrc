108254e84488bc64ab7c65a9b0080a3537456d41
Ying Huang <yhuang@imvu.com>
2018-01-26 15:28:40 -0800

    n

--- gitrc/gme.sh
+++ gitrc/gme.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ~/loadrc/gitrc/include_gitconfig.sh
-if [ $(git config merge.allow) != "true" ]
+if [ "$(git config merge.allow)" != "true" ]
 then
     echo -e "${red}git merge is not allowed, without git config merge.allow is true ... ${NC}"
     exit 1
