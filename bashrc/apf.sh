88586b7d2afa4637497c69caa5358c5145e015e1
Ying Huang <yhuang@imvu.com>
2018-02-16 23:41:47 -0800

    n

--- bashrc/apf.sh
+++ bashrc/apf.sh
@@ -5,6 +5,7 @@ cd "$SCRIPTPATH"
 
 if [ $(uname) != "Darwin" ]
 then
+    /usr/NX/scripts/setup/nxserver --update debian
     apt-get update && aptitude -y full-upgrade
 else
     brew update ; brew upgrade
