74372e52bae455d818b0f362db89658935d709f3
Ying Huang <yhuang@imvu.com>
2018-03-10 21:42:29 -0800

    n

--- bashrc/fav.sh
+++ bashrc/fav.sh
@@ -1,23 +1,8 @@
 #! /bin/bash
-if [ `hostname` == "mbuntu" ]
-then
-    if [ -f $HOME/loadrc/.passwd ]
-    then
-        FAV=`cat $HOME/loadrc/.passwd |grep fav|awk '{print $3}'`
-    fi
-    rm ~/fav/*
-    find "$FAV" -type f -mtime -700 -iname 9\* -size +600M -exec ls -rt {} \+|tail -n 100|while read ss
+FAV=/home/parallels/movie/media/volgrp/av/
+rm ~/fav/*
+find "$FAV" -type f -mtime -700 -iname 9\* -size +600M -exec ls -rt {} \+|tail -n 100|while read ss
 do
     ftemp=`basename "$ss"`
     ln -s "$ss" ~/fav/"$ftemp"
 done
-else
-    if [ -z "$1" ]
-    then
-        FAV=`cat $HOME/loadrc/.passwd |grep fav|awk '{print $3}'`
-    else
-        FAV="$1"
-    fi
-    find "$FAV" -type f -mtime -300 -iname 9\* -exec ls -t {} \+ > fav.findresult
-    vi fav.findresult
-fi
