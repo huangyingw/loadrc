05e861d98b1408cc1b1661b01e5f66b820039fef
Ying Huang <yhuang@imvu.com>
2018-03-15 10:46:01 -0700

    n

--- gitrc/gbis.sh
+++ gitrc/gbis.sh
@@ -1,23 +1,3 @@
 #!/bin/bash - 
-#===============================================================================
-#
-#          FILE: gbis.sh
-# 
-#         USAGE: ./gbis.sh 
-# 
-#   DESCRIPTION: 
-# 
-#       OPTIONS: ---
-#  REQUIREMENTS: ---
-#          BUGS: ---
-#         NOTES: ---
-#        AUTHOR: YOUR NAME (), 
-#  ORGANIZATION: 
-#       CREATED: 04/16/2016 10:40
-#      REVISION:  ---
-#===============================================================================
-
-set -o nounset                              # Treat unset variables as an error
-
 git bisect skip
 git bisect log | tee gbil.findresult
