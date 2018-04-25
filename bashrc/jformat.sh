662bc84a22b7358c11823dbf2fc5630a6ce7c4a9
Ying Huang <yhuang@imvu.com>
2018-04-25 14:43:28 -0700

    n

--- bashrc/jformat.sh
+++ bashrc/jformat.sh
@@ -5,5 +5,5 @@ if [ -f $HOME/.workrc ]
 then
   find "$folderForGit" -type f \( "${LIST[@]}" \) -exec astyle --mode=java -nfxejoU -y  --max-code-length=150 -s2 --style=java -pcH {} \;
 else
-  find "$folderForGit" -type f \( "${LIST[@]}" \) -exec astyle --mode=java -nfxejoU -y  --max-code-length=150 -s2 --style=allman -pcH {} \;
+  find "$folderForGit" -type f \( "${LIST[@]}" \) -exec astyle --mode=java -nfxejoUys4pHU --max-code-length=150 --style=allman -pcH {} \;
 fi
