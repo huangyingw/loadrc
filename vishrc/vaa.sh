67c6e021f520a7bbd02eff246b23d342587580d0
Ying Huang <yhuang@imvu.com>
2018-05-05 14:34:38 -0700

    n

--- vishrc/vaa.sh
+++ vishrc/vaa.sh
@@ -5,6 +5,20 @@ echo "search in " "$1"
 cd "$1"
 find_result="$3".vaa.findresult
 echo "find_result --> $find_result"
+if [ -f "$find_result" ]
+then
+    read -p "the search is already done, if you want to update, press u --> " update
+    case $update in
+        u)
+            ;;
+        "")
+            exit 1
+            ;;
+        ?)
+            exit 1
+            ;;
+    esac
+fi
 cscope_db_file=files.proj
 echo "result in " "$1/$find_result"
 xargs grep -inH -- "$keyword" < "$cscope_db_file" > "$find_result"
