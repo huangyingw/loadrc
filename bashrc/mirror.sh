0225cdf6ecec08e463f943243b0381e5505cc684
Ying Huang <yhuang@imvu.com>
2018-05-13 16:30:41 -0700

    n

--- bashrc/mirror.sh
+++ bashrc/mirror.sh
@@ -11,6 +11,12 @@ then
     exit 1
 fi
 
+if [ -z "$SOURCE" ]
+then
+    echo -e "${red}SOURCE could not be none... ${NC}"
+    exit 1
+fi
+
 if [ -z "$TARGET" ]
 then
     echo -e "${red}TARGET could not be none... ${NC}"
