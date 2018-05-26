99684c0ee8f77d0c563771b5745d7330101426dd
Ying Huang <yhuang@imvu.com>
2018-05-25 18:46:14 -0700

    n

--- bashrc/fav.sh
+++ bashrc/fav.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 FAV=/home/parallels/movie/media/volgrp/av/
+rm ~/fav/*
 find "$FAV" -type f -mtime -700 -iname 9\* -size +600M -exec ls -rt {} \+|tail -n 100|while read ss
 do
     ftemp=`basename "$ss"`
