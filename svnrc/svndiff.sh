b9432cd24902589d8b275925121978b198490949
Ying Huang <yhuang@imvu.com>
2018-06-02 13:33:00 -0700

    n

--- svnrc/svndiff.sh
+++ svnrc/svndiff.sh
@@ -2,7 +2,7 @@
 fullfile="$1"
 if [ $(basename "$fullfile") == "index" ]
 then
-    svn diff -w --cl utest > utest.diff
+    svn diff -x -w --cl utest > utest.diff
 else
     svn diff --diff-cmd kdiff3 "$fullfile"
 fi
