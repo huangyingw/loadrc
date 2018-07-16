02ed3d3b59ffa86861a5a6f49320793e572bfc39
Ying Huang <yhuang@imvu.com>
2018-07-16 15:17:54 -0700

    n

--- gitrc/gps.sh
+++ gitrc/gps.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 currentBranch=$(~/loadrc/gitrc/get_current_branch.sh)
+
 if ( echo $currentBranch | grep -q 'fix$' )
 then
     exit 1
 fi
 
 ~/loadrc/gitrc/gpl.sh
-
 ~/loadrc/gitrc/include_gitconfig.sh
+
 if [[ $(git config --get-all push.remote) ]]
 then
     for ss in $(git config --get-all push.remote)
     do
         git push -v "$ss" && git push -v --tag "$ss"
     done
-else
-    for ss in `git remote -v |awk '/\(fetch\)$/{print $1}'`; do git push -v "$ss" && git push -v --tag "$ss"; done
 fi
 
 git push -v BareReps && git push -v --tag BareReps
