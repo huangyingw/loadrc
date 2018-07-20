07069bac89a7fdea6751b43fd6a07417bdff7b87
Ying Huang <yhuang@imvu.com>
2018-07-20 14:22:55 -0700

    n

--- svnrc/svnup.sh
+++ svnrc/svnup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-if (git status "$folderForGit"|grep -q 'nothing to commit')
+if !(git status "$folderForGit" | grep -q 'nothing to commit')
 then
-    svn up | tee svnup.findresult
-    git st
-else
     echo -e "${red}the git repository is unclean, please check it before continuing... ${NC}"
     exit 1
 fi
+
+svn up | tee svnup.findresult
+git st
