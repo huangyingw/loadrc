119e2b3e62bf79914ead4f6b17d4530039b2186f
Ying Huang <yhuang@imvu.com>
2018-07-22 21:20:34 -0700

    n

--- vishrc/vsearch.sh
+++ vishrc/vsearch.sh
@@ -2,6 +2,7 @@
 echo "search in " "$1"
 cd "$1"
 find_result="$1/""`echo "$2"."$4".findresult |sed  -e "s/\//\_/g;s/\ /\_/g"`"
+
 if [ -f "$find_result" ]
 then
     read -p "the search is already done, if you want to update, press u --> " update
@@ -16,5 +17,12 @@ then
             ;;
     esac
 fi
+
 echo "result in " "$find_result"
-cscope -dL -f cscope.out -"$3""$2" > "$find_result" 
+
+if [[ "$3" == "w" ]]
+then
+    xargs grep -wnH "$2" < files.proj > "$find_result"
+else
+    cscope -dL -f cscope.out -"$3""$2" > "$find_result"
+fi
