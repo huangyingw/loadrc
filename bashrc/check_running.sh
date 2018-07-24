db4f1ace6b3994288f4fd68e3cdde963e21fca50
Ying Huang <yhuang@imvu.com>
2018-07-24 11:25:31 -0700

    n

--- bashrc/check_running.sh
+++ bashrc/check_running.sh
@@ -1,12 +1,5 @@
 #! /bin/bash
 
-RSYNCSOURCE=rsync://mirrors.sohu.com/ubuntu/
-#RSYNCSOURCE=rsync://debian.nctu.edu.tw/ubuntu/
-#RSYNCSOURCE=rsync://ubuntu.dormforce.net/ubuntu/
-#RSYNCSOURCE=rsync://mirror.anl.gov/ubuntu/
-
-BASEDIR=/media/volgrp/mirror/
-
 # check to see if script is already running
 PDIR=${0%`basename $0`}
 LCK_FILE=`basename $0`.lck
@@ -22,10 +15,7 @@ then
         # The process is not running
         # Echo current PID into lock file
         echo $$ > "${LCK_FILE}"
-        rsync -ahHv --log-file=/root/rlog --delete-after \
-            --exclude "*dapper*" --exclude "*hardy*" --exclude "*intrepid*" --exclude "*jaunty*" \
-            --exclude "*powerpc*" --exclude "*sparc*" \
-            ${RSYNCSOURCE} ${BASEDIR}
+        ./submission.sh
     else
         # the process IS running
         # handle it
