80029c37ae9383cfa6eec55f4e19629775ecf61a
Ying Huang <yhuang@imvu.com>
2018-08-07 22:25:54 -0700

    n

--- setup.sh
+++ setup.sh
@@ -6,7 +6,6 @@ cd "$SCRIPTPATH"
 ~/loadrc/gitrc/gpl.sh
 ./install_basic.sh
 ./install_nvim.sh
-./cscope-15.9/build.sh
 ./submodule_setup.sh
 ./vimrc/.vim/bundle/vim-ghost/install.sh
 ./kvmrc/install.sh
