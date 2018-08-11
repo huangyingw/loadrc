164f35dc35a11f50dc024b2a84f7371bb4afbb75
Ying Huang <yhuang@imvu.com>
2018-08-10 22:00:09 -0700

    n

--- gitrc/gdev.sh
+++ gitrc/gdev.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -
 touch files.proj \
+    && git config --file .gitconfig commit.message n \
     && git config --file .gitconfig merge.allow true \
-    && git config --file .gitconfig commit.message n
+    && git config --file .gitconfig vrun.silent false
