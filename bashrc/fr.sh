97318b1b1473e4da39b2fd6741e96a39c24102fe
Ying Huang <yhuang@imvu.com>
2018-08-18 11:25:00 -0700

    n

--- bashrc/fr.sh
+++ bashrc/fr.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 ~/loadrc/gitrc/checkGitStatus.sh
+
 if [ $? -ne 0 ]
 then
     exit 1
 fi
+
 FIND=$(echo $1 | sed 's/\//\\\//g')
 REPLACE=$(echo $2 | sed 's/\//\\\//g')
-cscope_db_file="/export/home1/username/cscope_db/""${pwd##*/}"
-xargs sed -i""  "s/\b${FIND}\b/${REPLACE}/g" < "$cscope_db_file"
+xargs sed -i""  "s/\b${FIND}\b/${REPLACE}/g" < files.proj
