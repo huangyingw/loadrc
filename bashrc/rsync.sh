a1f727c0cf323a6379717dfe730f472c0f9427c8
Ying Huang <yhuang@imvu.com>
2018-08-21 21:34:57 -0700

    n

--- bashrc/rsync.sh
+++ bashrc/rsync.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -
+cat files.proj | sed 's/^"//g;s/"$//g' > files.proj.tmp
 rsync -aHv --force --progress \
-    --exclude-from="excludeFile" \
+    --files-from=files.proj.tmp \
     "$1" \
     "$2"
