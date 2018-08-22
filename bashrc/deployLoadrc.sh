74cdf9eafc2fa0207362febc2c6393f1e2d30bb5
Ying Huang <yhuang@imvu.com>
2018-08-21 21:44:15 -0700

    n

--- bashrc/deployLoadrc.sh
+++ bashrc/deployLoadrc.sh
@@ -6,14 +6,12 @@ cd "$SCRIPTPATH"
 realDeploy () {
     server="$1"
     echo "Begin loadrc deploy on $server"
-    ./rsync.sh ~/loadrc/ "$server":~/loadrc/
-    ./rsync.sh ~/.tmux.conf "$server":~/.tmux.conf
+    ./rsync.sh "$server":~/loadrc/
     ssh -n "$server" "sudo apt-get update && sudo apt-get install -y vim-gnome cscope git tmux mosh htop realpath"
     ssh -n "$server" "ln -fs ~/loadrc/vimrc/.vimrc ~/.vimrc"
     ssh -n "$server" "ln -fs ~/loadrc/vimrc/.vim ~/.vim"
     ssh -n "$server" "ln -fs ~/loadrc/gitrc/.gitconfig ~/.gitconfig"
     ssh -n "$server" "ln -fs ~/loadrc/gitrc/.globalgitignore ~/.globalgitignore"
-    ssh -n "$server" "~/loadrc/bashrc/osmirror_test.sh"
 }
 
 if [ -n "$1" ]
--- bashrc/rsync.sh
+++ bashrc/rsync.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -
+. ~/loadrc/bashrc/find_up_goto.sh files.proj
 cat files.proj | sed 's/^"//g;s/"$//g' > files.proj.tmp
 rsync -aHv --force --progress \
     --files-from=files.proj.tmp \
-    "$1" \
-    "$2"
+    . \
+    "$1"
