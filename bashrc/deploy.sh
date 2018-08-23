1760f3f21a6763b1e5080debb60df66dc2a90bbf
Ying Huang <yhuang@imvu.com>
2018-08-23 11:22:08 -0700

    n

--- bashrc/deploy.sh
+++ bashrc/deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash -
-SCRIPT=$(realpath "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
-cd "$SCRIPTPATH"
 
 for ss in $(git config --get-all deploy.target)
 do
-    ./rsync.sh "$ss"
+    ~/loadrc/bashrc/rsync.sh "$ss"
 done
