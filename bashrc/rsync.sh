5138280393bd84ba3a9f3c29a5ca36fc5b66486c
Ying Huang <yhuang@imvu.com>
2018-08-24 23:08:28 -0700

    n

--- bashrc/rsync.sh
+++ bashrc/rsync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -
 . ~/loadrc/bashrc/find_up_goto.sh files.proj
-cat files.proj | sed 's/^"//g;s/"$//g' > files.proj.tmp
+cat files.proj.bak | sed 's/^"//g;s/"$//g' > files.proj.tmp
 rsync -aHv --force --progress \
     --files-from=files.proj.tmp \
     . \
