e88df51dca4b342ff6053b095d4a0803cf8956a5
Ying Huang <yhuang@imvu.com>
2018-08-25 21:49:49 -0700

    n

--- vishrc/vrun.sh
+++ vishrc/vrun.sh
@@ -23,8 +23,7 @@ case $extension in
         bash ${1} 2>&1 | tee ${1}.findresult
         ;;
     sh)
-        bash ${1} 2>&1 | tee ${1}.findresult.bak
-        cp ${1}.findresult.bak ${1}.findresult
+        bash ${1} 2>&1 | tee ${1}.findresult
         ;;
     py)
         SCRIPT=$(realpath "$1")
