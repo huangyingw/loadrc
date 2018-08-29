2e8e6d4be33c7cef1a0fac41c97c5f5bd25ffea9
Ying Huang <yhuang@imvu.com>
2018-08-28 21:53:50 -0700

    n

--- gitrc/gdev.sh
+++ gitrc/gdev.sh
@@ -2,4 +2,5 @@
 touch files.proj \
     && git config --file .gitconfig commit.message n \
     && git config --file .gitconfig merge.allow true \
-    && git config --file .gitconfig vrun.silent false
+    && git config --file .gitconfig vrun.silent false \
+    && git config --file .gitconfig vrun.async false
