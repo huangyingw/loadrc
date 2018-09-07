5949018618beeb25bbca5cd0ffd3a6189132e56a
Ying Huang <yhuang@imvu.com>
2018-09-06 22:45:12 -0700

    n

--- bashrc/find_projects.sh
+++ bashrc/find_projects.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -
 if [ -z "$1" ]
 then
-    find . -type f -name files.proj -exec realpath {} \; | sed 's/\(["'\''\]\)/\\\1/g;s/ /\\ /g;s/.*/"&"/'
+    find . -type f -name files.proj -exec realpath {} \; | sed 's/\(["'\''\]\)/\\\1/g;s/ /\\ /g;s/.*/"&"/' >> ~/all.proj
 else
-    find "$1" -type f -name files.proj -exec realpath {} \; | sed 's/\(["'\''\]\)/\\\1/g;s/ /\\ /g;s/.*/"&"/'
+    find "$1" -type f -name files.proj -exec realpath {} \; | sed 's/\(["'\''\]\)/\\\1/g;s/ /\\ /g;s/.*/"&"/' >> ~/all.proj
 fi
 
 sort -u ~/all.proj -o ~/all.proj
