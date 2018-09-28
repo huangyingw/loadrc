08d9112e7d769183356a35192ffdd0960da85df2
Ying Huang <yhuang@imvu.com>
2018-09-28 01:16:33 -0700

    n

--- bashrc/fav.sh
+++ bashrc/fav.sh
@@ -1,22 +1,13 @@
 #! /bin/bash
 if [ -z "$1" ]
 then
-    FAV=/home/parallels/movie/media/volgrp/av/
+    FAV=~/var/lib/mldonkey/incoming/files/
 else
     FAV=`realpath "$1"`
 fi
 
-rm ~/fav/*
-find "$FAV" -type f -mtime -700 -iname 9\* -size +600M -exec ls -rt {} \+|tail -n 100|while read ss
-do
-    ftemp=`basename "$ss"`
-    ln -s "$ss" ~/fav/"$ftemp"
-done
+cd "$FAV"
 
-FAV=~/var/lib/mldonkey/incoming/files/
-
-find "$FAV" -type f -mtime -700 -iname 9\* -size +600M -exec ls -rt {} \+|tail -n 100|while read ss
-do
-    ftemp=`basename "$ss"`
-    ln -s "$ss" ~/fav/"$ftemp"
-done
+find . -type f -size +100M -exec ls -t {} \+ > fav.log
+touch files.proj
+~/loadrc/bashrc/update_proj.sh
