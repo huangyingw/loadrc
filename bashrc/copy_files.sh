25930a354f92f5e146bf4bfa8fa21793bbc409b3
Ying Huang <yhuang@imvu.com>
2018-09-30 05:28:15 -0700

    n

--- bashrc/copy_files.sh
+++ bashrc/copy_files.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -
-~/loadrc/bashrc/copy.sh ~/var/lib/mldonkey/incoming/files/ mini:/media/mini_crypt/var/lib/mldonkey/incoming/files/ 2>&1 | tee copy_files.log
+~/loadrc/bashrc/copy.sh ~/var/lib/mldonkey/incoming/files/ mini:/media/mini_crypt/var/lib/mldonkey/incoming/files/ 2>&1 | tee ~/loadrc/bashrc/copy_files.log
