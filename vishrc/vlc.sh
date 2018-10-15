8c980b414056713d5201e7a32d2603fe7713a11d
Ying Huang <yhuang@imvu.com>
2018-10-14 22:03:34 -0700

    n

--- vishrc/vlc.sh
+++ vishrc/vlc.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -
+SCRIPT=$(realpath "$1")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd "$SCRIPTPATH"
 IFS=$'\r\n' GLOBIGNORE='*' command eval  'LINES=($(cat $1))'
 /Applications/VLC.app/Contents/MacOS/VLC "${LINES[@]}"
