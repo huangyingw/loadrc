2dda2fd9da8b50e226fbda374673ca7050cee74e
Ying Huang <yhuang@imvu.com>
2018-10-16 17:00:28 -0700

    n

--- mdadmrc/add.sh
+++ mdadmrc/add.sh
@@ -2,11 +2,15 @@
 #mdadm: at least 4 raid-devices needed for level 6
 mdadm --create --verbose /dev/md0 --level=6 --raid-devices=4 /dev/sd[b-d] --spare-devices=1 /dev/sde1
 cryptsetup luksAddKey /dev/md0 /root/loadrc/keys/keyfile
+
+#reactivate array
 mdadm --stop /dev/md0
 mdadm --assemble --force /dev/md0 /dev/sd[abcd]1
+
 mdadm --manage /dev/md0 --run
 mdadm --manage /dev/md0 --add /dev/sd[c-d]
 mdadm --grow /dev/md0 --size=max
+
 reboot
 e2fsck -f /dev/mapper/md0_crypt
 resize2fs /dev/mapper/md0_crypt
