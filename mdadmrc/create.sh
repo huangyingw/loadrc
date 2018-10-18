9549bec691dc0bb127b2fd4178a949c648f35f75
Ying Huang <yhuang@imvu.com>
2018-10-18 13:09:25 -0700

    n

--- mdadmrc/create.sh
+++ mdadmrc/create.sh
@@ -3,3 +3,5 @@
 mdadm --create --verbose /dev/md0 --level=6 --raid-devices=4 /dev/sd[b-e]1
 cryptsetup luksAddKey /dev/md0 /root/loadrc/keys/keyfile
 mdadm --detail --scan >> /etc/mdadm/mdadm.conf
+update-initramfs -u
+reboot
