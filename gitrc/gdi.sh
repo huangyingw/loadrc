1c93ac73010e5fd6dc74ecb5cbb14e8678809a07
Ying Huang <yhuang@imvu.com>
2018-10-19 11:20:29 -0700

    n

--- gitrc/gdi.sh
+++ gitrc/gdi.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-git diff -w --cached HEAD | tee gdi.findresult
+if [ -z "$1" ]
+then
+    git diff -w --cached HEAD | tee gdi.findresult
+else
+    COMMAND="git diff -w --cached $1"
+
+    for ss in $(git config --get-all gdif.ignore)
+    do
+        COMMAND="$COMMAND  ':(exclude)$ss'"
+    done
+
+    eval "$COMMAND" | tee gdi.findresult
+fi
