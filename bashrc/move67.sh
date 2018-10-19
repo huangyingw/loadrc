666d69e46d1b0d15526dcf0b535dea3ec13cdc0b
Ying Huang <yhuang@imvu.com>
2018-10-19 15:09:28 -0700

    n

--- bashrc/move67.sh
+++ bashrc/move67.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -
-find /media/volgrp/av/ -type f -name 6\*|while read ss; do dir=$(echo "$ss" | sed 's/volgrp/volgrp\/archive/g'); mkdir -p "$(dirname "$dir")";mv -v "$ss" "$(dirname "$dir")"; done
-find /media/volgrp/av/ -type f -name 7\*|while read ss; do dir=$(echo "$ss" | sed 's/volgrp/volgrp\/archive/g'); mkdir -p "$(dirname "$dir")";mv -v "$ss" "$(dirname "$dir")"; done
+find /media/volgrp/mirror/av/ -type f -name 6\*|while read ss; do dir=$(echo "$ss" | sed 's/\/mirror\//\/archive\//g'); mkdir -p "$(dirname "$dir")";mv -v "$ss" "$(dirname "$dir")"; done
+find /media/volgrp/mirror/av/ -type f -name 7\*|while read ss; do dir=$(echo "$ss" | sed 's/\/mirror\//\/archive\//g'); mkdir -p "$(dirname "$dir")";mv -v "$ss" "$(dirname "$dir")"; done
