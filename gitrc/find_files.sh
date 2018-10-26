e9ed9307676e312c1d941158c13be89f15ab7c3b
Ying Huang <yhuang@imvu.com>
2018-10-26 13:18:11 -0700

    n

--- gitrc/find_files.sh
+++ gitrc/find_files.sh
@@ -5,7 +5,7 @@ while IFS= read -r line; do
     my_array+=( "$line" )
 done < <( grep  $1 files.proj | sed -e 's/"//g' )
 
-output=$(echo "$1" | sed 's/\//_/g;s/ //g')
+output="$(echo "$1" | sed 's/\//_/g;s/ //g').findresult"
 
 if [ ${#my_array[@]} -eq 1 ]; then
     echo ${my_array[0]}
