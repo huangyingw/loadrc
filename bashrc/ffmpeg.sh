1ec42c93f8b1732dbb37710b7836fb0704e431a6
Ying Huang <yhuang@imvu.com>
2018-10-26 22:00:08 -0700

    n

--- bashrc/ffmpeg.sh
+++ bashrc/ffmpeg.sh
@@ -12,3 +12,15 @@ do
         ffmpeg -n -ss "$start" -to "$end" -i "$input" "$output"
 done < "$ss"
 done
+
+
+
+#Create a file mylist.txt with all the files you want to have concatenated in the following form (lines starting with a # are ignored):
+#
+## this is a comment
+#file '/path/to/file1'
+#file '/path/to/file2'
+#file '/path/to/file3'
+#Note that these can be either relative or absolute paths. Then you can stream copy or re-encode your files:
+#
+#ffmpeg -f concat -safe 0 -i mylist.txt -c copy output
