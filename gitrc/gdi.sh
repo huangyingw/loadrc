af8ceb473f63f205bfca6b31e9ecc818d5f834ae
Ying Huang <yhuang@imvu.com>
2018-10-30 13:00:59 -0700

    n

--- gitrc/gdi.sh
+++ gitrc/gdi.sh
@@ -3,7 +3,7 @@ if [ -z "$1" ]
 then
     git diff -w --cached HEAD
 else
-    COMMAND="git diff -w --cached $1"
+    COMMAND="git diff -w $1"
 
     for ss in $(git config --get-all gdif.ignore)
     do
