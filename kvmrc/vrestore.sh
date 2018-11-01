2bc2fc61ce2660435a1e7854f75ab85cca57b613
Ying Huang <yhuang@imvu.com>
2018-10-31 22:21:10 -0700

    n

--- kvmrc/vrestore.sh
+++ kvmrc/vrestore.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ -z "$1" ]
 then
-    find /media/volgrp/kvm/ -type f -maxdepth 2 -name \*.save|while read ss; do virsh restore "$ss" && rm -v "$ss"; done
+    find ~/kvm/ -type f -maxdepth 2 -name \*.save|while read ss; do virsh restore "$ss" && rm -v "$ss"; done
 else
-    virsh restore /media/volgrp/kvm/"$1"/"$1".save \
-        && rm -v /media/volgrp/kvm/"$1"/"$1".save
+    virsh restore ~/kvm/"$1"/"$1".save \
+        && rm -v ~/kvm/"$1"/"$1".save
 fi
--- kvmrc/vsave.sh
+++ kvmrc/vsave.sh
@@ -2,7 +2,7 @@
 $HOME/loadrc/kvmrc/vresume.sh
 if [ -z "$1" ]
 then
-    virsh list|awk '/running/{print $2}'|while read ss; do virsh save "$ss" /media/volgrp/kvm/"$ss"/$ss.save --verbose; done
+    virsh list|awk '/running/{print $2}'|while read ss; do virsh save "$ss" ~/kvm/"$ss"/$ss.save --verbose; done
 else
-    virsh save "$1" /media/volgrp/kvm/"$1"/"$1".save --verbose
+    virsh save "$1" ~/kvm/"$1"/"$1".save --verbose
 fi
