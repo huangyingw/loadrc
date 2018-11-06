b7bc65ff63a245ecf110bc5004eee54724ac2eb2
Ying Huang <yhuang@imvu.com>
2018-11-06 10:49:28 -0800

    n

--- gitrc/gdi.sh
+++ gitrc/gdi.sh
@@ -3,12 +3,17 @@ if [ -z "$1" ]
 then
     git diff -w --cached HEAD
 else
-    COMMAND="git diff -w $1"
+    if [ -z "$2" ]
+    then
+        COMMAND="git diff -w $1"
 
-    for ss in $(git config --get-all gdif.ignore)
-    do
-        COMMAND="$COMMAND  ':(exclude)$ss'"
-    done
+        for ss in $(git config --get-all gdif.ignore)
+        do
+            COMMAND="$COMMAND  ':(exclude)$ss'"
+        done
 
-    eval "$COMMAND"
+        eval "$COMMAND"
+    else
+        git diff -w "$1" "$2"
+    fi
 fi
