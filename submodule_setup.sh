eb3f26b89c5add36af32f438b39d5559b5b157fc
Ying Huang <yhuang@imvu.com>
2018-11-10 11:15:02 -0800

    n

--- submodule_setup.sh
+++ submodule_setup.sh
@@ -5,11 +5,6 @@ cd "$SCRIPTPATH"
 
 git submodule init
 git submodule sync
-
-if [ $(uname) != "Darwin" ]
-then
-    git submodule update
-fi
-
+git submodule update
 git submodule foreach ~/loadrc/gitrc/gps.sh
 git submodule foreach ~/loadrc/gitrc/gsync.sh
