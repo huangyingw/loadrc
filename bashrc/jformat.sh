a9db20ac82164428735389ccea88a59a2a50c7ba
Ying Huang <yhuang@imvu.com>
2018-11-12 10:37:31 -0800

    n

--- bashrc/jformat.sh
+++ bashrc/jformat.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 folderForGit=$(realpath .)
 LIST=( -name \*.java -o -name \*.vala )
+
 if [ -f $HOME/.workrc ]
 then
-  find "$folderForGit" -type f \( "${LIST[@]}" \) -exec astyle --mode=java -nfxejoU -y -s2 --style=java -pcH {} \;
+    find "$folderForGit" -type f \( "${LIST[@]}" \) -exec astyle --mode=java -nfxejoU -y -s2 --style=java -pcH {} \;
 else
-  find "$folderForGit" -type f \( "${LIST[@]}" \) -exec astyle --mode=java -nfxejoUys4pHU --style=allman -pcH {} \;
+    find "$folderForGit" -type f \( "${LIST[@]}" \) -exec astyle --mode=java -nfxejoUys4pHU --style=allman -pcH {} \;
 fi
