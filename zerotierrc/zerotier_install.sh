5cb50b0dc6bc0c9b5fbcb955e11bb39e63457cea
Ying Huang <yhuang@imvu.com>
2018-11-17 08:09:43 -0800

    n

--- zerotierrc/zerotier_install.sh
+++ zerotierrc/zerotier_install.sh
@@ -10,3 +10,18 @@ fi
 
 curl -s https://install.zerotier.com/ | bash
 zerotier-cli join 93afae5963560e41
+
+deploy_configs() {
+    TARGET="$1"
+    SOURCE=~/loadrc/"`hostname`$TARGET"
+
+    if [ -f "$SOURCE" ]
+    then
+        sudo cp -fv "$SOURCE" "$TARGET"
+    fi
+}
+
+while read -r line || [[ -n "$line" ]]
+do
+    deploy_configs "$line"
+done < zerotierrc.conf
