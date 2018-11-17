4d4f55371250c846a8cae3028e53b606cdc63a5f
Ying Huang <yhuang@imvu.com>
2018-11-17 09:04:28 -0800

    n

--- bashrc/rsyncFiles.sh
+++ bashrc/rsyncFiles.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -
+rsync -aHv --force --delete --progress \
+    "$1"/.git/ \
+    "$2"/.git/
+
 cat files.proj | sed 's/^"//g;s/"$//g;s/\\ / /g' > files.proj.tmp && \
     rsync -aHv --force --progress \
     --files-from files.proj.tmp \
