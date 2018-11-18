90c6cf19319cfd9735a75d6cc2cabcadb8c9d69f
Ying Huang <yhuang@imvu.com>
2018-11-17 21:12:30 -0800

    n

--- bashrc/deploy.sh
+++ bashrc/deploy.sh
@@ -6,5 +6,6 @@ path=$(git config deploy.path)
 if [[ -n "$host" ]] && [[ -n "$path" ]]
 then
     . ~/loadrc/bashrc/find_up_goto.sh files.proj
+    ssh -n "$host" "mkdir -p $path"
     . ~/loadrc/bashrc/rsyncFiles.sh . "$host:$path"
 fi
