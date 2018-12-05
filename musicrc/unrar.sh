7560fb3298dbe2941819333ecbb64c2be2306626
Ying Huang <yhuang@imvu.com>
2018-12-05 07:26:17 -0800

    n

---
+++ musicrc/unrar.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -
+
+if [ -z "$1" ]
+then
+    echo -e "${red}Please provide the target folder ... ${NC}"
+    exit 1
+fi
+
+find "$1" -type f -iname \*.rar | \
+    while read ss \
+    do ss="$(realpath "$ss")" && \
+        cd "$(dirname "$ss")" && \
+        unrar x -r -o- "$ss" && \
+        rm "$ss" \
+    done
