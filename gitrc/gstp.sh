3e6a728b51edef37a4ff001f1e2a7f1187b7e484
Ying Huang <yhuang@imvu.com>
2018-12-12 07:47:26 -0800

    n

--- gitrc/gstp.sh
+++ gitrc/gstp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -z "$1" ]
 then
-    echo -e "${red}please specify the stash index ... ${NC}"
-    exit 1
+    git stash show -p stash@{0} | git apply && git stash drop stash@{0}
+else
+    git stash show -p stash@{"$1"} | git apply && git stash drop stash@{"$1"}
 fi
-git stash show -p stash@{"$1"} | git apply && git stash drop stash@{"$1"}
