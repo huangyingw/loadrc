0a6e256b29aa81754526c88cdcd6f4de89fcae35
Ying Huang <yhuang@imvu.com>
2019-01-13 16:06:15 -0800

    n

--- bashrc/rsyncFiles.sh
+++ bashrc/rsyncFiles.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -
-cat files.proj | sed 's/^"//g;s/"$//g;s/\\ / /g' > files.proj.tmp && \
-    rsync -aHv --force --progress \
+rsync -aHv --force --progress \
     --files-from files.proj.tmp \
     "$1" \
     "$2"
