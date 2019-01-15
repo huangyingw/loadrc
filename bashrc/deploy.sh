6b27ed674a4ef078ea6573c7cfbf7ca18340fae8
Ying Huang <yhuang@imvu.com>
2019-01-14 20:13:25 -0800

    n

--- bashrc/deploy.sh
+++ bashrc/deploy.sh
@@ -3,6 +3,11 @@
 host=$(git config deploy.host)
 path=$(git config deploy.path)
 
+if [ "$host" == "localhost" ]
+then
+    return
+fi
+
 if [[ -n "$host" ]] && [[ -n "$path" ]]
 then
     . ~/loadrc/bashrc/find_up_goto.sh files.proj
