996bc068d144e8b6f335ce1a328b4d566610fad1
Ying Huang <yhuang@imvu.com>
2019-01-19 11:21:45 -0800

    n

--- gitrc/gme2.sh
+++ gitrc/gme2.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
 CURRENT_BRANCH=$(~/loadrc/gitrc/get_current_branch.sh)
+
 if [ -z "$1" ]
 then
     echo -e "${red}Please provide target_branch... ${NC}"
     exit 1
 fi
 
-TARGET_BRANCH="$1"
-echo "$TARGET_BRANCH"
-
-if [ -z "$(git status --porcelain)" ]
-then
-    git checkout "$TARGET_BRANCH" \
-        && ~/loadrc/gitrc/gme.sh "$CURRENT_BRANCH" \
-        ; git checkout "$CURRENT_BRANCH"
-else
-    git add . \
-        && git stash|grep -q "HEAD is now at" \
-        && git checkout "$TARGET_BRANCH" \
-        && ~/loadrc/gitrc/gme.sh "$CURRENT_BRANCH" \
-        ; git checkout "$CURRENT_BRANCH" \
-        && git stash pop stash@{0} \
-        && git reset HEAD .
-fi
+git fetch . "$CURRENT_BRANCH":"$1"
