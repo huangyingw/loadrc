a630ed737987d4972cb24f17dfbde34f03625932
Ying Huang <yhuang@imvu.com>
2019-01-28 15:08:01 -0800

    n

--- gitrc/gcom.sh
+++ gitrc/gcom.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
-old_branch=$(~/loadrc/gitrc/get_current_branch.sh)
-echo $old_branch
-
-if [ -z "$(git status --porcelain)" ]
-then
-    git checkout "$1" \
-        && ~/loadrc/gitrc/gme.sh "$old_branch"
-else
-    git add . && \
-        git stash | grep -q "HEAD is now at" && \
-        git checkout "$1" && \
-        ~/loadrc/gitrc/gme.sh "$old_branch" && \
-        git stash pop stash@{0} && \
-        git reset HEAD .
-fi
+~/loadrc/gitrc/gme2.sh "$1" && \
+    git checkout "$1"
