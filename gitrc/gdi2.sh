7a866b0535c7fbdb08d2c5a4e98c03cf3a1a2197
Ying Huang <yhuang@imvu.com>
2019-01-30 14:08:53 -0800

    n

--- gitrc/gdi2.sh
+++ gitrc/gdi2.sh
@@ -4,7 +4,14 @@ then
     echo -e "${red}Please provide branch name ... ${NC}"
 fi
 
+if [ -z $(git config gsync.remote) ]
+then
+    echo -e "${red}gsync.remote is not configured ... ${NC}"
+fi
+
 branch="$1"
+git branch -D "$branch"
+git branch "$branch" $(git config gsync.remote)"/"$(git config gsync.branch)
 ~/loadrc/gitrc/gdi.sh "$branch"  2>&1 | tee gdi.findresult && \
     git co "$branch" && \
     ~/loadrc/gitrc/gsync.sh && \
--- gitrc/gsync.sh
+++ gitrc/gsync.sh
@@ -2,6 +2,7 @@
 currentBranch=$(~/loadrc/gitrc/get_current_branch.sh)
 git co $currentBranch
 ~/loadrc/gitrc/include_gitconfig.sh
+
 if [ -n $(git config gsync.remote) ]
 then
     git pull $(git config gsync.remote) $(git config gsync.branch)
