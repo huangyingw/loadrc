5d8b8f1c67a6e080d19bc8ddfd074fc9f8099394
Ying Huang <yhuang@imvu.com>
2019-02-12 22:00:36 -0800

    n

--- install_cscope.sh
+++ install_cscope.sh
@@ -3,5 +3,5 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-git clone git@github.com:huangyingw/cscope.git
+git clone https://github.com/huangyingw/cscope.git
 ./cscope/build_pre.sh
