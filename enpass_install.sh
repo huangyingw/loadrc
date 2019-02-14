295e39fe1ca8c6e6ecbd06d3f9b826b33cd9af2b
Ying Huang <yhuang@imvu.com>
2019-02-14 13:10:08 -0800

    n

---
+++ enpass_install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd "$SCRIPTPATH"
+
+echo "deb https://apt.enpass.io/ stable main" > \
+    /etc/apt/sources.list.d/enpass.list
+
+#And import key that is used to sign the release:
+wget -O - https://apt.enpass.io/keys/enpass-linux.key | apt-key add -
+apt-get update
+apt-get install -y enpass
