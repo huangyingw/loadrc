ae7c4af1abc1043f930d20f9b7af3f008569a5ec
Ying Huang <yhuang@imvu.com>
2019-02-14 13:17:49 -0800

    n

---
+++ chrome_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd "$SCRIPTPATH"
+
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub |  apt-key add -
+sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+apt-get update
+apt-get install google-chrome-stable
