c889c9f11638245b51775d52e85cb8018fd74c1a
Ying Huang <yhuang@imvu.com>
2019-02-17 21:12:57 -0800

    n

--- kvmrc/vmdk2qcow2.sh
+++ kvmrc/vmdk2qcow2.sh
@@ -1,2 +1,14 @@
 #!/bin/bash -
-qemu-img convert -p -f vmdk -O qcow2 box-disk001.vmdk imvu.qcow2
+if [ -z "$1" ]
+then
+    echo -e "${red}Please provide vmdk file ... ${NC}"
+    exit 1
+fi
+
+if [ -z "$2" ]
+then
+    echo -e "${red}Please provide qcow2 file ... ${NC}"
+    exit 1
+fi
+
+qemu-img convert -p -f vmdk -O qcow2 "$1" "$2"
