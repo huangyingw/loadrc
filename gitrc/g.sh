20935ecf6654795f56ec3a177713959ea7d15773
Ying Huang <yhuang@imvu.com>
2019-02-19 21:11:58 -0800

    n

--- gitrc/g.sh
+++ gitrc/g.sh
@@ -2,4 +2,4 @@
 ~/loadrc/gitrc/include_gitconfig.sh
 . ~/loadrc/gitrc/getCommit_message.sh "$1"
 git commit  --no-verify -m "$commit_message"
-~/loadrc/gitrc/postCommit.sh
+. ~/loadrc/gitrc/postCommit.sh
--- gitrc/postCommit.sh
+++ gitrc/postCommit.sh
@@ -2,5 +2,9 @@
 cd $(~/loadrc/gitrc/get_git.sh $pwd)
 > COMMIT_EDITMSG
 cd -
-~/loadrc/gitrc/gsync.sh
-~/loadrc/gitrc/gps.sh
+
+if [ "$commit_message" == "n" ]
+then
+    ~/loadrc/gitrc/gsync.sh
+    ~/loadrc/gitrc/gps.sh
+fi
