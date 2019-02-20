905c29b21735d1743b3b475e9214ca684fbc3c5f
Ying Huang <yhuang@imvu.com>
2019-02-19 23:19:59 -0800

    n

--- gitrc/checkout_rejs.sh
+++ gitrc/checkout_rejs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-for ss in $(git st|grep .*\.rej$)
-do
+for ss in $(git st|grep .*\.rej$) ; \
+do ; \
     git checkout "$1" $(echo "$ss" | sed 's/\.rej$//g') && \
-        rm "$ss"
-        done
+    rm "$ss" ; \
+done
