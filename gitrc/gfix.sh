98ff5ef546d273b54cd017140e6c1703dfeaf1ec
Ying Huang <yhuang@imvu.com>
2019-02-21 12:55:45 -0800

    n

---
+++ gitrc/gfix.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+currentBranch=$(~/loadrc/gitrc/get_current_branch.sh)
+
+if ( echo "$currentBranch" | grep -q 'fix$' )
+then
+    echo -e "${red}gfix should not be run in fix branch ... ${NC}"
+    exit 1
+fi
+
+fix_branch=`git branch |awk '/^\*/{print $2}'`.fix
+
+if ( git branch|grep -q "$fix_branch" )
+then
+    git checkout "$fix_branch"
+    exit 0
+fi
+
+git checkout -b "$fix_branch" && \
+    ~/loadrc/gitrc/gdev.sh "$currentBranch"
