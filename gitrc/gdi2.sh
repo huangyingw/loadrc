b878f25e1b31177cce2f68d7f211180304fb84c7
Ying Huang <yhuang@imvu.com>
2019-02-20 17:03:14 -0800

    n

--- gitrc/gdi2.sh
+++ gitrc/gdi2.sh
@@ -1,7 +1,15 @@
 #!/bin/bash -
+currentBranch=$(~/loadrc/gitrc/get_current_branch.sh)
+
+if [[ "$currentBranch" != *".fix" ]]
+then
+    echo -e "${red} should only run in *.fix branch... ${NC}"
+    exit 1
+fi
+
 if [ -z "$1" ]
 then
-    echo -e "${red}Please provide branch name ... ${NC}"
+    targetBranch=$(echo "$currentBranch" | sed 's/\.fix$//g')
 fi
 
 if [ -z $(git config gsync.remote) ]
@@ -9,12 +17,10 @@ then
     echo -e "${red}gsync.remote is not configured ... ${NC}"
 fi
 
-currentBranch=$(~/loadrc/gitrc/get_current_branch.sh)
-branch="$1"
-git branch -D "$branch"
-git branch "$branch" $(git config gsync.remote)"/"$(git config gsync.branch)
-~/loadrc/gitrc/gdi.sh "$branch"  2>&1 | tee gdi.findresult && \
-    git co "$branch" && \
+git branch -D "$targetBranch"
+git branch "$targetBranch" $(git config gsync.remote)"/"$(git config gsync.branch)
+~/loadrc/gitrc/gdi.sh "$targetBranch"  2>&1 | tee gdi.findresult && \
+    git co "$targetBranch" && \
     ~/loadrc/gitrc/gsync.sh && \
     git apply --reject --whitespace=fix gdi.findresult
 ~/loadrc/gitrc/checkout_rejs.sh "$currentBranch" && \
