295511cf43d554a052afae848e0cf10d1dc99b1c
Ying Huang <yhuang@imvu.com>
2019-03-04 10:56:59 -0800

    n

--- gitrc/gsync.sh
+++ gitrc/gsync.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 ~/loadrc/gitrc/include_gitconfig.sh
 
-if [ -n $(git config gsync.remote) ]
-then
-    git pull $(git config gsync.remote) $(git config gsync.branch)
-else
-    git pull
-fi
+doGsync () {
+    if [ -n $(git config gsync.remote) ]
+    then
+        git pull $(git config gsync.remote) $(git config gsync.branch)
+    else
+        git pull
+    fi
+}
+export -f doGsync
+
+doGsync
+git submodule foreach bash -c 'doGsync || :'
