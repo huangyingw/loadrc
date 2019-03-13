f14e197e7b66abd1d0f36d7d81461bec68187343
Ying Huang <yhuang@imvu.com>
2019-03-12 21:31:57 -0700

    n

--- bashrc/get_first_column_of_file.sh
+++ bashrc/get_first_column_of_file.sh
@@ -2,11 +2,12 @@
 
 if [ "$1" == "files.proj" ]
 then
+    echo "files.proj"
     return
 fi
 
 inputFile="$1"
 #awk '{print $1}' "$inputFile" | tee "$inputFile"
-cp -f "$inputFile" "$inputFile".bak
-awk -F":| " '{print $1}' "$inputFile".bak > "$inputFile"
-sort -u "$inputFile" -o "$inputFile"
+awk -F":| " '{print $1}' "$inputFile" > "$inputFile".bak
+sort -u "$inputFile".bak -o "$inputFile".bak
+echo "$inputFile".bak
--- vishrc/vsearch.sh
+++ vishrc/vsearch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 keyword=$(echo "$2" | sed -e "s/ \+/ \\\+/g;s/\[/\\\[/g;s/\]/\\\]/g")
 find_result="$4".findresult
-~/loadrc/bashrc/get_first_column_of_file.sh "$1"
+inputFile=$(~/loadrc/bashrc/get_first_column_of_file.sh "$1")
 
 if [[ "$3" == "w" ]]
 then
-    xargs grep -inH -- "$keyword" < "$1" > "$find_result"
+    xargs grep -inH -- "$keyword" < "$inputFile" > "$find_result"
 else
     cscope -dL -f cscope.out -"$3""$2" > "$find_result"
 fi
