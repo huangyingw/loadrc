453d9c8e7e8b6c4a8af0161f44675446140a4639
Ying Huang <yhuang@imvu.com>
2019-03-12 17:07:11 -0700

    n

--- bashrc/get_first_column_of_file_test.sh
+++ bashrc/get_first_column_of_file_test.sh
@@ -3,7 +3,7 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-resultFile="/Users/yhuang/Dropbox/loadrc/vimrc.vim.findresult"
+resultFile="/Users/yhuang/Dropbox/loadrc/loadrc.findresult"
 cat "$resultFile" | wc -l
 ./get_first_column_of_file.sh "$resultFile"
 cat "$resultFile" | wc -l
