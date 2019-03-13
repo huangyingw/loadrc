9c542f2b8ac7fabc70dfae615c54dd06e0dd295a
Ying Huang <yhuang@imvu.com>
2019-03-12 17:06:17 -0700

    n

--- bashrc/get_first_column_of_file.sh
+++ bashrc/get_first_column_of_file.sh
@@ -8,5 +8,5 @@ fi
 inputFile="$1"
 #awk '{print $1}' "$inputFile" | tee "$inputFile"
 cp -f "$inputFile" "$inputFile".bak
-awk -F":" '{print $1}' "$inputFile".bak > "$inputFile"
+awk -F":| " '{print $1}' "$inputFile".bak > "$inputFile"
 sort -u "$inputFile" -o "$inputFile"
