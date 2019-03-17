2c3b88978bb276b65fac2c0c1c13b41464b70c3e
Ying Huang <yhuang@imvu.com>
2019-03-16 22:22:44 -0700

    n

--- osxrc/restart_bluetooth.sh
+++ osxrc/restart_bluetooth.sh
@@ -2,6 +2,5 @@
 blueutil -p 0 && \
     blueutil -p 1 && \
     blueutil --connect 74-1a-e0-20-d3-b7 ; \
-    blueutil --connect 2c-41-a1-4a-dd-09 ; \
     blueutil --connect 8c-85-90-f3-90-69 && \
     blueutil --connect 64-a5-c3-f2-5e-00
