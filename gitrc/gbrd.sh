9c9baac94795fc4206da7f34abfcaa4eb79e8a4b
Ying Huang <yhuang@imvu.com>
2019-03-22 23:04:12 -0700

    n

--- gitrc/gbrd.sh
+++ gitrc/gbrd.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 git branch -d "$1"
-for ss in `git remote -v |awk '/\(fetch\)$/{print $1}'`; do git push $ss --delete "$1"; done
+for ss in `git remote -v |awk '/\(fetch\)$/{print $1}'`; \
+do \
+    git push $ss --delete "$1"; \
+done
