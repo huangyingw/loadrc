8a3c1712443e7a91d84cf438ac5c002aad93d9bf
Ying Huang <yhuang@imvu.com>
2019-03-22 22:48:12 -0700

    n

--- bashrc/tmirror.sh
+++ bashrc/tmirror.sh
@@ -2,12 +2,13 @@
 SOURCE=$1
 TARGET=$2
 
-ready_file="${TARGET}"/"tmirror.ready"
-if [ ! -d "${TARGET}" ]
+ready_file="$TARGET"/"tmirror.ready"
+if [ ! -d "$TARGET" ]
 then
-    mkdir -p "${TARGET}"
+    mkdir -p "$TARGET"
 fi
 
 rsync -aHinv --delete-before --force \
-    "${SOURCE}" "${TARGET}"> ${ready_file}
-nvim ${ready_file}
+    "$SOURCE" "$TARGET" > "$ready_file"
+cp -fv "$ready_file" ~/loadrc/tmirror.ready
+nvim ~/loadrc/tmirror.ready 
