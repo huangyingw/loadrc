c92067a0ade80209b526837cfedf3d5e6f311979
Ying Huang <huangyingw@gmail.com>
2019-05-31 22:17:49 -0700

    n

--- macrc/mac_sync.sh
+++ macrc/mac_sync.sh
@@ -1,21 +1,2 @@
 #!/bin/bash -
-
-if [ -z "$1" ]
-then
-    echo -e "${red}Please provide TARGET server ... ${NC}"
-    exit 1
-fi
-
-TARGET="$1"
-
-mac_sync () {
-    #eval folder="$1"
-    #rsync -aHv --progress --delete-before --force "$folder" "$TARGET":"$folder"
-    #have to run from mbp
-    #rsync -aHv --progress --delete imac:"~/Pictures/Photos\ Library.photoslibrary/" ~/Pictures/Photos\ Library.photoslibrary/
-}
-
-while read -r line || [[ -n "$line" ]]
-do
-    mac_sync "$line"
-done < mac_sync.list
+rsync -aHv --progress --delete-before --force  ~/Pictures/Photos\ Library.photoslibrary/ work:~/Pictures/Photos\ Library.photoslibrary/
