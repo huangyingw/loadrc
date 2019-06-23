42a30379113beb400a5a9467575c3ee8d90833f2
Ying Huang <huangyingw@gmail.com>
2019-06-22 18:19:17 -0700

    n

---
+++ macosrc/configure_mosh.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -
+BINPATH=$(realpath $(which mosh-server))
+
+# Turn the firewall off
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate off
+
+# Enable the firewall rules (make sure you are using your actual path)
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --add "$BINPATH"
+
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --unblockapp "$BINPATH"
+
+# Turn the firewall back on
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
