5c02cf51896b38d927da95c59cbda3d28b694d69
Ying Huang <huangyingw@gmail.com>
2019-06-30 21:15:43 -0700

    n

--- dhcprc/config.sh
+++ dhcprc/config.sh
@@ -1,6 +1,6 @@
-#!/bin/bash - 
+#!/bin/bash -
 SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-cp -fv ~/loadrc/hosts/
+cp -fv ~/loadrc/hosts/`hostname`/etc/default/isc-dhcp-server /etc/default/isc-dhcp-server
