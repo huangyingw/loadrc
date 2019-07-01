a694e976724fb460776e0006f069b6e5a2695b21
Ying Huang <huangyingw@gmail.com>
2019-06-30 21:28:08 -0700

    n

--- dhcprc/config.sh
+++ dhcprc/config.sh
@@ -4,3 +4,13 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
 cp -fv ~/loadrc/hosts/`hostname`/etc/default/isc-dhcp-server /etc/default/isc-dhcp-server
+cp -fv ~/loadrc/hosts/`hostname`/etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf
+
+systemctl start isc-dhcp-server
+systemctl enable isc-dhcp-server
+service isc-dhcp-server start
+service isc-dhcp-server enable
+
+ufw allow  67/udp
+#ufw reload
+#ufw show
