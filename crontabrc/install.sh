bd7fbd2ac26d84e207a7721b074896fe51344d6f
Ying Huang <huangyingw@gmail.com>
2019-07-05 16:55:29 -0700

    n

--- crontabrc/install.sh
+++ crontabrc/install.sh
@@ -4,12 +4,26 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
 CRONRC=$HOME/loadrc/crontabrc/."`hostname`".cron
+
 if [ -f "$CRONRC" ]
 then
     crontab "$CRONRC"
 fi
 
+echo2crontab() {
+    RESTARTFILE="$1"
+    if [ -f "$RESTARTFILE" ]
+    then
+        echo "*/10 * * * * $RESTARTFILE" >> crontab_file
+    fi
+}
+
 crontab -l > crontab_file
-cat ./zerotier.cron >> crontab_file
+
+while read -r line || [[ -n "$line" ]]
+do
+    echo2crontab "$line"
+done < restartfiles
+
 sort -u crontab_file -o crontab_file
 crontab crontab_file
