ba2c6d28b856814e69486bc4e0c5aa82f09fc284
Ying Huang <huangyingw@gmail.com>
2019-07-05 22:23:39 -0700

    n

--- zerotierrc/configure.sh
+++ zerotierrc/configure.sh
@@ -17,3 +17,21 @@ while read -r line || [[ -n "$line" ]]
 do
     deploy_configs "$line"
 done < zerotierrc.conf
+
+echo2crontab() {
+    RESTARTFILE="$1"
+    if [ -f "$RESTARTFILE" ]
+    then
+        echo "*/10 * * * * $RESTARTFILE" >> crontab_file
+    fi
+}
+
+crontab -l > crontab_file
+
+while read -r line || [[ -n "$line" ]]
+do
+    echo2crontab "$line"
+done < ~/loadrc/crontabrc/restartfiles
+
+sort -u crontab_file -o crontab_file
+crontab crontab_file
