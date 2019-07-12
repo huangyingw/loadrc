485aca43f6f2f2ec6056eaa3ac177cfa7ca4f327
Ying Huang <huangyingw@gmail.com>
2019-07-11 22:18:01 -0700

    n

--- install_vagrant.sh
+++ install_vagrant.sh
@@ -6,6 +6,9 @@ cd "$SCRIPTPATH"
 if [ $(uname) != "Darwin" ]
 then
     apt-get build-dep -y vagrant ruby-libvirt
+    apt install -y \
+        vagrant \
+        virtualbox
     apt-get install -y qemu libvirt-bin ebtables dnsmasq
     apt-get install -y libxslt-dev libxml2-dev libvirt-dev zlib1g-dev ruby-dev
     vagrant plugin install vagrant-libvirt
