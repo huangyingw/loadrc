59b08051777f8bf9b28e919e17809fe1bf88e044
Ying Huang <huangyingw@gmail.com>
2019-07-11 22:18:21 -0700

    n

--- imvurc/sb_setup.sh
+++ imvurc/sb_setup.sh
@@ -3,4 +3,5 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-vagrant box add "https://artifactory.corp.imvu.com/artifactory/api/vagrant/vagrant-imvu/lint-sandbox"
+vagrant box add "https://artifactory.corp.imvu.com/artifactory/api/vagrant/vagrant-imvu/lint-sandbox" --name lint-sandbox
+vagrant up
