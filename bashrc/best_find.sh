247018801c77cb973737b8f15cbf8b1590612eb1
Ying Huang <huangyingw@gmail.com>
2019-07-12 17:53:20 -0700

    n

--- bashrc/best_find.sh
+++ bashrc/best_find.sh
@@ -1,6 +1,14 @@
-#!/bin/bash - 
+#!/bin/bash -
 SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-find . "(" "${prune_params[@]}" "${prune_files[@]}" ")" -a -prune -o -type f -print -exec file {} \; | grep text | cut -d: -f1
+cd ~/imvu/website1/ && \
+    ps ax|awk '/\yfind\y/{print $1}' | while read ss; do kill -9 "$ss"; done && \
+    ps ax|awk '/\ycscope\y/{print $1}' | while read ss; do kill -9 "$ss"; done && \
+    time \
+    find . -type f > /dev/null 2>&1 && \
+    time \
+    find . -type f -exec grep -Iq . {} \; > /dev/null 2>&1 && \
+    time \
+    find . -type f -print -exec file {} \; | grep text | cut -d: -f1 > /dev/null 2>&1
