162a4396545e3e91d04243ce248b3d454b93d689
Ying Huang <huangyingw@gmail.com>
2019-07-12 21:38:02 -0400

    n

--- bashrc/jdupes.sh
+++ bashrc/jdupes.sh
@@ -10,9 +10,13 @@ TARGET="$1"
 MIRRORCHECK=$HOME/loadrc/."`hostname`".mirror.check
 
 crontab -r
+if [ -f ${MIRRORCHECK} ] ; \
+then \
+    ~/loadrc/bashrc/move67.sh ; \
+fi && \
 jdupes -1dNr "$TARGET" 2>&1 | tee ~/loadrc/jdupes.log && \
-    if [ -f ${MIRRORCHECK} ] ; \
-    then \
-        ~/loadrc/bashrc/sleep.sh ; \
-    fi && \
-    nvim ~/loadrc/jdupes.log
+if [ -f ${MIRRORCHECK} ] ; \
+then \
+    ~/loadrc/bashrc/sleep.sh ; \
+fi ; \
+nvim ~/loadrc/jdupes.log
