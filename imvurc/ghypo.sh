c8b3e3e39b99a6515cec6555d7051d14e577599c
Ying Huang <huangyingw@gmail.com>
2019-07-19 10:45:46 -0700

    n

--- imvurc/ghypo.sh
+++ imvurc/ghypo.sh
@@ -4,9 +4,9 @@ if [ $(hostname) != "lint-sandbox" ]
 then
     ssh -nY "$host" "cd $path ; ~/loadrc/imvurc/ghypo.sh $1"
 else
+    git co -f master
     ~/loadrc/gitrc/grsh.sh
     ~/loadrc/gitrc/gclean.sh
-    git co master
     git remote update
     git branch -D "$1"
     git co -b "$1" origin/"$1"
