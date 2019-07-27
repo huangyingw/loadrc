be33443c43d706835ce1af77ef353f08e72be0b0
Ying Huang <huangyingw@gmail.com>
2019-07-26 21:33:27 -0700

    n

--- macrc/mac_sync.sh
+++ macrc/mac_sync.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -
-rsync -aHv --progress --delete-before --force  ~/Pictures/Photos\ Library.photoslibrary/ work:~/Pictures/Photos\ Library.photoslibrary/
+rsync -aHv --progress --delete-before --force  ~/Pictures/Photos\ Library.photoslibrary/ work:"~/Pictures/Photos\ Library.photoslibrary/"
