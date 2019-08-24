ec3f7017b9c0405367a690d1a870e3d22cede05f
Ying Huang <huangyingw@gmail.com>
2019-08-24 11:20:26 -0700

    n

--- vishrc/vlc.sh
+++ vishrc/vlc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -
 if [ $(uname) == "Darwin" ]
 then
-    /Applications/VLC.app/Contents/MacOS/VLC --macosx-continue-playback=2 "$1"
+    /Applications/VLC.app/Contents/MacOS/VLC -f --macosx-continue-playback=2 "$1"
 else
     smplayer -fullscreen "$1"
 fi
