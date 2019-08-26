e087627f20f09fd3eab27506e919d9745b8fe351
Ying Huang <huangyingw@gmail.com>
2019-08-26 09:54:10 -0700

    n

--- bashrc/rsyncFiles.sh
+++ bashrc/rsyncFiles.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -
+host=$(git config deploy.host)
+path=$(git config deploy.path)
+
 cat files.proj | sed 's/^"//g;s/"$//g;s/\\ / /g' > files.proj.tmp && \
     rsync -aHv --force --progress \
     --files-from files.proj.tmp \
-    "$1" \
-    "$2"
+    . \
+    "$host:$path"
