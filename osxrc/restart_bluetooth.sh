26d5f749c5edcc0fee17d91edd293d410a55068d
Ying Huang <huangyingw@gmail.com>
2019-08-25 21:13:18 -0700

    n

--- osxrc/restart_bluetooth.sh
+++ osxrc/restart_bluetooth.sh
@@ -15,7 +15,6 @@ do
 done
 
 KeepConnect 74-1a-e0-20-d3-b7
-KeepConnect 8c-85-90-f3-90-69
+KeepConnect 80-4a-14-71-28-8c
 KeepConnect 64-a5-c3-f2-5e-00
-KeepConnect 04-4b-ed-ec-2d-7b
-KeepConnect 1c-1a-c0-f2-c7-31
+KeepConnect 8c-85-90-f3-90-69
