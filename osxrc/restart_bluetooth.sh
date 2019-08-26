ec05f7a8adeaed868a9040eaad1b9ae53857bdbd
Ying Huang <huangyingw@gmail.com>
2019-08-25 21:09:12 -0700

    n

--- osxrc/restart_bluetooth.sh
+++ osxrc/restart_bluetooth.sh
@@ -1,8 +1,21 @@
 #!/bin/bash -
-blueutil -p 0 && \
-    blueutil -p 1 && \
-    blueutil --connect 74-1a-e0-20-d3-b7 ; \
-    blueutil --connect 8c-85-90-f3-90-69 ; \
-    blueutil --connect 64-a5-c3-f2-5e-00 ; \
-    blueutil --connect 04-4b-ed-ec-2d-7b ; \
-    blueutil --connect 1c-1a-c0-f2-c7-31
+KeepConnect() {
+    ID="$1"
+    while [[ $(blueutil --is-connected "$ID") != 1 ]]
+    do
+        blueutil --connect "$ID"
+    done
+}
+
+blueutil -p 0
+
+while [[ $(blueutil -p) != 1 ]]
+do
+    blueutil -p 1
+done
+
+KeepConnect 74-1a-e0-20-d3-b7
+KeepConnect 8c-85-90-f3-90-69
+KeepConnect 64-a5-c3-f2-5e-00
+KeepConnect 04-4b-ed-ec-2d-7b
+KeepConnect 1c-1a-c0-f2-c7-31
