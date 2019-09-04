6f67f872add373b52f52325aebb78d3f27787d68
Ying Huang <huangyingw@gmail.com>
2019-09-04 11:26:22 -0700

    n

--- vishrc/vsearch.sh
+++ vishrc/vsearch.sh
@@ -3,9 +3,4 @@ keyword=$(echo "$2" | sed -e "s/ \+/ \\\+/g;s/\[/\\\[/g;s/\]/\\\]/g;s/\\\c//g;s/
 find_result="$4".findresult
 inputFile=$(~/loadrc/bashrc/get_first_column_of_file.sh "$1")
 
-if [[ "$3" == "w" ]]
-then
-    xargs grep -inH -- "$keyword" < "$inputFile" > "$find_result"
-else
-    cscope -dL -f cscope.out -"$3""$2" > "$find_result"
-fi
+cscope -dL -f cscope.out -"$3""$2" > "$find_result"
