ae6cc5aa109bd2edfff463f4188628382ca7090d
Ying Huang <huangyingw@gmail.com>
2019-10-08 15:11:35 -0700

    n

--- gitrc/gcob.sh
+++ gitrc/gcob.sh
@@ -8,5 +8,13 @@ if [ -n "$2" ]
 then
     git checkout -b "$1" "$2"
 else
+    for remote in $(git remote)
+    do
+        if [ -n "$(git ls-remote --heads $remote $1)" ]
+        then
+            git checkout -b "$1" "$remote/$1"
+            exit 0
+        fi
+    done
     git checkout -b "$1"
 fi
