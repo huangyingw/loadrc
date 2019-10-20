c35c9163dc6c44959cd822088e4e9ad26e3ab6f0
Ying Huang <huangyingw@gmail.com>
2019-10-20 09:12:58 -0700

    n

--- gitrc/gdio.sh
+++ gitrc/gdio.sh
@@ -4,4 +4,4 @@ output="$2"
 branch="$(git config gsync.branch)"
 
 ~/loadrc/gitrc/gsync.sh
-~/loadrc/gitrc/gdi.sh "$branch " "$local_branch" "$output" 2>&1 | tee "$output"  
+~/loadrc/gitrc/gdi.sh "$branch " "$local_branch" 2>&1 | tee "$output"  
