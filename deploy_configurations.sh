06c76c282878c9c6426784acc4716ff4745a229b
Ying Huang <huangyingw@gmail.com>
2019-11-09 21:16:40 -0800

    n

--- deploy_configurations.sh
+++ deploy_configurations.sh
@@ -17,7 +17,6 @@ ln -fs ~/loadrc/.zshrc ~/.zshrc
 ln -fs ~/loadrc/.profile ~/.profile
 
 mv -fv ~/.config/git/gitk ~/.config/git/gitk.bak
-ln -fs "$SCRIPTPATH" ~/loadrc
 ln -fs ~/loadrc/."`hostname`".theanorc  ~/.theanorc
 ln -fs ~/loadrc/.config/git/gitk  ~/.config/git/gitk
 #ln -fs ~/loadrc/.ssh ~/.ssh
