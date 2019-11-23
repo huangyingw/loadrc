c688a662cedce52195fa2fb9d813aa82356fd4bb
Ying Huang <huangyingw@gmail.com>
2019-11-23 12:27:40 -0800

    n

--- set_macos_configuration.sh
+++ set_macos_configuration.sh
@@ -7,3 +7,17 @@ if [ $(uname) != "Darwin" ]
 then
     exit 0
 fi
+
+deploy_configs() {
+    TARGET="$1"
+    SOURCE=~/loadrc/macos/"$TARGET"
+    if [ -f "$SOURCE" ]
+    then
+        sudo cp -fv "$SOURCE" "$TARGET"
+    fi
+}
+
+while read -r line || [ -n "$line" ]
+do
+    deploy_configs "$line"
+done < macos.conf
