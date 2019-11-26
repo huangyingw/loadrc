897c0dc1035437515033f94375d1b5d1a0cbc530
Ying Huang <huangyingw@gmail.com>
2019-11-26 10:57:18 -0800

    n

--- gitrc/gdio.sh
+++ gitrc/gdio.sh
@@ -2,6 +2,7 @@
 local_branch="$1"
 output="$2"
 local_master="$(git config gsync.branch)"
+remote="$(git config gsync.remote)"
 
 ~/loadrc/gitrc/gsync.sh
-~/loadrc/gitrc/gdi.sh "$local_master " "$local_branch" 2>&1 | tee "$output"  
+~/loadrc/gitrc/gdi.sh "$remote/$local_master " "$local_branch" 2>&1 | tee "$output"  
