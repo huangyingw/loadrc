02d6a83e3b3b8073f5a029faeac3a3c05d695bf5
Ying Huang <huangyingw@gmail.com>
2019-11-26 14:36:52 -0800

    n

--- gitrc/gdit.sh
+++ gitrc/gdit.sh
@@ -2,10 +2,11 @@
 output="$1"
 current_branch="$(~/loadrc/gitrc/get_current_branch.sh)"
 target_branch=$(echo "$current_branch" | sed 's/\.fix$//g')
-local_master="$(git config gsync.branch)"
+remote="$(git config gsync.remote)"
+remote_branch="$(git config gsync.branch)"
 
 git co "$target_branch"
-git merge "$local_master" && \
+git merge "$remote/$remote_branch" && \
     git co "$current_branch" && \
     ~/loadrc/gitrc/gdi.sh "$target_branch " "$current_branch" 2>&1 | tee "$output"
 
