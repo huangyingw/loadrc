aa6b7ac4c3f58d637b5aebcc98d7081e44929b0d
Ying Huang <huangyingw@gmail.com>
2019-11-30 19:44:33 -0800

    n

---
+++ bashrc/fdocs.sh
@@ -0,0 +1,19 @@
+#! /bin/zsh
+
+DocConfig="fdocs.config"
+TARGET="fdocs.list"
+
+if [ -f "$DocConfig" ]
+then
+    or="";
+
+    while read suf
+    do
+        suf=$(echo "$suf" | sed 's/"//g')
+        include_params+=( $or "-wholename" "$suf" )
+        or="-o"
+    done < "$DocConfig"
+
+    find . "(" "${include_params[@]}" ")" -type f -print | sed 's/\(["'\''\]\)/\\\1/g;s/.*/"&"/' >> "$TARGET"
+    sort -u "$TARGET" -o "$TARGET"
+fi
