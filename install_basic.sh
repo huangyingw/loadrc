caf2be3ac1ff3a570c54e30b7dfac0ccfb6240fc
Ying Huang <huangyingw@gmail.com>
2020-01-16 09:21:55 -0800

    n

--- install_basic.sh
+++ install_basic.sh
@@ -3,44 +3,8 @@ SCRIPT=$(realpath "$0") && \
     SCRIPTPATH=$(dirname "$SCRIPT") && \
     cd "$SCRIPTPATH" || cd ~/loadrc/
 
-if [ $(uname) != "Darwin" ]
-then
-    ./install_prerequisite.sh
-    ./enpass_install.sh
-    ./nxrc/install.sh
-else
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    brew update
-    brew install \
-        Caskroom/cask/xquartz \
-        astyle \
-        blueutil \
-        coreutils \
-        git \
-        htop \
-        hub \
-        jupyterlab \
-        libtool \
-        lynx \
-        minikube \
-        mosh \
-        node \
-        openssl \
-        rename \
-        tig \
-        tmux \
-        wakeonlan \
-        watch \
-        xpdf
-    brew cask install \
-        kdiff3 \
-        libreoffice \
-        lynxlet \
-        wkhtmltopdf
-    sudo easy_install pip
-    sudo pip install --upgrade pip
-
-fi
+./install_basic_linux.sh
+./install_basic_macos.sh
 
 pip install \
     pdftotext
