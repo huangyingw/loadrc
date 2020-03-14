3a59c887688a5816a9548f3f39345196af33e98a
Ying Huang <huangyingw@gmail.com>
2020-03-14 15:01:48 -0700

    n

--- bashrc/mosh.sh
+++ bashrc/mosh.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 target=$(echo "$1" | sed  -e "s/\/$//g")
 mkdir "$target"
-mosh "$target" -- ${tmuxAction}
+
+if ( ssh "$target" "uname" | grep -q Darwin )
+then
+    server="/usr/local/bin/mosh-server"
+else
+    server="/usr/bin/mosh-server"
+fi
+
+if ( ssh "$target" "uname" | grep -q Darwin )
+then
+    tmuxAction='/usr/local/bin/tmux new-session -A -s main'
+else
+    tmuxAction='/usr/bin/tmux new-session -A -s main'
+fi
+
+mosh --server="$server" "$target" -- ${tmuxAction}
--- bashrc/ssh.sh
+++ bashrc/ssh.sh
@@ -1,3 +1,11 @@
 #! /bin/zsh
 target=$(echo "$1" | sed  -e "s/\/$//g")
+
+if ( ssh "$target" "uname" | grep -q Darwin )
+then
+    tmuxAction='/usr/local/bin/tmux new-session -A -s main'
+else
+    tmuxAction='/usr/bin/tmux new-session -A -s main'
+fi
+
 ssh -Y "$target" -t -- ${tmuxAction}
