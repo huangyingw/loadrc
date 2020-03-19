673755bceff4f8e3a1199a2c40124be6e5911518
Ying Huang <huangyingw@gmail.com>
2020-03-19 18:35:41 -0400

    n

--- bashrc/fvideos.sh
+++ bashrc/fvideos.sh
@@ -1,6 +1,6 @@
 #! /bin/zsh
 
-find . -type f -size +10M -exec ls -S {} \+ > fav.log.bak && \
+find . -type f -size +10M -exec du -h {} + | sort -r -h | cut -f 2 > fav.log.bak && \
     cp -fv fav.log.bak fav.log && \
     find . -type f -size +10M -exec ls -t {} \+ > fav.log.sort.bak && \
     cp -fv fav.log.sort.bak fav.log.sort
