1c8c94315a6795435fd81f02913886fe63749073
Ying Huang <huangyingw@gmail.com>
2020-03-30 20:53:59 -0700

    n

--- install_nodejs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-SCRIPT=$(realpath "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
-cd "$SCRIPTPATH"
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-. $HOME/loadrc/.nvmrc
-nvm install 4
-nvm use 4
