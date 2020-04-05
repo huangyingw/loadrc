31f9dec36a5ba966fc6964a96865bcbba96babff
Ying Huang <huangyingw@gmail.com>
2020-04-05 14:35:25 -0700

    n

--- gitrc/dogsync.sh
+++ gitrc/dogsync.sh
@@ -1,20 +1,4 @@
 #!/bin/zsh
-~/loadrc/gitrc/include_gitconfig.sh
-currentBranch=$(~/loadrc/gitrc/get_current_branch.sh)
-git remote update
 
-for ss in $(git config --get-all pull.from)
-do
-    remote_branch=$(echo "$ss" | sed  -e "s/\// /g")
-    COMMAND="git pull $remote_branch"
-    eval "$COMMAND"
-done
-
-git checkout files.proj
-
-git pull
-if [ -n $(git config gsync.remote) ]
-then
-    git pull $(git config gsync.remote) $(git config gsync.branch)
-fi
+~/loadrc/gitrc/gpl.sh
 ~/loadrc/gitrc/gps.sh
--- gitrc/gpl.sh
+++ gitrc/gpl.sh
@@ -1,8 +1,22 @@
 #!/bin/zsh
+git remote update
 git checkout files.proj
+git pull
 currentBranch=$(~/loadrc/gitrc/get_current_branch.sh)
 
 for ss in `git remote -v |awk '/\(fetch\)$/{print $1}'`
 do
     git pull $ss $currentBranch
 done
+
+for ss in $(git config --get-all pull.from)
+do
+    remote_branch=$(echo "$ss" | sed  -e "s/\// /g")
+    COMMAND="git pull $remote_branch"
+    eval "$COMMAND"
+done
+
+if [ -n $(git config gsync.remote) ]
+then
+    git pull $(git config gsync.remote) $(git config gsync.branch)
+fi
