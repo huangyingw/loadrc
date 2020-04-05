cccaf5d664ee23c404b8866bdbf1bb55e39ea0bd
Ying Huang <huangyingw@gmail.com>
2020-04-05 14:39:35 -0700

    n

--- gitrc/gme.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-~/loadrc/gitrc/include_gitconfig.sh
-if [ "$(git config merge.allow)" != "true" ]
-then
-    echo -e "${red}git merge is not allowed, without git config merge.allow is true ... ${NC}"
-    exit 1
-fi
-git merge "$1" \
-    && ~/loadrc/gitrc/gps.sh
