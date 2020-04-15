baa3efcc311999f9bf3207c8c94cf0b0f415838a
Ying Huang <huangyingw@gmail.com>
2020-04-15 09:47:31 -0700

    n

--- gitrc/dogdio.sh
+++ gitrc/dogdio.sh
@@ -1,6 +1,11 @@
 #!/bin/zsh
-local_branch="$1"
-output="$2"
+if [ -z $(git config gsync.remote) ] || [ -z $(git config gsync.branch) ] 
+then
+    exit 0
+fi
+
+local_branch="$(~/loadrc/gitrc/get_current_branch.sh)" 
+output="$local_branch.gdio.diff"
 local_master="$(git config gsync.branch)"
 remote="$(git config gsync.remote)"
 
--- gitrc/gdio.sh
+++ gitrc/gdio.sh
@@ -1,8 +1,3 @@
 #!/bin/zsh
-local_branch="$1"
-output="$2"
-local_master="$(git config gsync.branch)"
-remote="$(git config gsync.remote)"
-
-~/loadrc/gitrc/gsync.sh
-~/loadrc/gitrc/gdi.sh "$remote/$local_master " "$local_branch" 2>&1 | tee "$output"  
+~/loadrc/gitrc/dogdio.sh &
+git submodule foreach ~/loadrc/gitrc/dogdio.sh &
