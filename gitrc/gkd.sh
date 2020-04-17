052f3890d6057bf418242ffef2f299fc29861efb
Ying Huang <huangyingw@gmail.com>
2020-04-17 16:10:31 -0700

    n

---
+++ gitrc/gkd.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+if [ -n "$1" ]
+then
+    if [ -n "$2" ]
+    then
+        git difftool -t kdiff3 -y "$1" "$2"
+    else
+        COMMAND="git difftool -y $1"
+
+        for ss in $(git config --get-all gdif.ignore)
+        do
+            COMMAND="$COMMAND  ':(exclude)$ss'"
+        done
+
+        eval "$COMMAND"
+    fi
+else
+    git difftool -t kdiff3 -y --cached
+fi
