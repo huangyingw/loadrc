407ab588d89d088d666de2ddc172933ed006eef0
Ying Huang <huangyingw@gmail.com>
2020-04-30 16:35:07 -0700

    n

--- gitrc/gdi.sh
+++ gitrc/gdi.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 if [ -z "$1" ]
 then
-    git diff --ignore-space-at-eol -b -w --ignore-blank-lines --cached HEAD
+    git diff --ignore-space-at-eol -b -w --cached HEAD
 else
-    COMMAND="git diff --ignore-space-at-eol -b -w --ignore-blank-lines $1 $2"
+    COMMAND="git diff --ignore-space-at-eol -b -w $1 $2"
 
     for ss in $(git config --get-all gdif.ignore)
     do
