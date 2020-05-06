1bbbf229916e414669762e36c2b22cfc1c28d90a
Ying Huang <huangyingw@gmail.com>
2020-05-05 20:20:57 -0700

    n

--- gitrc/checkout_rejs.sh
+++ gitrc/checkout_rejs.sh
@@ -1,6 +1,8 @@
 #!/bin/zsh
 for ss in $(git status | grep \.rej$) ; \
 do \
-    git checkout "$1" $(echo "$ss" | sed 's/\.rej$//g') && \
+    targetFile=$(echo "$ss" | sed 's/\.rej$//g')
+    git checkout "$1" "$targetFile" && \
     rm "$ss" ; \
+    git add "$targetFile" ; \
 done
