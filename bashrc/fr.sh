614b10216e9804ef19021de751f4e859a1a38cac
Ying Huang <huangyingw@gmail.com>
2020-05-15 16:03:54 -0700

    n

--- bashrc/fr.sh
+++ bashrc/fr.sh
@@ -1,11 +1,4 @@
 #!/bin/zsh
-~/loadrc/gitrc/checkGitStatus.sh
-
-if [ $? -ne 0 ]
-then
-    exit 1
-fi
-
 FIND=$(echo $1 | sed 's/\//\\\//g')
 REPLACE=$(echo $2 | sed 's/\//\\\//g')
 xargs sed -i.bak  "s/${FIND}/${REPLACE}/g" < "$3"
