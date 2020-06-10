f85b3351326f0a8c6d20bb7a03142a41b5dad49e
Ying Huang <huangyingw@gmail.com>
2020-06-10 14:58:05 -0700

    n

--- gitrc/gdit.sh
+++ gitrc/gdit.sh
@@ -5,15 +5,15 @@ target_branch=$(echo "$current_branch" | sed 's/\.fix$//g')
 remote="$(git config gsync.remote)"
 remote_branch="$(git config gsync.branch)"
 
-git co *.fix.gdio.diff
 git co "$target_branch"
 git merge "$remote/$remote_branch" && \
+    git pull && \
     git push ; \
     git co "$current_branch" && \
     ~/loadrc/gitrc/gdi.sh "$target_branch " "$current_branch" 2>&1 | tee "$output"
 
 if [ $? -ne 0 ]
 then
-    echo -e "${red}most probably sth wrong in git merge... ${NC}" 2>&1 | tee "$output" 
+    echo -e "${red}most probably sth wrong in git merge... ${NC}" 2>&1 | tee "$output"
     exit 1
 fi
