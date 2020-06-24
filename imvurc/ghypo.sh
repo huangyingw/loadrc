33ea0b789fd37db52709c7d68ce25df262946d3e
Ying Huang <huangyingw@gmail.com>
2020-06-23 21:02:49 -0700

    n

--- imvurc/ghypo.sh
+++ imvurc/ghypo.sh
@@ -8,10 +8,7 @@ else
     ~/loadrc/gitrc/gclean.sh
     git remote update
     git checkout -b "$1" origin/"$1"
-    git checkout "$1"
-    s/pull && \
-        git pull --rebase && \
-        git push
+    s/pull
 
     if [ $(~/loadrc/gitrc/git_ready.sh) ]
     then
