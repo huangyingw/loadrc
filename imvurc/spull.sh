617e7088af9f855da8bc6bc7ae328ee5de0d4381
Ying Huang <huangyingw@gmail.com>
2020-06-23 20:27:48 -0700

    n

---
+++ imvurc/spull.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+if [ $(hostname) != "linb-sandbox" ]
+then
+    ssh -nY "$host" "cd $rpath ; ~/loadrc/imvurc/ghypo.sh $1"
+else
+    ~/loadrc/gitrc/grsh.sh
+    ~/loadrc/gitrc/gclean.sh
+    git remote update
+    git checkout -b "$1" origin/"$1"
+    git checkout "$1"
+    s/pull && \
+        git pull --rebase && \
+        git push
+
+    if [ $(~/loadrc/gitrc/git_ready.sh) ]
+    then
+        s/hypo
+    fi
+fi
