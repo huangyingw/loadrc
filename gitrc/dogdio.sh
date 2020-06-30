0d821edf38ee7dc6995b7bb6fef0d0b58e3c8bcf
Ying Huang <huangyingw@gmail.com>
2020-06-30 12:05:22 -0400

    n

--- gitrc/dogdio.sh
+++ gitrc/dogdio.sh
@@ -10,6 +10,6 @@ output=$(echo "$output" | sed 's/\//_/g')
 local_master="$(git config gsync.branch)"
 remote="$(git config gsync.remote)"
 
-~/loadrc/gitrc/gdi.sh "$remote/$local_master " "$local_branch" 2>&1 | tee "$output"  
+~/loadrc/gitrc/gdi.sh "$remote/$local_master " 2>&1 | tee "$output"  
 sed -i.bak "s/^--- a\//--- \.\//g;s/^+++ b\//+++ \.\//g" "$output"
 exit 0
