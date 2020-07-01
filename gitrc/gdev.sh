3d8d3b6804029e7f143008541802ecd44fb5549e
Ying Huang <huangyingw@gmail.com>
2020-06-30 20:09:40 -0700

    n

--- gitrc/gdev.sh
+++ gitrc/gdev.sh
@@ -1,7 +1,5 @@
 #!/bin/zsh
 touch files.proj \
-    && git config --file .gitconfig commit.message n \
-    && git config --file .gitconfig merge.allow true \
     && git config --file .gitconfig vrun.silent false \
     && git config --file .gitconfig vrun.async false \
     && git config --file .gitconfig deploy.host localhost \
