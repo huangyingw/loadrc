656604f380e7a35b750af077239db0e22606a9d2
Ying Huang <huangyingw@gmail.com>
2020-07-02 13:24:47 -0700

    n

--- bashrc/generate_files_from.sh
+++ bashrc/generate_files_from.sh
@@ -1,3 +1,18 @@
 #!/bin/zsh
+cat files.proj | sed 's/^"//g;s/"$//g;s/\\ / /g' > rsync.files
+PRUNE_POSTFIX=prunefix.rsync
 
-cat files.proj | sed 's/^"//g;s/"$//g;s/\\ / /g' > files.from
+if [ ! -f "$PRUNE_POSTFIX" ]
+then
+    exit
+fi
+
+or="";
+
+while read suf
+do
+    suf=$(echo "$suf" | sed 's/"//g')
+    prune_params+=( $or "-wholename" "$suf" )
+    or="-o"
+done < "$PRUNE_POSTFIX"
+find . "(" "${prune_params[@]}" ")" -a -prune -o -size +0 -type f -exec grep -Il "" {} +
