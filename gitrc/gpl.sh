0a6ed50fde6c3b0e8291226c2db11892b02c53a3
Ying Huang <huangyingw@gmail.com>
2020-07-02 09:49:04 -0700

    n

--- gitrc/gpl.sh
+++ gitrc/gpl.sh
@@ -7,17 +7,18 @@ currentBranch=$(~/loadrc/gitrc/get_current_branch.sh)
 
 for ss in `git remote -v |awk '/\(fetch\)$/{print $1}'`
 do
-    git pull $ss $currentBranch
+    git pull $ss $currentBranch &
+    git remote prune $ss &
 done
 
 for ss in $(git config --get-all pull.from)
 do
     remote_branch=$(echo "$ss" | sed  -e "s/\// /g")
-    COMMAND="git pull $remote_branch"
+    COMMAND="git pull $remote_branch &"
     eval "$COMMAND"
 done
 
 if [ -n $(git config gsync.remote) ]
 then
-    git pull $(git config gsync.remote) $(git config gsync.branch)
+    git pull $(git config gsync.remote) $(git config gsync.branch) &
 fi
