f3eb3a5dd697c21debe58bfec5a8c06c30aa0710
Ying Huang <huangyingw@gmail.com>
2020-07-03 10:57:26 -0700

    n

--- gitrc/gcl.sh
+++ gitrc/gcl.sh
@@ -11,19 +11,20 @@ target="${author}-${repo}"
 echo "target --> $target"
 
 git clone "$1" "$target" ; \
-    cp -fv ~/loadrc/.gitconfig_sample "$target"/.gitconfig
+    cp -v ~/loadrc/.gitconfig_sample "$target"/.gitconfig
 
 urlVar="$1"
 urlVar=$(echo "$urlVar" | sed 's/\//\\\//g')
 sed -i.bak "s/urlVar/$urlVar/g;s/repoVar/$target/g;s/authorVar/$author/g" "$target"/.gitconfig
 echo ".gitconfig --> $(realpath ${target}/.gitconfig)"
 
-cd "$target" \
-    && $HOME/loadrc/gitrc/gcob.sh dev \
-    && $HOME/loadrc/gitrc/gdev.sh \
-    && $HOME/loadrc/bashrc/cscope.sh \
-    && git add . \
-    && git add -f files.proj .gitconfig \
-    && $HOME/loadrc/gitrc/g.sh \
-    && hub create \
-    && ~/loadrc/gitrc/gclb.sh
+cd "$target" && \
+    git remote remove origin ; \
+    $HOME/loadrc/gitrc/gcob.sh dev && \
+    $HOME/loadrc/gitrc/gdev.sh && \
+    $HOME/loadrc/bashrc/cscope.sh && \
+    git add . && \
+    git add -f files.proj .gitconfig && \
+    $HOME/loadrc/gitrc/g.sh && \
+    hub create && \
+    ~/loadrc/gitrc/gclb.sh
