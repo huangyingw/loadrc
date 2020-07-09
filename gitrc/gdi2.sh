cc8e1aefdbbc13f5d4095f2597c05bedfbcd41a1
Ying Huang <huangyingw@gmail.com>
2020-07-09 11:33:13 -0700

    n

--- gitrc/gdi2.sh
+++ gitrc/gdi2.sh
@@ -29,7 +29,7 @@ local_master="$(git config gsync.branch)"
 GDITDIFF=$(echo "$currentBranch.gdit.diff" | sed 's/\//_/g')
 git checkout -b "$targetBranch" "$local_master" ; \
     ~/loadrc/gitrc/discard_unnecessaries.sh ; \
-    git checkout "$targetBranch" ; \
+    git checkout -f "$targetBranch" ; \
     git apply --reject --whitespace=fix "$GDITDIFF" ; \
     ~/loadrc/gitrc/checkout_rejs.sh "$currentBranch" && \
     git add . && \
