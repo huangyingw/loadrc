34b41c9ef968ef915246924e5f556f2d0617f77e
Ying Huang <huangyingw@gmail.com>
2020-07-23 20:48:11 -0700

    n

--- gitrc/parse-github.test.sh
+++ gitrc/parse-github.test.sh
@@ -3,5 +3,5 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-./parse_github.sh git@github.com:gluster/glusterfs.git
-./parse_github.sh https://github.com/gluster/glusterfs.git
+./parse-github.sh git@github.com:gluster/glusterfs.git
+./parse-github.sh https://github.com/gluster/glusterfs.git
