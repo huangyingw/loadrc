cb338c4576154159d34e927d1c108c3f7d767227
Ying Huang <huangyingw@gmail.com>
2020-07-24 21:36:02 -0700

    n

--- musicrc/move.test.sh
+++ musicrc/move.test.sh
@@ -3,4 +3,4 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-./move.sh /media/volgrp/mirror/musics/ xiaomi:/data/data/com.arachnoid.sshelper/files/home/SDCard/music/
+./move.sh mini:/media/mirror2/mirror2/musics/ "/Volumes/NO NAME/musics"
