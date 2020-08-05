15db04f9cf9c4c98d8273f2a57547225c218bf8e
Ying Huang <huangyingw@gmail.com>
2020-08-05 09:45:59 -0700

    n

--- vishrc/vlc.py
+++ vishrc/vlc.py
@@ -6,19 +6,18 @@ home = str(Path.home()) + '/'
 sys.path.insert(1, home + 'loadrc/pythonrc/')
 from parse_vtt import parse_vtt_str
 
-with open(home + "output.txt", "w") as text_file:
-    text_file.write("sys.argv[0] --> %s\n" % sys.argv[0])
-    text_file.write("sys.argv[1] --> %s\n" % sys.argv[1])
-    text_file.write("sys.argv[2] --> %s\n" % sys.argv[2])
-    fileVar = sys.argv[1]
-    lineVar = sys.argv[2]
-    text_file.write("fileVar --> %s\n" % fileVar)
-    text_file.write("lineVar --> %s\n" % lineVar)
-    targetFile = fileVar + '/' + lineVar
-    text_file.write("targetFile --> %s\n" % targetFile)
-    if path.exists(targetFile):
-        text_file.write("path.exists --> \n")
-        start = parse_vtt_str(lineVar)
-        text_file.write("start --> %s\n" % start)
-    else:
-        text_file.write("path.exists not --> \n")
+print("sys.argv[0] --> %s\n" % sys.argv[0])
+print("sys.argv[1] --> %s\n" % sys.argv[1])
+print("sys.argv[2] --> %s\n" % sys.argv[2])
+fileVar = sys.argv[1]
+lineVar = sys.argv[2]
+print("fileVar --> %s\n" % fileVar)
+print("lineVar --> %s\n" % lineVar)
+targetFile = fileVar + '/' + lineVar
+print("targetFile --> %s\n" % targetFile)
+if path.exists(targetFile):
+    print("path.exists --> \n")
+    start = parse_vtt_str(lineVar)
+    print("start --> %s\n" % start)
+else:
+    print("path.exists not --> \n")
