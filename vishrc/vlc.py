d18575c8b8afb34d6bedac8d3c95122d9801330b
Ying Huang <huangyingw@gmail.com>
2020-08-05 09:51:58 -0700

    n

--- vishrc/vlc.py
+++ vishrc/vlc.py
@@ -6,18 +6,18 @@ home = str(Path.home()) + '/'
 sys.path.insert(1, home + 'loadrc/pythonrc/')
 from parse_vtt import parse_vtt_str
 
-print("sys.argv[0] --> %s\n" % sys.argv[0])
-print("sys.argv[1] --> %s\n" % sys.argv[1])
-print("sys.argv[2] --> %s\n" % sys.argv[2])
+print("sys.argv[0] --> %s" % sys.argv[0])
+print("sys.argv[1] --> %s" % sys.argv[1])
+print("sys.argv[2] --> %s" % sys.argv[2])
 fileVar = sys.argv[1]
 lineVar = sys.argv[2]
-print("fileVar --> %s\n" % fileVar)
-print("lineVar --> %s\n" % lineVar)
-targetFile = fileVar + '/' + lineVar
-print("targetFile --> %s\n" % targetFile)
+print("fileVar --> %s" % fileVar)
+print("lineVar --> %s" % lineVar)
+targetFile = path.dirname(fileVar) + '/' + lineVar
+print("targetFile --> %s" % targetFile)
 if path.exists(targetFile):
-    print("path.exists --> \n")
+    print("path.exists --> ")
     start = parse_vtt_str(lineVar)
-    print("start --> %s\n" % start)
+    print("start --> %s" % start)
 else:
-    print("path.exists not --> \n")
+    print("path.exists not --> ")
