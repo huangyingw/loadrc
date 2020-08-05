af49c23cf32cfadb9ba1d0db27b9fe0f6c6bab23
Ying Huang <huangyingw@gmail.com>
2020-08-04 20:59:44 -0700

    n

--- pythonrc/parse_vtt.py
+++ pythonrc/parse_vtt.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 import datetime
+import re
 
 time_string = "00:00:08.690 --> 00:00:08.700 align:start position:0%"
-date_time = datetime.datetime.strptime(time_string, "%H:%M:%S")
 
-print(date_time)
+r2 = re.findall(r"\d+:\d+:\d+(?=[^:]*-->)", time_string)
+print(r2)
+
+date_time = datetime.datetime.strptime(r2[0], "%H:%M:%S")
 
 a_timedelta = date_time - datetime.datetime(1900, 1, 1)
 seconds = a_timedelta.total_seconds()
