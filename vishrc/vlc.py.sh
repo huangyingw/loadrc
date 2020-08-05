e4b1c52b840039efa9ac45d1847d4e6475e83072
Ying Huang <huangyingw@gmail.com>
2020-08-05 09:46:19 -0700

    n

--- vishrc/vlc.py.sh
+++ vishrc/vlc.py.sh
@@ -3,4 +3,5 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-./vlc.py "/Users/huangyingw/Dropbox/learning/AI/fast.ai" "02:11:12.620 --> 02:11:17.560"
+./vlc.py "/Users/huangyingw/Dropbox/learning/AI/fast.ai/Intro_to_Machine_Learning_-_Lesson_1.en.vtt" "02:11:12.620 --> 02:11:17.560"
+./vlc.py "/Users/huangyingw/Dropbox/learning/AI/fast.ai/fav.log" "Machine_Learning_1_-_Lesson_8.mkv"
