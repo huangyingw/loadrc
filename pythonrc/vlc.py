c680c4acdc88594b6fef63ec69fd7d61cd610fbe
Ying Huang <huangyingw@gmail.com>
2020-08-06 21:23:33 -0700

    n

--- pythonrc/vlc.py
+++ pythonrc/vlc.py
@@ -10,15 +10,12 @@ targetFile = path.dirname(fileVar) + '/' + lineVar
 
 
 def callvlc(targetFile, start=0):
-    subprocess.check_call(['/Applications/VLC.app/Contents/MacOS/VLC', '-f', '--macosx-continue-playback=2', targetFile])
+    subprocess.check_call(['/Applications/VLC.app/Contents/MacOS/VLC', '-f', '--macosx-continue-playback=2', '--start-time=' + str(start), targetFile])
 
 
 if path.exists(targetFile):
-    print("path.exists --> ")
     callvlc(targetFile)
 else:
-    print("path.exists not --> ")
     start = parse_vtt_str(lineVar)
     targetFile = find_vtt_video(fileVar)
-    print("targetFile --> %s" % targetFile)
-    print("start --> %s" % start)
+    callvlc(targetFile, start)
