668bf528d87edc8db0946dfd9dbbc60cb6072c57
Ying Huang <huangyingw@gmail.com>
2020-08-09 22:59:35 -0400

    n

--- kvmrc/virt_resize.sh
+++ kvmrc/virt_resize.sh
@@ -5,7 +5,8 @@ cd "$SCRIPTPATH"
 
 # https://maunium.net/blog/resizing-qcow2-images/
 target="$1"
-qemu-img create -f qcow2 -o preallocation=metadata "$target".tmp 15G
-virt-resize "$target" "$target".tmp
-mv -v "$target" "$target".bak
-mv -v "$target".tmp "$target"
+size="$2"
+qemu-img create -f qcow2 -o preallocation=metadata "$target".tmp "$size" && \
+    virt-resize "$target" "$target".tmp && \
+    mv -v "$target" "$target".bak && \
+    mv -v "$target".tmp "$target"
