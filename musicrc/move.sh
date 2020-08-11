706587cac34615d48d5e621155e9fc754a7909f7
Ying Huang <huangyingw@gmail.com>
2020-08-11 09:15:31 -0700

    n

--- musicrc/move.sh
+++ musicrc/move.sh
@@ -3,15 +3,10 @@ SOURCE=$1
 TARGET=$2
 
 doCopy () {
-    rsync -aHv --progress --remove-source-files --max-size="$1" --force \
+    rsync -aHv --progress --remove-source-files --force \
         --exclude \*.zip  \
         --exclude \*.rar  \
         "${SOURCE}" "${TARGET}"
 }
 
-COUNTER=10
-while [[  $COUNTER -lt 1900 ]]; do
-    MAXSIZE="$COUNTER""m"
-    doCopy "$MAXSIZE"
-    let COUNTER=COUNTER+10
-done
+doCopy
