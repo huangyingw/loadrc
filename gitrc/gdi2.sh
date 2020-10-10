070da50ae0e0f82fec397764374c721c0cfdac50
Ying Huang <huangyingw@gmail.com>
2020-10-09 21:22:36 -0700

    n

--- gitrc/gdi2.sh
+++ gitrc/gdi2.sh
@@ -28,9 +28,8 @@ rpath=$(git config deploy.path)
 GDITDIFF=$(echo "$currentBranch.gdit.diff" | sed 's/\//_/g')
     ~/loadrc/gitrc/discard_unnecessaries.sh ; \
     git checkout -f "$targetBranch" ; \
-    git apply --reject --whitespace=fix "$GDITDIFF" ; \
+    git apply --index --reject --whitespace=fix "$GDITDIFF" ; \
     ~/loadrc/gitrc/checkout_rejs.sh "$currentBranch" && \
-    git add . && \
     git commit  --no-verify -am "$commit_message" ; \
     git pull ; \
     git push ; \
