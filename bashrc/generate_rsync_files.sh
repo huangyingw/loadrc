3132be0f28cfa4285429f3eef77ca710fbe3de59
Ying Huang <huangyingw@gmail.com>
2020-10-15 09:46:48 -0700

    n

--- bashrc/generate_rsync_files.sh
+++ bashrc/generate_rsync_files.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-rsyncFiles=rsync.files.bak
+rsyncFiles="$(~/loadrc/bashrc/random_string.sh 20)"
 cat files.proj | sed 's/^"//g;s/"$//g;s/\\ / /g' > "$rsyncFiles"
 PRUNE_POSTFIX=prunefix.rsync
 INCLUDE_FILE=includefile.rsync
@@ -27,13 +27,14 @@ done < "$INCLUDE_FILE"
 
 if [ ${#prune_params[@]} -gt 0 ] ; \
 then \
-    find . "(" "${prune_params[@]}" ")" -type f -size -9000k > "$rsyncFiles".diff && \
-    comm -23 <(sort "$rsyncFiles") <(sort "$rsyncFiles".diff) > "$rsyncFiles".tmp && \
-    cp -fv "$rsyncFiles".tmp "$rsyncFiles" ; \
+    find . "(" "${prune_params[@]}" ")" -type f -size -9000k > "$rsyncFiles.diff" && \
+    comm -23 <(sort "$rsyncFiles") <(sort "$rsyncFiles".diff) > "$rsyncFiles.tmp" && \
+    cp -fv "$rsyncFiles.tmp" "$rsyncFiles" ; \
     fi && \
     if [ ${#include_params[@]} -gt 0 ] ; \
     then \
         find . "(" "${include_params[@]}" ")" -type f | sed 's/\(["'\''\]\)/\\\1/g' >> "$rsyncFiles" && \
         sort -u "$rsyncFiles" -o "$rsyncFiles" ; \
         fi && \
-        cp -fv "$rsyncFiles" rsync.files
+        cp -fv "$rsyncFiles" rsync.files && \ 
+        rm "$rsyncFiles" "$rsyncFiles.diff" "$rsyncFiles.tmp" "$rsyncFiles.bak" 
