4a10dbc2ff5821e9ad855d98c1e387a7a3237065
Ying Huang <huangyingw@gmail.com>
2020-10-16 20:36:28 -0700

    n

--- bashrc/cscope.sh
+++ bashrc/cscope.sh
@@ -1,1 +1,6 @@
 #!/bin/zsh
+
+cscope -bq -i files.proj -f cscope.out.bak && \
+    cp -fv cscope.out.bak cscope.out && \
+    cp -fv cscope.out.bak.in cscope.out.in && \
+    cp -fv cscope.out.bak.po cscope.out.po
