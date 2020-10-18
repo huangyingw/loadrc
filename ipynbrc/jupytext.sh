f28db29d19a3dfc8cbbe16d008b15b2d184847ee
Ying Huang <huangyingw@gmail.com>
2020-10-18 14:03:59 -0700

    n

--- ipynbrc/jupytext.sh
+++ ipynbrc/jupytext.sh
@@ -6,4 +6,11 @@ then
     TARGET="$1"
 fi
 
-find "$TARGET" -type f -name \*.ipynb -exec jupytext --sync {} \;
+find "$TARGET" -type f -name \*.ipynb | while read ss
+do
+    pyfile=$(echo "$ss" | sed 's/\.ipynb$/\.py/g')
+    rm "$pyfile"
+    jupytext --sync "$ss"
+    autopep8 --in-place "$pyfile"
+    sed -i.bak '/!pip install/d' "$pyfile"
+done
