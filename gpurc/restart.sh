21b47e0371da0acafc58f2165db2e12feb374abf
Ying Huang <huangyingw@gmail.com>
2020-11-03 15:41:07 -0800

    n

--- gpurc/restart.sh
+++ gpurc/restart.sh
@@ -1,1 +1,6 @@
 #!/bin/zsh
+
+ps ax | grep -v grep | grep nvidia-smi | awk '{print $1}' | xargs -n1 kill -9
+nvidia-smi | awk '/python/{print $5}' | xargs -n1 kill -9
+rmmod nvidia_uvm
+modprobe nvidia_uvm
