f873f684b0f0bd39dcce82e542e8b86e243c3dd6
Ying Huang <huangyingw@gmail.com>
2020-11-03 19:37:07 -0800

    n

--- gpurc/restart.sh
+++ gpurc/restart.sh
@@ -1,7 +1,8 @@
 #!/bin/zsh
 
-ps ax | grep -v grep | grep nvidia-smi | awk '{print $1}' | xargs -n1 kill -9
+# ps ax | grep -v grep | grep nvidia-smi | awk '{print $1}' | xargs -n1 kill -9
 nvidia-smi | awk '/python/{print $5}' | xargs -n1 kill -9
 # ps ax | grep -v grep | grep jupyter-notebook | awk '{print $1}' | xargs -n1 kill -9 
+# nvidia-smi --gpu-reset
 rmmod nvidia_uvm
 modprobe nvidia_uvm
