a59d5e6f4c6fd10cb84b2d160e87b2622b4ee2e7
Ying Huang <huangyingw@gmail.com>
2020-11-27 14:21:58 -0800

    n

--- gitrc/gdif.sh
+++ gitrc/gdif.sh
@@ -1,11 +1,33 @@
 #!/bin/zsh
 
+while [[ "$#" -gt 0 ]]
+do
+    case $1 in
+        -r | --reverse)
+            local reverse="true"
+            ;;
+        -b | --branch)
+            local branch="$2"
+            ;;
+        -f | --file)
+            local file="$2"
+            ;;
+    esac
+    shift
+done
+
 COMMAND=$(~/loadrc/gitrc/git_diff_command.sh)
-COMMAND="$COMMAND HEAD $1"
+
+if [[ "$reverse" == "true" ]]
+then
+    COMMAND="$COMMAND HEAD $branch"
+else
+    COMMAND="$COMMAND $branch"
+fi
 
 while read ss
 do
     COMMAND="$COMMAND  -- '$ss'"
-done < "$2"
+done < "$file"
 
 eval "$COMMAND"
