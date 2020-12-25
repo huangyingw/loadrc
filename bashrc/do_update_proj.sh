1cd86523fbe08bbee06ca7de40a2df4d31db75b8
Ying Huang <huangyingw@gmail.com>
2020-12-25 12:02:42 -0800

    n

--- bashrc/do_update_proj.sh
+++ bashrc/do_update_proj.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
-~/loadrc/bashrc/check_running.sh ~/loadrc/bashrc/generate_files_proj.sh &
-~/loadrc/bashrc/check_running.sh ~/loadrc/bashrc/fvideos.sh &
-~/loadrc/bashrc/check_running.sh ~/loadrc/bashrc/fdocs.sh &
-~/loadrc/bashrc/check_running.sh ~/loadrc/bashrc/cscope.sh &
-~/loadrc/bashrc/check_running.sh ~/loadrc/bashrc/generate_rsync_files.sh &
+. ~/loadrc/bashrc/check_running.sh ~/loadrc/bashrc/generate_files_proj.sh &
+. ~/loadrc/bashrc/check_running.sh ~/loadrc/bashrc/fvideos.sh &
+. ~/loadrc/bashrc/check_running.sh ~/loadrc/bashrc/fdocs.sh &
+. ~/loadrc/bashrc/check_running.sh ~/loadrc/bashrc/cscope.sh &
+. ~/loadrc/bashrc/check_running.sh ~/loadrc/bashrc/generate_rsync_files.sh &
