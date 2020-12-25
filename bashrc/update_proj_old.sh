f21a415b01f355f6a579f2c0d163ea68087fc935
Ying Huang <huangyingw@gmail.com>
2020-12-25 11:23:15 -0800

    n

--- bashrc/update_proj_old.sh
+++ bashrc/update_proj_old.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
 ~/loadrc/bashrc/find_up_goto.sh "files.proj"
-~/loadrc/bashrc/check_running.sh ~/loadrc/bashrc/cscope.sh
+~/loadrc/bashrc/check_running.sh ~/loadrc/bashrc/cscope_old.sh
