9a6cd51dbaaa4351d1e3fe78a07928e9940c8145
Ying Huang <huangyingw@gmail.com>
2021-02-13 11:48:26 -0800

    n

--- bashrc/update_proj.sh
+++ bashrc/update_proj.sh
@@ -1,7 +1,8 @@
 #!/bin/zsh
 
-~/loadrc/bashrc/generate_files_proj.sh &
-~/loadrc/bashrc/generate_rsync_files.sh &
-~/loadrc/bashrc/fvideos.sh &
-~/loadrc/bashrc/fdocs.sh &
-~/loadrc/bashrc/cscope.sh &
+. ~/loadrc/bashrc/find_up_goto.sh "files.proj"
+. ~/loadrc/bashrc/generate_files_proj.sh &
+. ~/loadrc/bashrc/generate_rsync_files.sh &
+. ~/loadrc/bashrc/fvideos.sh &
+. ~/loadrc/bashrc/fdocs.sh &
+. ~/loadrc/bashrc/cscope.sh &
