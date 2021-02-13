8f7ecb82aae27b775e144f5b977695b951f39134
Ying Huang <huangyingw@gmail.com>
2021-02-13 11:07:31 -0800

    n

--- bashrc/cscope.sh
+++ bashrc/cscope.sh
@@ -1,7 +1,11 @@
 #!/bin/zsh
 
+SOURCEFILE="cscopesourcefile.bak"
 CSCOPEFILE="cscopefile.bak"
-cscope -bq -i files.proj -f "$CSCOPEFILE" && \
+
+cp -fv files.proj "$SOURCEFILE" && \
+    sed -i.bak 's/\\ / /g' "$SOURCEFILE" && \
+    cscope -bq -i "$SOURCEFILE" -f "$CSCOPEFILE" && \
     cp -fv "$CSCOPEFILE" cscope.out && \
     cp -fv "$CSCOPEFILE".in cscope.out.in && \
     cp -fv "$CSCOPEFILE".po cscope.out.po
