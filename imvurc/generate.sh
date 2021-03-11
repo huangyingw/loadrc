c7c549e7efcefc43daa2751022a0fe127c01cbe8
Ying Huang <huangyingw@gmail.com>
2021-03-10 17:17:42 -0800

    n

--- imvurc/generate.sh
+++ imvurc/generate.sh
@@ -1,11 +1,12 @@
 #!/bin/zsh
-file="$1"
+
+file=$(realpath "$1")
 target="$1.sh"
 
 if [ ! -f "$target" ]
 then
     cp -nv ~/loadrc/imvurc/generate_sh.template "$target"
-    rootFolder=$(~/loadrc/bashrc/find_up_folder.sh "$PWD/$file" "files.proj")
+    rootFolder=$(~/loadrc/bashrc/find_up_folder.sh "$file" "files.proj")
     relative_path=$(realpath --relative-to="$rootFolder" "$file")
     relative_path=$(echo "$relative_path" | sed 's/\//\\\//g')
     sed -i.bak "s/relative_path_var/$relative_path/g" "$target"
