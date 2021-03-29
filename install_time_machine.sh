15487c603407a54fa4841c4ebea52c2c8ae5001f
Ying Huang <huangyingw@gmail.com>
2021-03-29 10:56:09 -0700

    n

--- install_time_machine.sh
+++ install_time_machine.sh
@@ -4,3 +4,10 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
 apt install -y netatalk avahi-daemon
+cp -fv ./etc/netatalk/afp.conf /etc/netatalk/afp.conf
+
+mkdir -p /srv/netatalk/time-machine
+chown nobody:nogroup /srv/netatalk/time-machine
+chmod -R 777 /srv/netatalk/time-machine
+
+service netatalk restart
