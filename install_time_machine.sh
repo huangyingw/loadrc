824e0cdc6d7ebada489d44e42c1a52c131b9276a
Ying Huang <huangyingw@gmail.com>
2021-03-29 11:31:28 -0700

    n

--- install_time_machine.sh
+++ install_time_machine.sh
@@ -3,11 +3,14 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
+TARGET_DIR="/srv/netatalk/time-machine"
+TARGET_DIR_FOR_SED=$(echo "$TARGET_DIR" | sed 's/\//\\\//g')
 apt install -y netatalk avahi-daemon
 cp -fv ./etc/netatalk/afp.conf /etc/netatalk/afp.conf
+sed -i.bak "s/target_dir_var/$TARGET_DIR_FOR_SED/g" /etc/netatalk/afp.conf
 
-mkdir -p /srv/netatalk/time-machine
-chown nobody:nogroup /srv/netatalk/time-machine
-chmod -R 777 /srv/netatalk/time-machine
+mkdir -p "$TARGET_DIR"
+chown nobody:nogroup "$TARGET_DIR"
+chmod -R 777 "$TARGET_DIR"
 
 service netatalk restart
