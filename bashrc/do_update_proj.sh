99404b6dccd295bf9ab00939ea1f1a665712d556
Ying Huang <huangyingw@gmail.com>
2021-03-29 17:24:07 -0700

    n

--- bashrc/do_update_proj.sh
+++ bashrc/do_update_proj.sh
@@ -1,11 +1,5 @@
 #!/bin/zsh
 
-if [ ! -f files.proj ]
-then
-    echo -e "${red}No files.proj file here, will not update the proj ... ${NC}"
-    exit 1
-fi
-
 ~/loadrc/bashrc/generate_files_proj.sh
 ~/loadrc/bashrc/generate_rsync_files.sh
 ~/loadrc/bashrc/fvideos.sh
