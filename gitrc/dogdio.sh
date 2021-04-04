87b70132d46f34154bac59bf37a991dc8356c281
Ying Huang <huangyingw@gmail.com>
2021-04-04 13:58:11 -0700

    n

--- gitrc/dogdio.sh
+++ gitrc/dogdio.sh
@@ -7,9 +7,9 @@ fi
 local_branch="$(~/loadrc/gitrc/get_current_branch.sh)" 
 output="$local_branch.gdio.diff"
 output=$(echo "$output" | sed 's/\//_/g')
-local_master="$(git config gsync.branch)"
+remote_branch="$(git config gsync.branch)"
 remote="$(git config gsync.remote)"
 
-~/loadrc/gitrc/gdi.sh "$remote/$local_master" 2>&1 | tee "$output"  
+~/loadrc/gitrc/gdi.sh "$remote/$remote_branch" 2>&1 | tee "$output"  
 sed -i.bak "s/^--- a\//--- \.\//g;s/^+++ b\//+++ \.\//g" "$output"
 exit 0
