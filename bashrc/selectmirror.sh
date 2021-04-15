862f9c16ea2364b28d88a0f0111ca0ed4705b3d2
Ying Huang <huangyingw@gmail.com>
2021-04-14 22:36:38 -0700

    n

--- bashrc/selectmirror.sh
+++ bashrc/selectmirror.sh
@@ -9,4 +9,12 @@ fi
 SOURCE="$1"
 TARGET="$2"
 
+ready_file="$SOURCE/tselectmirror.ready"
+
+if [ ! -f ${ready_file} ]
+then
+    echo -e "${red}tmirror must be run before mirror ... ${NC}"
+    exit 1
+fi
+
 rsync -aHv --exclude-from="$SOURCE"/excludeFile --delete-during --force "$SOURCE"/  "$TARGET"/
