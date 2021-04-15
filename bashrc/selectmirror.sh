9b2a6da6a2bbc554be4af312ff5834ae141afac6
Ying Huang <huangyingw@gmail.com>
2021-04-14 22:32:16 -0700

    n

--- bashrc/selectmirror.sh
+++ bashrc/selectmirror.sh
@@ -9,4 +9,4 @@ fi
 SOURCE="$1"
 TARGET="$2"
 
-rsync -aHv --exclude-from=excludeFile --delete-during --force "$SOURCE"/  "$TARGET"/ | tee selectMirror.findresult
+rsync -aHv --exclude-from="$SOURCE"/excludeFile --delete-during --force "$SOURCE"/  "$TARGET"/
