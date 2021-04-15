8171f25b5d38f38748e83bbb326ed9af81a85223
Ying Huang <huangyingw@gmail.com>
2021-04-14 22:23:36 -0700

    n

--- bashrc/selectmirror.sh
+++ bashrc/selectmirror.sh
@@ -5,6 +5,8 @@ then
   echo -e "${red}Please specify enough parameter ... ${NC}"
   exit 1
 fi
+
 SOURCE="$1"
 TARGET="$2"
+
 rsync -aHv --exclude-from=excludeFile --delete-during --force "$SOURCE"/  "$TARGET"/ | tee selectMirror.findresult
