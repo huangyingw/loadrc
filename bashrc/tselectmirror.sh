fb5c073bb2a517c577b349379ebdecef10bb3ad3
Ying Huang <huangyingw@gmail.com>
2021-04-14 22:37:09 -0700

    n

--- bashrc/tselectmirror.sh
+++ bashrc/tselectmirror.sh
@@ -5,6 +5,9 @@ then
   echo -e "${red}Please specify enough parameter ... ${NC}"
   exit 1
 fi
+
 SOURCE="$1"
 TARGET="$2"
-rsync -aHvn --exclude-from=excludeFile --delete-during --force "$SOURCE"/  "$TARGET"/ | tee tselectMirror.findresult
+ready_file="$SOURCE/tselectmirror.ready"
+
+rsync -aHvn --exclude-from="$SOURCE"/excludeFile --delete-during --force "$SOURCE"/  "$TARGET"/  > "$ready_file" 
