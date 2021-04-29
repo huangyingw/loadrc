ee5e79294fd2a6c01749df0c12321a2961ee342b
Ying Huang <huangyingw@gmail.com>
2021-04-28 21:32:25 -0700

    n

--- install_basic_macos.sh
+++ install_basic_macos.sh
@@ -10,6 +10,7 @@ fi
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
+brew upgrade
 brew install --cask osxfuse
 brew install \
     Caskroom/cask/xquartz \
