3a095086ca02ca91b39dc68c7f44a57221f2e15f
Ying Huang <huangyingw@gmail.com>
2021-04-29 17:54:07 -0700

    n

--- gitrc/apply_branch_test.sh
+++ gitrc/apply_branch_test.sh
@@ -3,6 +3,5 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-# ./apply_branch.sh  hypo
-./apply_branch.sh remotes/github/generate_files_proj
-# ./apply_branch.sh remotes/github/yhuang/RunShell
+cd ../
+~/loadrc/gitrc/apply_branch.sh remotes/github/generate_files_proj
