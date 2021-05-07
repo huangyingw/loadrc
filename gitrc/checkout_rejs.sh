eba743fd1ec1cad8edde264dfe553b1bec1421e4
Ying Huang <huangyingw@gmail.com>
2021-05-07 10:15:05 -0700

    n

--- gitrc/checkout_rejs.sh
+++ gitrc/checkout_rejs.sh
@@ -2,6 +2,6 @@
 for ss in $(git status | grep \.rej$) ; \
 do \
     targetFile=$(echo "$ss" | sed 's/\.rej$//g')
-    git show "$1":"$targetFile" > "$targetFile" && \
+    git checkout "$1" "$targetFile" && \
         rm "$ss"
 done
