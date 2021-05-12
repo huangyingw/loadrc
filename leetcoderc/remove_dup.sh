54797b404f0679e2cefbba4b6642142000e72569
Ying Huang <huangyingw@gmail.com>
2021-05-11 22:42:59 -0700

    n

--- leetcoderc/remove_dup.sh
+++ leetcoderc/remove_dup.sh
@@ -1,8 +1,7 @@
 #!/bin/zsh
+NUM="$1"
+cd "$NUM"
 
-while true
-do
-    ~/loadrc/leetcoderc/remove_gabages.sh
-    find . -type f \( -name \*.orig -o -name \*.bak \) -delete
-    jdupes -1dNr submissions/
-done
+~/loadrc/leetcoderc/remove_gabages.sh
+find . -type f \( -name \*.orig -o -name \*.bak \) -delete
+jdupes -1dNr .
