f8163e156dd4f5ad6b6f2a8048cba92abc3d15c0
Ying Huang <huangyingw@gmail.com>
2021-05-11 22:42:32 -0700

    n

--- leetcoderc/remove_gabages.sh
+++ leetcoderc/remove_gabages.sh
@@ -1,1 +1,12 @@
 #!/bin/zsh
+
+find . -type f -name \*.py | while read ss
+do
+    autopep8 --in-place "$ss"
+    if $(~/loadrc/pythonrc/remove_comments.py "$ss")
+    then
+        mv -fv "$ss.strip" "$ss"
+        sed -i.bak '/print.*(/d;/^_author_/d;/__main__/d;/ = Solution()/d;/^_project_/d;/\bprint\b/d;s/#--//g;s/##//g;s/ \/= /\/\/=/g;s/ \/ /\/\//g;/^$/d;/^\s*$/d' "$ss"
+        autopep8 --in-place "$ss"
+    fi
+done
