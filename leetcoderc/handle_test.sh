8b30a4049503c3aad7135a9cf757d5f276c2f042
Ying Huang <huangyingw@gmail.com>
2021-05-11 20:46:59 -0700

    n

--- leetcoderc/handle_test.sh
+++ leetcoderc/handle_test.sh
@@ -3,4 +3,4 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-./handle.sh /Users/huangyingw/leetcode-cli/submissions/1.two-sum.260837994.Accepted.leetcode.py 
+./handle.sh /Users/huangyingw/leetcode-cli/submissions/1.two-sum.260837994.Accepted.leetcode.py
