afc2a5d8422a6b7ab54015c0ddfe8199ec0c28cb
Ying Huang <huangyingw@gmail.com>
2021-05-11 22:23:36 -0700

    n

--- leetcoderc/handle.sh
+++ leetcoderc/handle.sh
@@ -4,3 +4,4 @@ FILE="$1"
 ~/loadrc/leetcoderc/submit.sh "$FILE"
 NUM=$(echo "$FILE" | sed 's/.*\/\([0-9]*\)\..*/\1/g') 
 ~/loadrc/bashrc/check_running.sh ~/loadrc/leetcoderc/submission.sh "$NUM"
+~/loadrc/leetcoderc/restore.sh "$NUM"
