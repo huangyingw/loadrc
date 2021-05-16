860ac2f05bf25296c113c1f4db01331dd83b48d1
Ying Huang <huangyingw@gmail.com>
2021-05-15 22:09:51 -0700

    n

--- wifirc/autoconnect.sh
+++ wifirc/autoconnect.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-wlan=`/sbin/ifconfig wlan0 | grep inet\ addr | wc -l`
-if [ $wlan -eq 0 ]; then
-    service network-manager restart
-else
-    echo WIFI IS UP
+# find the uuid by nmcli con show 
+# and put this script, which is owned by root and executable, in the folder: /etc/NetworkManager/dispatcher.d
+
+if [ "$CONNECTION_UUID" = "df31faf5-3440-49ec-be88-9e040176663e" ]
+then
+    if [ "$2" = "down" ]; then
+        sleep 10
+        nmcli con up uuid $CONNECTION_UUID
+    fi
 fi
