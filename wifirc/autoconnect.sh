714524415e47723f634f9589c9ac4e6dccea6479
Ying Huang <huangyingw@gmail.com>
2021-05-18 21:31:57 -0700

    n

--- wifirc/autoconnect.sh
+++ wifirc/autoconnect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# find the uuid by nmcli con show
+# find the uuid by nmcli con show 
 # and put this script, which is owned by root and executable, in the folder: /etc/NetworkManager/dispatcher.d
 
 if [ "$CONNECTION_UUID" = "df31faf5-3440-49ec-be88-9e040176663e" ]
