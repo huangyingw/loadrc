6df3ab7df2e6a880784152820892654eb71be209
Ying Huang <huangyingw@gmail.com>
2021-05-18 21:30:28 -0700

    n

---
+++ wifirc/autoconnect.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# find the uuid by nmcli con show
+# and put this script, which is owned by root and executable, in the folder: /etc/NetworkManager/dispatcher.d
+
+if [ "$CONNECTION_UUID" = "df31faf5-3440-49ec-be88-9e040176663e" ]
+then
+    if [ "$2" = "down" ]; then
+        sleep 10
+        nmcli con up uuid $CONNECTION_UUID
+    fi
+fi
