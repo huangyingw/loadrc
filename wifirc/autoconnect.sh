5a0a4319c6483e19830fed5753a4c0918f911d78
Ying Huang <huangyingw@gmail.com>
2021-05-18 21:38:53 -0700

    n

--- wifirc/autoconnect.sh
+++ wifirc/autoconnect.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# find the uuid by nmcli con show 
+# find the uuid by nmcli con show
 # and put this script, which is owned by root and executable, in the folder: /etc/NetworkManager/dispatcher.d
+
+# the following command to get the wifi device:
+# for dev in `ls /sys/class/net`; do
+#     [ -d "/sys/class/net/$dev/wireless" ] && echo "$dev"
+# done
+
 
 if [ "$CONNECTION_UUID" = "df31faf5-3440-49ec-be88-9e040176663e" ]
 then
