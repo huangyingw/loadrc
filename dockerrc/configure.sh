c7e9536c47b46a3098a6a6b160b9cd78da631d8e
Ying Huang <huangyingw@gmail.com>
2021-05-24 23:06:12 -0700

    n

--- dockerrc/configure.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-SCRIPT=$(realpath "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
-cd "$SCRIPTPATH"
-
-mkdir -p /media/nvme/var/lib/docker/
-service docker stop
-~/loadrc/bashrc/mklnk.sh /var/lib/docker/ /media/nvme/var/lib/docker/
--- kvmrc/configure.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-SCRIPT=$(realpath "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
-cd "$SCRIPTPATH"
-
-mkdir -p /media/nvme/var/lib/libvirt/
-mkdir -p /media/nvme/etc/libvirt/
-~/loadrc/bashrc/mklnk.sh /var/lib/libvirt/ /media/nvme/var/lib/libvirt/
-~/loadrc/bashrc/mklnk.sh /etc/libvirt/ /media/nvme/etc/libvirt/
