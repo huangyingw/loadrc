f8e84bea4e2ddbd2ebbaacd637c6201d99ba03e4
Ying Huang <huangyingw@gmail.com>
2021-06-11 13:50:01 -0700

    n

--- gitrc/g.sh
+++ gitrc/g.sh
@@ -1,7 +1,19 @@
 #!/bin/zsh
 ~/loadrc/gitrc/include_gitconfig.sh
+
+commit_message="n"
+
+if [ -f COMMIT_EDITMSG ]
+then
+    commit_message=$(cat COMMIT_EDITMSG)
+fi
+
+if [ -n "$1" ]
+then
+    commit_message="$1"
+fi
+
 git commit --no-edit || \
-    git commit -m "$1" || \
-    git commit -m n
+    git commit -m "$commit_message"
 
 ~/loadrc/gitrc/postCommit.sh
