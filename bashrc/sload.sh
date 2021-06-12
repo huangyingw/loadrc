de012e7707037918ebff7d0d734c175ea4a5759f
Ying Huang <huangyingw@gmail.com>
2021-06-12 14:23:13 -0700

    n

--- bashrc/sload.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh 
-if ping -c 1 macpro &> /dev/null
-then
-  sshfs -o nonempty huangyingw@macpro:~/Dropbox/loadrc/ ~/loadrc/
-  df -TH
-  . $HOME/loadrc/.loadrc
-else
-  . $HOME/loadrc/.loadrc
-fi
