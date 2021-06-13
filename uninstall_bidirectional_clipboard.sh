52c37e391723fb2f0b531c50b5dfbb588460953d
Ying Huang <huangyingw@gmail.com>
2021-06-13 14:40:49 -0700

    n

--- uninstall_bidirectional_clipboard.sh
+++ uninstall_bidirectional_clipboard.sh
@@ -3,10 +3,11 @@ SCRIPT=$(realpath "$0") && \
     SCRIPTPATH=$(dirname "$SCRIPT") && \
     cd "$SCRIPTPATH" || cd ~/loadrc/
 
-cp -fv ./Library/LaunchAgents/pbcopy.plist ~/Library/LaunchAgents/pbcopy.plist
-cp -fv ./Library/LaunchAgents/pbpaste.plist ~/Library/LaunchAgents/pbpaste.plist
+launchctl stop ~/Library/LaunchAgents/pbcopy.plist
+launchctl stop ~/Library/LaunchAgents/pbpaste.plist
 
-launchctl load ~/Library/LaunchAgents/pbcopy.plist
-launchctl load ~/Library/LaunchAgents/pbpaste.plist
-launchctl start ~/Library/LaunchAgents/pbcopy.plist
-launchctl start ~/Library/LaunchAgents/pbpaste.plist
+launchctl unload ~/Library/LaunchAgents/pbcopy.plist
+launchctl unload ~/Library/LaunchAgents/pbpaste.plist
+
+rm ~/Library/LaunchAgents/pbcopy.plist
+rm ~/Library/LaunchAgents/pbpaste.plist
