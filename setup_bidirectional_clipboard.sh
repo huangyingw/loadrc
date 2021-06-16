a1c78d1b6aca8837e1c46f66eda9f03dd2e00b5d
Ying Huang <huangyingw@gmail.com>
2021-06-15 21:51:20 -0700

    n

--- setup_bidirectional_clipboard.sh
+++ setup_bidirectional_clipboard.sh
@@ -3,6 +3,11 @@ SCRIPT=$(realpath "$0") && \
     SCRIPTPATH=$(dirname "$SCRIPT") && \
     cd "$SCRIPTPATH" || cd ~/loadrc/
 
+if [ -f ~/Library/LaunchAgents/pbcopy.plist  ]
+then
+    exit 0    
+fi
+
 cp -fv ./Library/LaunchAgents/pbcopy.plist ~/Library/LaunchAgents/pbcopy.plist
 cp -fv ./Library/LaunchAgents/pbpaste.plist ~/Library/LaunchAgents/pbpaste.plist
 
