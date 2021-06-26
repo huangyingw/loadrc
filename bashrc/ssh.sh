f548c06d8e885358b384fc2fae529d5698915558
Ying Huang <huangyingw@gmail.com>
2021-06-25 22:47:08 -0700

    n

--- bashrc/ssh.sh
+++ bashrc/ssh.sh
@@ -1,11 +1,4 @@
 #! /bin/zsh
 target=$(echo "$1" | sed  -e "s/\/$//g")
-
-if ( ssh "$target" "uname" | grep -q Darwin )
-then
-    tmuxAction='/usr/local/bin/tmux new-session -A -s ssh'
-else
-    tmuxAction='/usr/bin/tmux new-session -A -s ssh'
-fi
-
+tmuxAction="$(ssh "$target" ". ~/loadrc/.loadrc ; which tmux") new-session -A -s ssh"
 ssh -Y "$target" -t -- ${tmuxAction}
