b612b4305c6706fdc433a66241fe7552d143f38d
Ying Huang <huangyingw@gmail.com>
2021-06-25 22:17:16 -0700

    n

--- bashrc/mosh.sh
+++ bashrc/mosh.sh
@@ -2,14 +2,8 @@
 target=$(echo "$1" | sed  -e "s/\/$//g")
 mkdir "$target"
 
-if ( ssh "$target" "uname" | grep -q Darwin )
-then
-    MOSHSERVER=$(ssh "$target" ". ~/loadrc/.loadrc ; which mosh-server")
-    tmuxAction="$(ssh "$target" ". ~/loadrc/.loadrc ; which tmux") new-session -A -s mosh"
-else
-    MOSHSERVER="/usr/bin/mosh-server"
-    tmuxAction='/usr/bin/tmux new-session -A -s mosh'
-fi
+MOSHSERVER=$(ssh "$target" ". ~/loadrc/.loadrc ; which mosh-server")
+tmuxAction="$(ssh "$target" ". ~/loadrc/.loadrc ; which tmux") new-session -A -s mosh"
 
 while true
 do
