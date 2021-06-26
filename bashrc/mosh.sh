502f44662e4a2da6dea5e941629982cc37632229
Ying Huang <huangyingw@gmail.com>
2021-06-26 15:52:30 -0700

    n

--- bashrc/mosh.sh
+++ bashrc/mosh.sh
@@ -2,12 +2,12 @@
 target=$(echo "$1" | sed  -e "s/\/$//g")
 mkdir "$target"
 
-MOSHSERVER=$(ssh "$target" ". ~/loadrc/.loadrc ; which mosh-server")
-tmuxAction="$(ssh "$target" ". ~/loadrc/.loadrc ; which tmux") new-session -A -s mosh"
+MOSHSERVER=$(ssh "$target" ". ~/loadrc/.pathrc ; which mosh-server")
+tmuxAction="$(ssh "$target" ". ~/loadrc/.pathrc ; which tmux") new-session -A -s mosh"
 
 while true
 do
     ssh -fnN -R 2224:localhost:2224 "$target" &
-    ssh "$target" ". ~/loadrc/.loadrc ; ~/loadrc/macosrc/configure_mosh.sh" &
+    ssh "$target" ". ~/loadrc/.pathrc ; ~/loadrc/macosrc/configure_mosh.sh" &
     mosh --server="$MOSHSERVER" "$target" -- ${tmuxAction}
 done
