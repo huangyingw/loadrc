fc30c8fa47caf8d38ec950bfc7a668be6ce85303
Ying Huang <huangyingw@gmail.com>
2021-06-27 12:13:28 -0700

    n

--- bashrc/mosh.sh
+++ bashrc/mosh.sh
@@ -5,9 +5,15 @@ mkdir "$target"
 MOSHSERVER=$(ssh "$target" ". ~/loadrc/.pathrc ; which mosh-server")
 tmuxAction="$(ssh "$target" ". ~/loadrc/.pathrc ; which tmux") new-session -A -s mosh"
 
+if [ -z "$MOSHSERVER" ]
+then
+    echo -e "${red}connection to $target failed... ${NC}" 
+    exit 1
+fi
+
 while true
 do
     ssh -fnN -R 2224:localhost:2224 "$target" &
     ssh "$target" ". ~/loadrc/.pathrc ; ~/loadrc/macosrc/configure_mosh.sh" &
-    mosh --server="$MOSHSERVER" "$target" -- ${tmuxAction}
+    mosh --server="$MOSHSERVER" "$target" -- "$tmuxAction"
 done
