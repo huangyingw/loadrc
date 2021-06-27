701eef837aecf598d1394422ff639f643beb61ba
Ying Huang <huangyingw@gmail.com>
2021-06-27 14:41:44 -0700

    n

--- gitrc/gcom.sh
+++ gitrc/gcom.sh
@@ -1,3 +1,11 @@
 #!/bin/zsh
-~/loadrc/gitrc/gme2.sh "$1" && \
+
+if [ -z "$1" ]
+then
+    echo -e "${red}Please provide target_branch... ${NC}"
+    exit 1
+fi
+
+git checkout -b "$1" || \
+    ~/loadrc/gitrc/gme2.sh "$1" && \
     git checkout "$1"
