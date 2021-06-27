4ee46e4c9e0eb7c73d21fb3617230d4304daf184
Ying Huang <huangyingw@gmail.com>
2021-06-27 12:39:12 -0700

    n

--- bashrc/mosh.sh
+++ bashrc/mosh.sh
@@ -5,9 +5,9 @@ mkdir "$target"
 MOSHSERVER=$(ssh "$target" ". ~/loadrc/.pathrc ; which mosh-server")
 tmuxAction="$(ssh "$target" ". ~/loadrc/.pathrc ; which tmux") new-session -A -s mosh"
 
-if [ -z "$MOSHSERVER" ] || [ -z "$tmuxAction" ] 
+if ( nc -vz "$target" 22 2>&1 | grep -q succeeded ) && [ -z "$MOSHSERVER" ] || [ -z "$tmuxAction" ]
 then
-    echo -e "${red}please check connection, mosh and tmux setting of $target first... ${NC}" 
+    echo -e "${red}please check connection, mosh and tmux setting of $target first... ${NC}"
     exit 1
 fi
 
