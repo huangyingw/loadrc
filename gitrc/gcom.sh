8485a97183b78a61df63c44bf87ccfd7b0d01335
Ying Huang <huangyingw@gmail.com>
2021-06-29 12:54:50 -0700

    n

--- gitrc/gcom.sh
+++ gitrc/gcom.sh
@@ -6,8 +6,20 @@ then
     exit 1
 fi
 
+clean=$(git status --porcelain)
+
+if [ -n "$clean" ]
+then
+    git stash
+fi
+
 ~/loadrc/gitrc/discard_unnecessaries.sh
 CURRENT_BRANCH=$(~/loadrc/gitrc/get_current_branch.sh)
 git checkout -b "$1" || \
     git checkout "$1" &&
     git merge "$CURRENT_BRANCH"
+
+if [ -n "$clean" ]
+then
+    git stash pop stash@{0}
+fi
