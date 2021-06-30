cde659e5cc9fd120cf6b2af2bf42d2111902b7a9
Ying Huang <huangyingw@gmail.com>
2021-06-29 21:41:20 -0700

    n

--- gitrc/reapply.sh
+++ gitrc/reapply.sh
@@ -1,6 +1,12 @@
 #!/bin/zsh
 DIFF="$1"
-git apply --reverse "$DIFF" 
-git checkout -- "$DIFF"
+CURRET_BRANCH_DIFF="$(~/loadrc/gitrc/get_current_branch.sh).gdio.diff"
+git apply --reverse "$CURRET_BRANCH_DIFF" 
+git checkout -- "$CURRET_BRANCH_DIFF"
 git apply --reject --whitespace=fix "$DIFF"
 ~/loadrc/gitrc/gdio.sh
+
+if [ "$CURRET_BRANCH_DIFF" = "$DIFF" ]
+then
+    git add "$CURRET_BRANCH_DIFF"
+fi
