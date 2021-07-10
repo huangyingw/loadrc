4a810de7be7d3911ffe0c990e56cb4c92ed7026c
Ying Huang <huangyingw@gmail.com>
2021-07-10 11:08:10 -0700

    n

--- gitrc/gbak.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-current_branch="$(~/loadrc/gitrc/get_current_branch.sh)"
-target_branch=$(echo "$current_branch" | sed 's/\.fix$//g').bak
-remote="$(git config gsync.remote)"
-remote_branch="$(git config gsync.branch)"
-~/loadrc/gitrc/gbrd.sh "$target_branch"
-git checkout -b "$target_branch" "$remote/$remote_branch"
