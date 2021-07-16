11e67c907c5e859d23d03ddee9de8d1f11f08191
Ying Huang <huangyingw@gmail.com>
2021-07-15 22:10:58 -0700

    n

--- gitrc/dogdio.sh
+++ gitrc/dogdio.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
-if [ -z $(git config gsync.remote) ] || [ -z $(git config gsync.branch) ] 
+
+if [ -z $(git config gsync.target) ]
 then
     exit 0
 fi
@@ -7,9 +8,8 @@ fi
 local_branch="$(~/loadrc/gitrc/get_current_branch.sh)" 
 output="$local_branch.gdio.diff"
 output=$(echo "$output" | sed 's/\//_/g')
-remote_branch="$(git config gsync.branch)"
-remote="$(git config gsync.remote)"
+target="$(git config gsync.target)"
 
-~/loadrc/gitrc/gdi.sh "$remote/$remote_branch" 2>&1 | tee "$output"  
+~/loadrc/gitrc/gdi.sh "$target" 2>&1 | tee "$output"
 sed -i.bak "s/^--- a\//--- \.\//g;s/^+++ b\//+++ \.\//g" "$output"
 exit 0
