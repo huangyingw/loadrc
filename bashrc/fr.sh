e7a4ecf8a1b499283351507c144409a111c4c0d1
Ying Huang <huangyingw@gmail.com>
2021-08-05 15:32:57 -0700

    n

--- bashrc/fr.sh
+++ bashrc/fr.sh
@@ -1,4 +1,11 @@
 #!/bin/zsh
+
 FIND=$(echo $1 | sed 's/\//\\\//g')
 REPLACE=$(echo $2 | sed 's/\//\\\//g')
-xargs sed -i.bak  "s/${FIND}/${REPLACE}/g" < "$3"
+
+if [[ "$4" == "w" ]]
+then
+    xargs sed -i.bak "s/[[:<:]]${FIND}[[:>:]]/${REPLACE}/g" < "$3"
+else
+    xargs sed -i.bak  "s/${FIND}/${REPLACE}/g" < "$3"
+fi
