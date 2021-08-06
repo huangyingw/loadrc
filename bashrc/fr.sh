7efebd3032f88ed218d3805f6145d19824a8a1de
Ying Huang <huangyingw@gmail.com>
2021-08-05 20:35:42 -0700

    n

--- bashrc/fr.sh
+++ bashrc/fr.sh
@@ -5,7 +5,12 @@ REPLACE=$(echo $2 | sed 's/\//\\\//g')
 
 if [[ "$4" == "w" ]]
 then
-    xargs sed -i.bak "s/[[:<:]]${FIND}[[:>:]]/${REPLACE}/g" < "$3"
+    if [ $(uname) = "Darwin" ]
+    then
+        xargs sed -i.bak "s/[[:<:]]${FIND}[[:>:]]/${REPLACE}/g" < "$3"
+    else
+        xargs sed -i.bak "s/\b${FIND}\b/${REPLACE}/g" < "$3"
+    fi
 else
     xargs sed -i.bak  "s/${FIND}/${REPLACE}/g" < "$3"
 fi
