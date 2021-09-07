542a7c079d6b9033948d817c162d56404600afcf
Ying Huang <huangyingw@gmail.com>
2021-09-07 12:41:37 -0700

    n

--- gitrc/gcl.sh
+++ gitrc/gcl.sh
@@ -18,8 +18,11 @@ urlVar=$(echo "$urlVar" | sed 's/\//\\\//g')
 sed -i.bak "s|urlVar|$urlVar|g;s|repoVar|${author}_${repo}|g;s|authorVar|$author|g" "$target"/.gitconfig
 echo ".gitconfig --> $(realpath ${target}/.gitconfig)"
 
-cd "$target" && \
-    git remote remove origin ; \
+cd "$target"
+CURRENT_BRANCH=$(~/loadrc/gitrc/get_current_branch.sh)
+sed -i.bak "s|branchVar|$CURRENT_BRANCH|g" .gitconfig
+
+git remote remove origin ; \
     $HOME/loadrc/gitrc/gcob.sh dev && \
     $HOME/loadrc/gitrc/gdev.sh && \
     $HOME/loadrc/bashrc/do_update_proj.sh && \
