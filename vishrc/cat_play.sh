a74f20d6a54b84c6c1288e64857de12c8ad0d1ee
Ying Huang <huangyingw@gmail.com>
2021-09-07 12:03:13 -0700

    n

--- vishrc/cat_play.sh
+++ vishrc/cat_play.sh
@@ -5,7 +5,8 @@ do
     while read ss
     do
         ss=$(echo "$ss" | sed 's/"//g')
-        echo "now playing $ss"
+        echo "now playing"
+        echo "$ss"
         ~/loadrc/vishrc/vlc.sh "$ss"
     done < "$1"
 done
