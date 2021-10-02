2f3a9ab53f8354fab09d9738c15ac7bc2944face
Ying Huang <huangyingw@gmail.com>
2021-10-01 18:20:47 -0700

    n

--- vishrc/cat_rate.sh
+++ vishrc/cat_rate.sh
@@ -2,6 +2,7 @@
 
 while read ss
 do
-    target=$(echo "$ss" | sed "s/\/[6-9]\s\([^\/]*$\)/\/\1/g;s/\/\([^\/]*$\)/\/$2 \1/g;s/  \+/ /g")
-    mv -nv "$ss" "$target"
+    source=$(echo "$ss" | sed 's/"//g')
+    target=$(echo "$ss" | sed "s/\/[6-9]\s\([^\/]*$\)/\/\1/g;s/\/\([^\/]*$\)/\/$2 \1/g;s/  \+/ /g;s/\"//g")
+    mv -nv "$source" "$target"
 done < "$1"
