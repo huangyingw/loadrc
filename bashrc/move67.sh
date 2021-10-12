5ba5f55fd28d26fad2392281e8021977b63b9063
Ying Huang <huangyingw@gmail.com>
2021-10-12 11:41:23 -0700

    n

--- bashrc/move67.sh
+++ bashrc/move67.sh
@@ -2,11 +2,13 @@
 function move_zarchive()
 {
     num=$1
-    find /media/volgrp/mirror1/av/ -type f -size +100M -name $num\* | while read ss; do dir=$(echo "$ss" | sed 's/\/mirror1\//\/zarchive\//g'); mkdir -p "$(dirname "$dir")";mv -v "$ss" "$(dirname "$dir")"; done
+    find /media/volgrp/mirror1/av/ -type f -size +100M -name $num\* | while read ss
+    do 
+        dir=$(echo "$ss" | sed 's/\/mirror1\//\/zarchive\//g'); mkdir -p "$(dirname "$dir")";mv -v "$ss" "$(dirname "$dir")"
+    done
 }
 
 for (( c=6; c<=8; c++ ))
 do
-    echo "Welcome $c times"
     move_zarchive "$c"
 done
