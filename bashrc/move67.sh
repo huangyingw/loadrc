9659d9ee65d82e0493b59c3d065fd79eb13ee8e7
Ying Huang <huangyingw@gmail.com>
2021-10-16 14:47:59 -0700

    n

--- bashrc/move67.sh
+++ bashrc/move67.sh
@@ -7,18 +7,4 @@
 # done
 source="/media/volgrp/mirror1/av/Ayami Shunka 旬果/8 uncensored 声音不同步 自慰 啪啪有声 潮吹 高跟鞋 (無修正流出) FC2PPV-881903 Shunka Ayami あやみ旬果 Uncensored Leaked 無碼流出 (ABP-557).mp4"
 echo "$source"
-# echo "$source" | sed 's/\/mirror1\/*/zarchive/g'
-# echo "$source" | sed 's/\/mirror1\/*\//zarchive/g'
-# echo "$source" | sed 's/\/mirror1*\/[6-8]/zarchive/g'
-# echo "$source" | sed 's/\/mirror1*\//zarchive/g'
-# echo "$source" | sed 's/\/mirror1*[6-8]\s/zarchive/g'
-echo "$source" | sed 's/\/mirror1\(\/.*\/[6-8]\s\)/\/zarchive\1/g'
-
-source="/media/volgrp/mirror1/av/Ayami Shunka 旬果/9 uncensored 声音不同步 自慰 啪啪有声 潮吹 高跟鞋 (無修正流出) FC2PPV-881903 Shunka Ayami あやみ旬果 Uncensored Leaked 無碼流出 (ABP-557).mp4"
-echo "$source"
-# echo "$source" | sed 's/\/mirror1\/*/zarchive/g'
-# echo "$source" | sed 's/\/mirror1\/*\//zarchive/g'
-# echo "$source" | sed 's/\/mirror1*\/[6-8]/zarchive/g'
-# echo "$source" | sed 's/\/mirror1*\//zarchive/g'
-# echo "$source" | sed 's/\/mirror1*[6-8]\s/zarchive/g'
 echo "$source" | sed 's/\/mirror1\(\/.*\/[6-8]\s\)/\/zarchive\1/g'
