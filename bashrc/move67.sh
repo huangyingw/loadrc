4f7a163b5fbc22bc0dca73b5c3123d10cd78dbb2
Ying Huang <huangyingw@gmail.com>
2021-10-16 14:53:11 -0700

    n

--- bashrc/move67.sh
+++ bashrc/move67.sh
@@ -1,10 +1,11 @@
 #!/bin/zsh
 
-# find /media/volgrp/mirror1/av/ -type f -size +100M  | while read ss
-# do
-#     # dir=$(echo "$ss" | sed 's/\/mirror1\//\/zarchive\//g'); mkdir -p "$(dirname "$dir")";mv -v "$ss" "$(dirname "$dir")"
-# bashrc/append_num.sh:3 echo "$1" | sed "s/\/[6-9]\s\([^\/]*$\)/\/\1/g;s/\/\([^\/]*$\)/\/$2 \1/g;s/  \+/ /g;s/\"//g"
-# done
-source="/media/volgrp/mirror1/av/Ayami Shunka 旬果/8 uncensored 声音不同步 自慰 啪啪有声 潮吹 高跟鞋 (無修正流出) FC2PPV-881903 Shunka Ayami あやみ旬果 Uncensored Leaked 無碼流出 (ABP-557).mp4"
-echo "$source"
-echo "$source" | sed 's/\/mirror1\(\/.*\/[6-8]\s\)/\/zarchive\1/g'
+find /media/volgrp/mirror1/av/ -type f -size +100M  | while read ss
+do
+    target=$(echo "$ss" | sed 's/\/mirror1\(\/.*\/[6-8]\s\)/\/zarchive\1/g')
+
+    if [ "$ss" != "$target" ]
+    then
+        echo mv -nv "$ss" "$target"
+    fi
+done
