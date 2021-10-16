df3e7054e4ef4446db1307863c9a17ba14db5668
Ying Huang <huangyingw@gmail.com>
2021-10-16 11:53:08 -0700

    n

--- vishrc/cat_rate.sh
+++ vishrc/cat_rate.sh
@@ -2,7 +2,5 @@
 
 while read ss
 do
-    source=$(echo "$ss" | sed 's/"//g')
-    target=$(~/loadrc/bashrc/append_num.sh "$ss")
-    mv -nv "$source" "$target"
+    ~/loadrc/bashrc/append_cat.sh "$ss"
 done < "$1"
