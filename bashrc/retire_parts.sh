438b0849cf04c60833af3943292583996c6d303d
Ying Huang <huangyingw@gmail.com>
2021-11-10 17:31:56 -0800

    n

--- bashrc/retire_parts.sh
+++ bashrc/retire_parts.sh
@@ -8,7 +8,7 @@ function CountParts()
 left=1
 right=30
 
-while [ $left -lt $right ]
+while [ $((left + 1)) -lt $right ]
 do
     mid=$(printf '%s\n' $left $right | datamash median 1)
     mid=${mid%.*}
@@ -20,7 +20,7 @@ do
 
     if [ $result -gt 0 ]
     then
-        left=$((mid + 1))
+        left=$mid
         echo "left --> $left"
     else
         right=$mid
@@ -28,6 +28,13 @@ do
     fi
 done
 
-echo "find /var/lib/transmission-daemon/ -type f -mtime +$mid -name \*.part | wc -l"
-echo "find /var/lib/transmission-daemon/ -type f -mtime +$left -name \*.part | wc -l"
-echo "find /var/lib/transmission-daemon/ -type f -mtime +$right -name \*.part | wc -l"
+local result=$(CountParts $right)
+result=${result%.*}
+echo "result --> $result"
+
+if [ $result -gt 0 ]
+then
+    find /var/lib/transmission-daemon/ -type f -mtime +$right -name \*.part -delete
+else
+    find /var/lib/transmission-daemon/ -type f -mtime +$left -name \*.part -delete 
+fi
