acd05e7ad573a810bdebad50cb3f25ac44551bab
Ying Huang <huangyingw@gmail.com>
2021-11-12 18:32:35 -0800

    n

--- bashrc/generate_files_proj.sh
+++ bashrc/generate_files_proj.sh
@@ -47,7 +47,7 @@ done < "$INCLUDE_FILE"
 
 export LC_ALL=C
 
-find . "(" "${prune_params[@]}" ")" -a -prune -o -size +0 -type f -size -9000k -print | sed 's/\(["'\''\]\)/\\\1/g;s/.*/"&"/' > "$TARGET" && \
+find . "(" "${prune_params[@]}" ")" -a -prune -o -size +0 -type f -exec grep -Il "" {} + | sed 's/\(["'\''\]\)/\\\1/g;s/.*/"&"/' > "$TARGET" && \
     if [ ${#include_params[@]} -gt 0 ] ; \
     then \
         find . "(" "${include_params[@]}" ")" -type f -size -9000k | sed 's/\(["'\''\]\)/\\\1/g;s/.*/"&"/' >> ${TARGET} ; \
