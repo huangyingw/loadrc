65aafbf740e8540d38d3d83465b4733a5cc636f2
Ying Huang <huangyingw@gmail.com>
2021-11-16 21:41:05 -0800

    n

--- bashrc/select_move.sh
+++ bashrc/select_move.sh
@@ -4,5 +4,5 @@ cat "$1" | sed 's/^"//g;s/"$//g;s/\\ / /g' > "$1".tmp
 rsync -aHv --force --progress \
     --remove-source-files \
     --files-from "$1".tmp \
-    "$2" \
-    "$3"
+    . \
+    "$2"
