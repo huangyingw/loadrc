6833dd2e66e255f72f415aa4b22ef4ed9c2973cb
Ying Huang <huangyingw@gmail.com>
2021-11-23 20:37:56 -0800

    n

--- bashrc/ubak.sh
+++ bashrc/ubak.sh
@@ -1,25 +1,33 @@
 #! /bin/zsh
+
 if [ $(uname) = "Darwin" ]
 then
     echo -e "${red}It could only run in Linux... ${NC}"
     exit 1
 fi
+
 TARGET=/media/volgrp/media/`hostname`/
+
 if [ -n "$1" ]
 then
     TARGET="$1"
 fi
+
 echo $TARGET
 UBAKEX="$HOME/loadrc/bashrc/ubak_ex"
 UBAKIN="$HOME/loadrc/bashrc/ubak_in"
 exclude_params=();
+
 while read suf
 do
     exclude_params+=( "--exclude=$suf" )
 done < "$UBAKEX"
+
 include_params=();
+
 while read suf
 do
     include_params+=( "--include=$suf" )
 done < "$UBAKIN"
+
 rsync -aH --force --delete-during "${exclude_params[@]}" "${include_params[@]}" / "${TARGET}"
