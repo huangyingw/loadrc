31f14567cdba12d6cacab8f14b9f11c5c17b91ec
Ying Huang <huangyingw@gmail.com>
2021-11-28 01:35:46 -0500

    n

--- bashrc/iso2usb.sh
+++ bashrc/iso2usb.sh
@@ -1,3 +1,8 @@
 #!/bin/zsh
 
-dd bs=4M if=/media/volgrp/cubic/ubuntu-21.10.0-2021.11.27-desktop-amd64.iso of=/dev/sda conv=fdatasync status=progress
+dd \
+    bs=4M \
+    if=/media/volgrp/cubic/ubuntu-21.10.0-2021.11.27-desktop-amd64.iso \
+    of=/dev/sda \
+    conv=fdatasync \
+    status=progress
