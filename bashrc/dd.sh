dba59d79b6e3d113365ec17b6ee7ee9d77c2e953
Ying Huang <huangyingw@gmail.com>
2021-12-05 22:07:25 -0800

    n

--- bashrc/dd.sh
+++ bashrc/dd.sh
@@ -2,5 +2,5 @@
 
 input=/dev/nbd0
 output=/media/volgrp/image.img
-dd if="$input" bs=4096 conv=sync,noerror | gzip -9 | (pv -s $(fdisk -l "$input" | grep -o '[0-9]*\{1\} GiB' | awk '{print $1}')g) | dd of="$output" 
+dd if="$input" bs=4096 conv=sync,noerror | (pv -s $(fdisk -l "$input" | grep -o '[0-9]*\{1\} GiB' | awk '{print $1}')g) | dd of="$output" 
 dd if=/media/volgrp/image.img of=/dev/nbd0 bs=16M
