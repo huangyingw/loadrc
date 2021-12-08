05bc07d54a7edc15a8ef48501eebfd77520c5958
Ying Huang <huangyingw@gmail.com>
2021-12-08 12:36:12 -0800

    n

--- bashrc/dd.sh
+++ bashrc/dd.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh 
 
 input=/dev/nbd0
-output=/media/volgrp/image.img
+output=/media/volgrp/software/os/ubuntu/u2004.img
 dd if="$input" bs=4096 conv=sync,noerror | (pv -s $(fdisk -l "$input" | grep -o '[0-9]*\{1\} GiB' | awk '{print $1}')g) | dd of="$output" 
 dd if=/media/volgrp/image.img of=/dev/nbd0 bs=16M
