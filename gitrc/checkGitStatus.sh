72da7415f52b51628ca23567c249408997b828a6
Ying Huang <huangyingw@gmail.com>
2021-12-09 21:58:48 -0800

    n

--- gitrc/checkGitStatus.sh
+++ gitrc/checkGitStatus.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
-if [ -n "$(git status --porcelain)" ]
+
+if [ -n "$(git status --untracked-files=no --porcelain --ignore-submodules)" ]
 then
     echo -e "${red}the git repository is unclean, please check it before continuing... ${NC}"
     exit 1
