09f0204e2ff751b490ced265ff3bd9ba947442ce
Ying Huang <huangyingw@gmail.com>
2021-12-11 22:36:40 -0800

    n

--- bashrc/dd.sh
+++ bashrc/dd.sh
@@ -2,5 +2,5 @@
 
 input=/dev/nbd0
 output=/media/volgrp/software/os/ubuntu/u2004.img
-dd if="$input" bs=4096 conv=sync,noerror | (pv -s $(fdisk -l "$input" | grep -o '[0-9]*\{1\} GiB' | awk '{print $1}')g) | dd of="$output" 
-dd if=/media/volgrp/image.img of=/dev/nbd0 bs=16M
+# dd if="$input" bs=4096 conv=sync,noerror | (pv -s $(fdisk -l "$input" | grep -o '[0-9]*\{1\} GiB' | awk '{print $1}')g) | dd of="$output" 
+(pv -n "$input" | dd of="$output" bs=128M conv=notrunc,noerror) 2>&1 | dialog --gauge "Running dd command (cloning), please wait..." 10 70 0
