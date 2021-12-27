1da77a49a8d490a7a8d2e2d675bfedd209f54bd5
Ying Huang <huangyingw@gmail.com>
2021-12-26 22:05:11 -0800

    n

--- vishrc/vsearch.sh
+++ vishrc/vsearch.sh
@@ -14,4 +14,5 @@ fi
 # awk 'BEGIN {FS=OFS=":"}{if(NF>1)gsub(" ","\\ ",$1)}1' "$find_result" > "$find_result".bak 
 # perl -pe 's/\G[^ :]*\K /\\ /g' "$find_result" > "$find_result".bak   
 # sed -i.bak -E 's/:/\n:/;h;s/ /\\ /g;G;s/\n.*\n//' "$find_result"
-awk -i inplace 'BEGIN {FS=OFS=":"}{if(NF>1)gsub(" ","\\ ",$1)}1' "$find_result"
+awk 'BEGIN {FS=OFS=":"}{if(NF>1)gsub(" ","\\ ",$1)}1' "$find_result" > "$find_result".bak  
+cp -fv "$find_result".bak "$find_result"
