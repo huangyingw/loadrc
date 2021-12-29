60e5f26ef0a02c62216060d9a38eee6d8abacb5c
Ying Huang <huangyingw@gmail.com>
2021-12-28 16:07:53 -0800

    n

--- dockerrc/edocker.sh
+++ dockerrc/edocker.sh
@@ -4,5 +4,5 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
 . ~/loadrc/dockerrc/getContainerID.sh "$1"
-docker exec \
-  -it "$CONTAINERID" bash
+docker exec -it "$CONTAINERID" zsh || \
+    docker exec -it "$CONTAINERID" bash
