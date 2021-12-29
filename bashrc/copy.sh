ebd303f9cb8fa30a2080a39e7a094192bae801db
Ying Huang <huangyingw@gmail.com>
2021-12-29 11:57:06 -0800

    n

--- bashrc/copy.sh
+++ bashrc/copy.sh
@@ -18,5 +18,5 @@ siconv=$(~/loadrc/bashrc/get_iconv.sh "$source")
 ticonv=$(~/loadrc/bashrc/get_iconv.sh "$target")
 
 rsync -aHSv --progress --force \
-    --iconv="$siconv,$ticonv" \
+    --iconv="$ticonv,$siconv" \
     "$source" "$target"
