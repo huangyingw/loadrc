d5f955caac35f298905f7c560675667467ddc169
Ying Huang <huangyingw@gmail.com>
2021-12-30 12:40:27 -0800

    n

--- networkrc/ping.sh
+++ networkrc/ping.sh
@@ -1,12 +1,17 @@
 #!/bin/zsh
 ((count = 10))                           # Maximum number to try.
 
+
 while [[ $count -ne 0 ]] ; do
-    ping -c 1 8.8.8.8                    # Try once.
+    /usr/bin/ping -c 1 8.8.8.8 >> /root/loadrc/crontab.log 2>&1 
+    echo "step --> 2" >> /root/loadrc/crontab.log  
     rc=$?
+
     if [[ $rc -eq 0 ]] ; then
+        echo "step --> 3" >> /root/loadrc/crontab.log
         ((count = 1))                    # If okay, flag loop exit.
     else
+        echo "step --> 4" >> /root/loadrc/crontab.log
         sleep 1                          # Minimise network storm.
     fi
     ((count = count - 1))                # So we don't go forever.
