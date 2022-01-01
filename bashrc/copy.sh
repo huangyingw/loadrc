d9f14c05a9d904867443d60a9520913fd89c3043
Ying Huang <huangyingw@gmail.com>
2022-01-01 10:31:08 -0800

    n

--- bashrc/copy.sh
+++ bashrc/copy.sh
@@ -14,9 +14,8 @@ then
     mkdir -p "$target"
 fi
 
-siconv=$(~/loadrc/bashrc/get_iconv.sh "$source")
-ticonv=$(~/loadrc/bashrc/get_iconv.sh "$target")
+iconvs=$(~/loadrc/bashrc/get_iconvs.sh "$source" "$target") 
 
 rsync -aHSv --progress --force \
-    --iconv="$siconv,$ticonv" \
+    "$iconvs" \
     "$source" "$target"
