5e6918d3461ee359e5612f489c400d121f0c363d
Ying Huang <huangyingw@gmail.com>
2022-01-02 11:45:31 -0800

    n

--- transmissionrc/auto_reboot.sh
+++ transmissionrc/auto_reboot.sh
@@ -1,13 +1,11 @@
 #!/bin/zsh
 
-echo "" > /root/loadrc/crontab.log
-
-/root/loadrc/networkrc/ping.sh 
+/root/loadrc/networkrc/ping.sh
 rc=$?
 
 if [[ $rc -eq 0 ]]
 then
     echo "say The internet is back up."
 else
-    reboot
+    /usr/sbin/reboot
 fi
