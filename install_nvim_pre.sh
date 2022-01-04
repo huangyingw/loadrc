ae2413ae01c30ca65a4b87d9f00c71efd95c7c64
Ying Huang <huangyingw@gmail.com>
2022-01-03 21:17:42 -0800

    n

--- install_nvim_pre.sh
+++ install_nvim_pre.sh
@@ -5,12 +5,10 @@ cd "$SCRIPTPATH"
 
 if [ $(uname) != "Darwin" ]
 then
-    add-apt-repository -y ppa:neovim-ppa/stable
     apt-get update
     apt-get upgrade -y
     apt-get install -y \
         exuberant-ctags \
-        neovim \
         python-dev \
         python3-dev \
         python3-pip
@@ -19,10 +17,8 @@ then
 else
     brew install \
         coreutils \
-        neovim \
         python
     brew reinstall python
-    sudo pip3 install --upgrade neovim 
     sudo pip3 install pynvim jedi 
     #brew install --with-toolchain --with-clang llvm
 fi
