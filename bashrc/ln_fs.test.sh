87b02d9ac58f2b573f5c92cf0aa46858bd7da530
Ying Huang <huangyingw@gmail.com>
2022-01-06 22:51:30 -0800

    n

--- bashrc/ln_fs.test.sh
+++ bashrc/ln_fs.test.sh
@@ -3,4 +3,25 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-./ln_fs.sh 
+rm -fr lib
+./ln_fs.sh /var/lib lib
+ls -al lib
+echo
+
+rm lib
+mkdir lib
+./ln_fs.sh /var/lib lib
+ls -al lib
+echo
+
+rm lib
+touch lib
+./ln_fs.sh /var/lib lib
+ls -al lib
+echo
+
+rm lib
+ln -fs /var/lib lib
+./ln_fs.sh /var/lib lib
+ls -al lib
+echo
