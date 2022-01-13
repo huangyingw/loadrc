7ad7abe94416def693bdc964e08483712d293e10
Ying Huang <huangyingw@gmail.com>
2022-01-13 13:45:24 -0800

    n

--- gitrc/reapply.sh
+++ gitrc/reapply.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 DIFF="$1"
 CURRET_BRANCH_DIFF="$(~/loadrc/gitrc/get_current_branch.sh).gdio.diff"
-git apply --index --reverse "$CURRET_BRANCH_DIFF"
-git checkout -- "$CURRET_BRANCH_DIFF"
-git apply --index --reject --whitespace=fix "$DIFF"
+git show HEAD:"$CURRET_BRANCH_DIFF" > "$CURRET_BRANCH_DIFF".bak
+git apply --reverse "$CURRET_BRANCH_DIFF".bak
+git apply --reject --whitespace=fix "$DIFF"
 ~/loadrc/gitrc/gdio.sh
 
 if [ "$CURRET_BRANCH_DIFF" = "$DIFF" ]
