18ef063e01c8e2b691437018d26ec45e63a66a93
Ying Huang <huangyingw@gmail.com>
2022-01-14 22:50:16 -0800

    n

---
+++ macosrc/configure_ssh.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd "$SCRIPTPATH"
+
+sudo cp -nv /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
+sudo cp -fv ./etc/ssh/sshd_config /etc/ssh/sshd_config  
