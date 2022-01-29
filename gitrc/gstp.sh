f6dd62c6dfa9a917731ed3b64e33ef030c57a831
Ying Huang <huangyingw@gmail.com>
2022-01-29 14:59:45 -0800

    n

--- gitrc/gstp.sh
+++ gitrc/gstp.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 if [ -z "$1" ]
 then
-    git stash show -p stash@{0} | git apply && git stash drop stash@{0}
+    git stash show -p stash@{0} | git apply --reject --whitespace=fix --recount && git stash drop stash@{0}
 else
-    git stash show -p stash@{"$1"} | git apply && git stash drop stash@{"$1"}
+    git stash show -p stash@{"$1"} | git apply --reject --whitespace=fix --recount && git stash drop stash@{"$1"}
 fi
