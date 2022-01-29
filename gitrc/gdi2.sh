a0664ccd8fac19badaa9b3be01191df18db05e4e
Ying Huang <huangyingw@gmail.com>
2022-01-29 14:59:29 -0800

    n

--- gitrc/gdi2.sh
+++ gitrc/gdi2.sh
@@ -40,7 +40,7 @@ fi
 
 ~/loadrc/gitrc/discard_unnecessaries.sh ; \
     git checkout -f "$targetBranch" ; \
-    git apply --index --reject --whitespace=fix --recount "$GDITDIFF"
+    git apply --reject --whitespace=fix --recount --index "$GDITDIFF"
 
 retVal=$?
 
