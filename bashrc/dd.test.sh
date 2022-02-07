848effc0332eb48224b296ce4d28b88cc7c3dbfd
Ying Huang <huangyingw@gmail.com>
2022-02-06 23:29:47 -0800

    n

--- bashrc/dd.test.sh
+++ bashrc/dd.test.sh
@@ -3,4 +3,4 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-./dd.sh /dev/nbd0 /media/volgrp/software/os/ubuntu/test.img
+./dd.sh /media/psf/Home/software/tinypilot-voyager-2.3.2-2022-01-10T1531Z.img /dev/sdc
