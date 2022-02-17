f7fdced91a03fa054ab88996f48dc62d2df22a3b
Ying Huang <huangyingw@gmail.com>
2022-02-16 21:40:55 -0800

    n

--- bashrc/cscope.sh
+++ bashrc/cscope.sh
@@ -3,9 +3,9 @@
 SOURCEFILE="cscopesourcefile.bak"
 CSCOPEFILE="cscopefile.bak"
 
-rsync -v files.proj "$SOURCEFILE" && \
+cp -fv files.proj "$SOURCEFILE" && \
     sed -i.bak 's/\\ / /g' "$SOURCEFILE" && \
     cscope -bq -i "$SOURCEFILE" -f "$CSCOPEFILE" && \
-    rsync -v "$CSCOPEFILE" cscope.out && \
-    rsync -v "$CSCOPEFILE".in cscope.out.in && \
-    rsync -v "$CSCOPEFILE".po cscope.out.po
+    cp -fv "$CSCOPEFILE" cscope.out && \
+    cp -fv "$CSCOPEFILE".in cscope.out.in && \
+    cp -fv "$CSCOPEFILE".po cscope.out.po
