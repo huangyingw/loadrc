245d84c47cdb530d8fdb942b47f4024f4f32fbac
Ying Huang <huangyingw@gmail.com>
2022-02-16 17:08:09 -0800

    n

--- install_nvim_post.sh
+++ install_nvim_post.sh
@@ -4,8 +4,8 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
 mkdir -p ~/.config/nvim/
-~/loadrc/bashrc/ln_fs.sh ~/loadrc/vimrc/.vim ~/.vim
-~/loadrc/bashrc/ln_fs.sh ~/loadrc/vimrc/.vimrc ~/.vimrc
-~/loadrc/bashrc/ln_fs.sh ~/loadrc/.config/nvim/init.vim ~/.config/nvim/init.vim
-rm -fv ~/.viminfo*
-./gitrc/gclean.sh
+~/loadrc/bashrc/ln_fs.sh ~/loadrc/vimrc/.vim ~/.vim || true
+~/loadrc/bashrc/ln_fs.sh ~/loadrc/vimrc/.vimrc ~/.vimrc || true
+~/loadrc/bashrc/ln_fs.sh ~/loadrc/.config/nvim/init.vim ~/.config/nvim/init.vim || true
+rm -fv ~/.viminfo* || true
+./gitrc/gclean.sh || true
