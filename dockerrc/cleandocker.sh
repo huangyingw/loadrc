cfcaf7f3b3ea9f67b9daf91ff31ad40ba41cb55a
Ying Huang <huangyingw@gmail.com>
2022-02-20 14:16:12 -0800

    n

--- dockerrc/cleandocker.sh
+++ dockerrc/cleandocker.sh
@@ -1,21 +1,2 @@
 #!/bin/zsh 
-#===============================================================================
-#
-#          FILE: cleandocker.sh
-# 
-#         USAGE: ./cleandocker.sh 
-# 
-#   DESCRIPTION: 
-# 
-#       OPTIONS: ---
-#  REQUIREMENTS: ---
-#          BUGS: ---
-#         NOTES: ---
-#        AUTHOR: YOUR NAME (), 
-#  ORGANIZATION: 
-#       CREATED: 03/17/2016 21:42
-#      REVISION:  ---
-#===============================================================================
-
-set -o nounset                              # Treat unset variables as an error
 docker rmi -f $(docker images -a | grep '<none>' | tr -s ' ' | cut -d ' ' -f 3)
