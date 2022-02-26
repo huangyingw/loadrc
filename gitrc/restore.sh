df2e0456f53eaa2bb31930c7e905902bff975101
Ying Huang <huangyingw@gmail.com>
2022-02-25 22:41:31 -0800

    n

--- gitrc/restore.sh
+++ gitrc/restore.sh
@@ -4,4 +4,4 @@ CURRET_BRANCH_DIFF="$(~/loadrc/gitrc/get_current_branch.sh).gdio.diff"
 ~/loadrc/gitrc/gdio.sh
 git apply --reject --whitespace=fix --recount --reverse "$CURRET_BRANCH_DIFF" || \
     echo "git reverse apply failed" && \
-    exit 1                                            
+    exit 1
