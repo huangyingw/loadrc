bf9f39eef70d2c0fcfb32916f95574654756c034
Ying Huang <huangyingw@gmail.com>
2022-02-25 22:38:48 -0800

    n

--- bashrc/dd.sh
+++ bashrc/dd.sh
@@ -1,5 +1,5 @@
-#!/bin/zsh 
+#!/bin/zsh
 
 input="$1"
-output="$2" 
+output="$2"
 (pv -n "$input" | dd of="$output" bs=128M conv=notrunc,noerror) 2>&1 | dialog --gauge "Running dd command (cloning), please wait..." 10 70 0
