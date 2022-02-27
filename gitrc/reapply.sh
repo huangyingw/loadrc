4eb4c9cfca2253d4aff6705fa05d481a86590add
Ying Huang <huangyingw@gmail.com>
2022-02-26 16:26:32 -0800

    n

--- gitrc/reapply.sh
+++ gitrc/reapply.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 DIFF="$1"
 
-cp -fv "$DIFF" "$DIFF".bak
+cp -fv "$DIFF" "$DIFF".tmp
 ~/loadrc/gitrc/restore.sh
-cp -fv "$DIFF".bak "$DIFF"
+cp -fv "$DIFF".tmp "$DIFF"
 
 git apply --reject --whitespace=fix --recount "$DIFF" || \
     echo "git apply failed" && \
