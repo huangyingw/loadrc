2806c7a4edb0202195ea474e772d12feb670d0b4
Ying Huang <huangyingw@gmail.com>
2022-03-02 23:03:22 -0800

    n

--- bashrc/dd.sh
+++ bashrc/dd.sh
@@ -6,7 +6,7 @@ output="$2"
 if [[ "$input" = *'.img' ]] && [[ "$output" = *'/dev/'* ]]
 then
     (pv -n "$input" | dd of="$output" bs=128M conv=notrunc,noerror) 2>&1 | gunzip -c | dialog --gauge "Running dd command (cloning), please wait..." 10 70 0
-elif [[ "$output" = *'.img' ]] && [[ "$input" = *'/dev/'* ]] 
+elif [[ "$output" = *'.img' ]] && [[ "$input" = *'/dev/'* ]]
 then
-    (pv -n "$input" | dd of="$output" bs=128M conv=notrunc,noerror) 2>&1 | gzip -c | dialog --gauge "Running dd command (cloning), please wait..." 10 70 0
+    (pv -n "$input" | gzip -c | dd of="$output" bs=128M conv=notrunc,noerror) 2>&1 | dialog --gauge "Running dd command (cloning), please wait..." 10 70 0
 fi
