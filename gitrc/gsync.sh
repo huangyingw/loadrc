ba630f093be4aa6bc5d4d2e573ceb73afc60e96f
Ying Huang <huangyingw@gmail.com>
2022-03-06 12:43:48 -0800

    n

--- gitrc/gsync.sh
+++ gitrc/gsync.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 ~/loadrc/gitrc/dogsync.sh &
-git submodule status | awk '{print $2}' | parallel -j50 'cd {}; ~/loadrc/gitrc/dogsync.sh'
+git submodule status | awk '{print $2}' | parallel -j70 'cd {}; ~/loadrc/gitrc/dogsync.sh'
