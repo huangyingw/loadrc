032fbceeaafd9ca59d7663c7a48e94caebd88c1b
Ying Huang <huangyingw@gmail.com>
2022-03-25 21:06:45 -0700

    n

--- bashrc/fav.sh
+++ bashrc/fav.sh
@@ -6,7 +6,7 @@ if [ -d "$FAV" ]
 then
     cd "$FAV"
 
-    find . -type f -size +10M -exec du -h {} + | sort -r -h | cut -f 2 | sed 's/\(["\]\)/\\\1/g;s/.*/"&"/' > fav.log 
-    find . -type f -size +10M -exec ls -t {} \+ | sed 's/\(["\]\)/\\\1/g;s/.*/"&"/' > fav.log.sort
+    find . -type f -size +10M -exec ls -t {} \+ | sed 's/\(["\]\)/\\\1/g;s/.*/"&"/' > fav.log 
+    find . -type f -size +10M -exec ls -S {} \+ | sed 's/\(["\]\)/\\\1/g;s/.*/"&"/' > fav.log.sort
     touch files.proj
 fi
