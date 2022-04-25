393263a5f42589f87b2520f11118c9a05317b729
Ying Huang <huangyingw@gmail.com>
2022-04-25 16:16:33 -0700

    n

--- install_nvim_pre.sh
+++ install_nvim_pre.sh
@@ -3,25 +3,23 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-if [ $(uname) != "Darwin" ]
-then
-    apt-get update
-    apt-get upgrade -y
+apt-get update && \
+    apt-get upgrade -y && \
     apt-get install -y \
-        exuberant-ctags \
-        python-dev \
-        python3-dev \
-        python3-pip
-    pip3 install pynvim
-    pip3 install jedi
-else
-    brew install \
-        coreutils \
-        python
-    brew reinstall python
-    sudo pip3 install pynvim jedi
-    #brew install --with-toolchain --with-clang llvm
-fi
+    exuberant-ctags \
+    python-dev \
+    python3-dev \
+    python3-pip
+
+pip3 install pynvim
+pip3 install jedi
+
+brew install \
+    coreutils \
+    python
+
+brew reinstall python
+sudo pip3 install pynvim jedi
 
 ./neovim/install_prerequisite.sh
 ./neovim/install.sh
