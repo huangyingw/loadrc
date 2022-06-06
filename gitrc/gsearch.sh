6a914a35969ae007fc14330aeb19b75d6c3e48f2
Ying Huang <huangyingw@gmail.com>
2022-06-05 20:09:31 -0700

    n

--- gitrc/gsearch.sh
+++ gitrc/gsearch.sh
@@ -1,4 +1,10 @@
 #!/bin/zsh
 
-git log --binary --ignore-all-space --ignore-blank-lines --ignore-cr-at-eol --ignore-space-at-eol --ignore-space-change -p -G "$1" &> "$2"
-# git log --binary --ignore-all-space --ignore-blank-lines --ignore-cr-at-eol --ignore-space-at-eol --ignore-space-change -p master -G "property\(" --author="Ying Huang <yhuang@imvu.com>" --since="2017-08-15" -- rest/service > diff1.log
+filesStr=($(< $3))
+git log --binary \
+    --ignore-all-space \
+    --ignore-blank-lines \
+    --ignore-cr-at-eol \
+    --ignore-space-at-eol \
+    --ignore-space-change \
+    -p -G $1 -- "${filesStr[@]}" > "$2"
