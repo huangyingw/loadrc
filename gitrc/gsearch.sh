7cab68dc85590ea336a5186dfe1b79c16692354f
Ying Huang <huangyingw@gmail.com>
2022-06-08 07:20:15 +0100

    n

--- gitrc/gsearch.sh
+++ gitrc/gsearch.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 filesStr=($(< $3))
-COMMAND="git log --binary --ignore-all-space --ignore-blank-lines --ignore-cr-at-eol --ignore-space-at-eol --ignore-space-change -p -G $1 -- ${filesStr[@]}"
+COMMAND="git log --binary \
+    --ignore-all-space \
+    --ignore-blank-lines \
+    --ignore-cr-at-eol \
+    --ignore-space-at-eol \
+    --ignore-space-change \
+    -p -G $1 -- ${filesStr[@]}"
 eval "$COMMAND" > "$2"
