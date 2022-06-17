25b1f1da201b770246d125b0d5523e4421513a18
Ying Huang <huangyingw@gmail.com>
2022-06-16 22:56:44 -0700

    n

--- setup_locale.sh
+++ setup_locale.sh
@@ -5,3 +5,5 @@ echo "locales locales/default_environment_locale select $LOCALE" | debconf-set-s
 echo "locales locales/locales_to_be_generated multiselect $LOCALE UTF-8" | debconf-set-selections
 rm "/etc/locale.gen"
 dpkg-reconfigure --frontend noninteractive locales
+cp -fv ./linux/etc/locale.gen /etc/locale.gen
+locale-gen
