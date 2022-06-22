a7e878687662761119ab2d06a314bfc3320b5e27
Ying Huang <huangyingw@gmail.com>
2022-06-21 17:00:13 -0700

    n

--- pythonrc/vlc.py
+++ pythonrc/vlc.py
@@ -13,9 +13,9 @@ def callvlc(targetFile, start=0):
     if targetFile:
         if platform.system() == 'Darwin':
             if start == 0:
-                subprocess.check_call(['/Applications/VLC.app/Contents/MacOS/VLC', '-f', '--macosx-continue-playback=2', '--rate=2.0', targetFile])
+                subprocess.check_call(['/Applications/VLC.app/Contents/MacOS/VLC', '--sub-language', 'Chinese', '--sub-autodetect-file', '-f', '--macosx-continue-playback=2', '--rate=2.0', targetFile])
             else:
-                subprocess.check_call(['/Applications/VLC.app/Contents/MacOS/VLC', '-f', '--macosx-continue-playback=2', '--start-time=' + str(start), targetFile])
+                subprocess.check_call(['/Applications/VLC.app/Contents/MacOS/VLC', '--sub-language', 'Chinese', '--sub-autodetect-file', '-f', '--macosx-continue-playback=2', '--start-time=' + str(start), targetFile])
         else:
             subprocess.check_call(['smplayer', '-fullscreen', targetFile])
 
