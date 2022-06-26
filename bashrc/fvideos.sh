e6ee884cafb45153748067bcc763c6495dad93a2
Ying Huang <huangyingw@gmail.com>
2022-06-25 22:30:34 -0700

    n

--- bashrc/fvideos.sh
+++ bashrc/fvideos.sh
@@ -1,9 +1,3 @@
-#! /bin/zsh
+#!/bin/zsh
 
-FAVLOG="fav.log.tmp"
-FAVLOGSORT="fav.log.sort.tmp"
-export LC_ALL=C
-find . -type f -size +500k -exec du -h {} + | sort -r -h | cut -f 2 | sed 's/\(["\]\)/\\\1/g;s/.*/"&"/' > "$FAVLOG" && \
-    cp -fv "$FAVLOG" fav.log && \
-    find . -type f -size +500k -exec ls -t {} \+ | sed 's/\(["\]\)/\\\1/g;s/.*/"&"/' > "$FAVLOGSORT" && \
-    cp -fv "$FAVLOGSORT" fav.log.sort
+~/loadrc/bashrc/check_running.sh ~/loadrc/bashrc/do_fvideos.sh
