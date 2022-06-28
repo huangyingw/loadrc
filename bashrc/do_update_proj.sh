12dea04e257db86108266b436c17381a989c2a1b
Ying Huang <huangyingw@gmail.com>
2022-06-27 22:31:14 -0700

    n

--- bashrc/do_update_proj.sh
+++ bashrc/do_update_proj.sh
@@ -1,9 +1,8 @@
 #!/bin/zsh
 
-parallel ::: \
-    ~/loadrc/bashrc/generate_files_proj.sh \
-    ~/loadrc/bashrc/generate_rsync_files.sh \
-    ~/loadrc/bashrc/fvideos.sh \
-    ~/loadrc/bashrc/fdocs.sh \
-    ~/loadrc/bashrc/cscope.sh \
-    'git remote update'
+~/loadrc/bashrc/generate_files_proj.sh &
+~/loadrc/bashrc/generate_rsync_files.sh &
+~/loadrc/bashrc/fvideos.sh &
+~/loadrc/bashrc/fdocs.sh &
+~/loadrc/bashrc/cscope.sh &
+git remote update &
