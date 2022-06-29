f688a6f53a41c5e3c05c61b5a7002e136dc60cac
Ying Huang <huangyingw@gmail.com>
2022-06-28 19:47:22 -0700

    n

--- bashrc/do_update_proj.sh
+++ bashrc/do_update_proj.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-parallel ::: \
+parallel --jobs 0 -k ::: \
     ~/loadrc/bashrc/generate_files_proj.sh \
     ~/loadrc/bashrc/generate_rsync_files.sh \
     ~/loadrc/bashrc/fvideos.sh \
