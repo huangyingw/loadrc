b1c87e9c30dca390b2b52c83cbcd31ea91e53744
Ying Huang <huangyingw@gmail.com>
2022-07-03 22:43:49 -0700

    n

--- gitrc/dogsync.sh
+++ gitrc/dogsync.sh
@@ -5,7 +5,6 @@ then
     exit
 fi
 
-parallel --jobs 0 -k ::: \
-    ~/loadrc/gitrc/gps.sh \
-    ~/loadrc/gitrc/gpl.sh \
-    ~/loadrc/gitrc/dogdio.sh
+~/loadrc/gitrc/gps.sh &
+~/loadrc/gitrc/gpl.sh
+~/loadrc/gitrc/dogdio.sh
