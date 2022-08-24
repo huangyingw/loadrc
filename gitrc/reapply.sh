903a7faf22db1ebd8c852f98eb64261db2861967
Ying Huang <huangyingw@gmail.com>
2022-08-24 15:21:34 -0700

    n

--- gitrc/reapply.sh
+++ gitrc/reapply.sh
@@ -1,10 +1,22 @@
 #!/bin/zsh
-DIFF="$1"
 
+DIFF="$1"
 cp -fv "$DIFF" "$DIFF".tmp
+clean=$(git status --porcelain)
+
+if [ -n "$clean" ]
+then
+    git stash
+fi
+
 ~/loadrc/gitrc/restore.sh
 cp -fv "$DIFF".tmp "$DIFF"
 
 git apply --reject --whitespace=fix --recount "$DIFF" || \
     echo "git apply failed" && \
     exit 1
+
+if [ -n "$clean" ]
+then
+    git stash pop stash@{0}
+fi
