2068d05b02b4c96ff84cf3fbd190be3b0488fa49
Ying Huang <huangyingw@gmail.com>
2022-08-25 20:03:10 -0700

    n

--- bashrc/jdupes.sh
+++ bashrc/jdupes.sh
@@ -7,16 +7,4 @@ then
 fi
 
 TARGET="$1"
-MIRRORCHECK=$HOME/loadrc/."`hostname`".mirror.check
-
-crontab -r
-
-if [ -f ${MIRRORCHECK} ] ; \
-then \
-    ~/loadrc/bashrc/move67.sh ; \
-    fi ; \
-    jdupes -dNr -X size+:100m -X nostr:/.git/ "$TARGET" 2>&1 | tee ~/loadrc/jdupes.log && \
-    if [ -f ${MIRRORCHECK} ] ; \
-    then \
-        ~/loadrc/bashrc/sleep.sh ; \
-    fi
+jdupes -dNr -X size+:100m -X nostr:/.git/ "$TARGET" 2>&1 | tee ~/loadrc/jdupes.log
