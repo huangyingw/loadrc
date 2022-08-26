4530a42976b462e23cce35882dd7ebedae5dc160
Ying Huang <huangyingw@gmail.com>
2022-08-25 21:50:24 -0700

    n

--- ffmpegrc/append_bad_files.sh
+++ ffmpegrc/append_bad_files.sh
@@ -4,6 +4,6 @@ while read ss
 do
     if ! (~/loadrc/ffmpegrc/check_moov_atom.sh "$ss")
     then
-        echo ~/loadrc/bashrc/append_rate.sh "$ss" 6
+        echo "~/loadrc/bashrc/append_rate.sh $ss 6"
     fi
 done < "$1"
