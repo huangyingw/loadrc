952d4c305314444eb175bf2143993746751cb4dc
Ying Huang <huangyingw@gmail.com>
2022-08-25 22:38:47 -0700

    n

--- ffmpegrc/cat_bad_files.sh
+++ ffmpegrc/cat_bad_files.sh
@@ -4,6 +4,6 @@ while read ss
 do
     if ! (~/loadrc/ffmpegrc/check_moov_atom.sh "$ss")
     then
-        ~/loadrc/bashrc/append_rate.sh $ss 6
+        ~/loadrc/bashrc/append_rate.sh $ss 66
     fi
 done < "$1"
