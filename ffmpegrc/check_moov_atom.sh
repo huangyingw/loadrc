cd5bb93135d21cb7343627a6a9c4b58ef76484fa
Ying Huang <huangyingw@gmail.com>
2022-08-25 21:41:11 -0700

    n

--- ffmpegrc/check_moov_atom.sh
+++ ffmpegrc/check_moov_atom.sh
@@ -4,7 +4,5 @@ FILE="$1"
 
 if (ffprobe  -print_format json -show_format -show_streams "$FILE" 2>&1 | grep -q 'moov atom not found')
 then
-    echo "$FILE is corrupted"
-else
-    echo "$FILE is ok"
+    exit 1
 fi
