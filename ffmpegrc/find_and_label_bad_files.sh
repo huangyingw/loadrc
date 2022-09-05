d3dec7d8567a69a4a6963cc761122907e513ee4d
Ying Huang <huangyingw@gmail.com>
2022-09-05 15:55:20 -0700

    n

--- ffmpegrc/find_and_label_bad_files.sh
+++ ffmpegrc/find_and_label_bad_files.sh
@@ -2,9 +2,9 @@
 
 find . -type f -size +100M -exec du -h {} + | sort -r -h | cut -f 2 | while read ss
 do
+    echo "now checking --> $ss"
     if (ffprobe  -print_format json -show_format -show_streams "$ss" 2>&1 | grep -q 'moov atom not found')
     then
-        echo "now checking --> $ss"
         ~/loadrc/bashrc/append_rate.sh $ss 66
     fi
 done
