35e1f3698b2a302a28098ace112eaab2667ffa7b
Ying Huang <huangyingw@gmail.com>
2022-09-13 22:49:17 -0700

    n

--- bashrc/umletc.sh
+++ bashrc/umletc.sh
@@ -1,10 +1,12 @@
 #!/bin/zsh
+
 if [ -z "$1" ]
 then
     target=.
 else
     target="$1"
 fi
+
 find "$target" -type f -name \*.uxf|while read ss
 do
     output="`echo "$ss"|sed  -e "s/\.uxf$/\.pdf/g"`"
