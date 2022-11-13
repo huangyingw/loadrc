597d54407dcbbb87aceb1f1c61313b9042eceb43
Ying Huang <ying.huang@cohesity.com>
2022-11-12 16:24:32 -0800

    n

--- bashrc/do_update_proj.sh
+++ bashrc/do_update_proj.sh
@@ -2,6 +2,7 @@
 
 parallel --jobs 10 -k ::: \
     ~/loadrc/bashrc/generate_files_proj.sh \
+    ~/loadrc/bashrc/generate_rsync_files.sh \
     ~/loadrc/bashrc/fvideos.sh \
     ~/loadrc/bashrc/fdocs.sh \
     ~/loadrc/bashrc/cscope.sh \
