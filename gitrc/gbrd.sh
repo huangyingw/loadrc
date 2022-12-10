46e9d8a32a7953953b3d9b3bf3de34e8d3c8c6d2
Ying Huang <huangyingw@gmail.com>
2022-12-10 15:27:26 -0800

    n

--- gitrc/gbrd.sh
+++ gitrc/gbrd.sh
@@ -1,7 +1,11 @@
 #!/bin/zsh
 git branch -d "$1"
+retVal=$?
 
-for ss in $(git remote)
-do
-    git push "$ss" --delete "$1" || true
-done
+if [ $retVal -eq 0 ]
+then
+    for ss in $(git remote)
+    do
+        git push "$ss" --delete "$1" || true
+    done
+fi
