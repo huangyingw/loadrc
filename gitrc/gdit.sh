09c16d3b464b8dd743383e4161ba403c092f7600
Ying Huang <ying.huang@cohesity.com>
2022-12-20 16:34:29 -0800

    n

--- gitrc/gdit.sh
+++ gitrc/gdit.sh
@@ -5,12 +5,10 @@ target_branch=$(echo "$current_branch" | sed 's/\.fix$//g')
 remote="$(git config gsync.remote)"
 target="$(git config gsync.target)"
 
-~/loadrc/gitrc/discard_unnecessaries.sh
 git branch "$target_branch" "$target"
 git fetch "$remote" "$target_branch":"$target_branch" ; \
     git fetch "$remote" "$target":"$target_branch" && \
     ~/loadrc/gitrc/gdi.sh "$target_branch " "$current_branch" 2>&1 | tee "$output"
-
 
 if [ $? -ne 0 ]
 then
