b0bd1cce3379f2a8ea663c10700bca3891a5b82d
Ying Huang <ying.huang@cohesity.com>
2022-12-20 17:11:35 -0800

    n

--- gitrc/checkout_rejs.sh
+++ gitrc/checkout_rejs.sh
@@ -4,10 +4,11 @@ for ss in $(git status | grep \.rej$) ; \
 do \                                     
     targetFile=$(echo "$ss" | sed 's/\.rej$//g')
     targetFile=$(echo "$targetFile" | sed 's/\(["'\''\]\)/\\\1/g;s/ /\\ /g')
-    if [ "$2" = "f" ] || [ $(git config checkoutrejs.force) ]
+    if [ "$2" = "f" ] || [ -n $(git config checkoutrejs.force) ] ; \
     then
         git checkout "$1" "$targetFile" && \
             rm "$ss"
+
     else
         git show "$1":"$targetFile" > "$ss"
     fi
