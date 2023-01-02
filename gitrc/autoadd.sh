da226226e82c47ece88aafad9571328fd29fad13
Ying Huang <huangyingw@gmail.com>
2023-01-02 12:27:04 -0800

    n

--- gitrc/autoadd.sh
+++ gitrc/autoadd.sh
@@ -4,6 +4,7 @@ fileName="$1"
 if [[ "$fileName" != *"../"* ]]
 then
     filesProj=$(~/loadrc/bashrc/find_up.sh $(pwd) "files.proj")
+    fileName=$(echo $fileName | sed 's/ /\\ /g')
 
     if ( grep -Fxq "\"./$fileName\"" "$filesProj" ) && ( ! git ls-files --error-unmatch "$fileName" )
     then
