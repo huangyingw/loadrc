ae094ab6fe2122388f2179b138d4fb971050d31b
Ying Huang <huangyingw@gmail.com>
2023-01-04 12:57:31 -0800

    n

--- gitrc/gres.sh
+++ gitrc/gres.sh
@@ -1,7 +1,8 @@
 #!/bin/zsh
 
 conflictArr=($(git status | awk '/both .+:/{print $3}'))
-
+#        git status | sed -e '/both modified:/!d;s/both modified://;s/^[\s\t]*//g;s/\ /\\ /g'
+#        both modified:   dev.gdio.diff
 for conflict in "${conflictArr[@]}"
 do
     git add $conflict
