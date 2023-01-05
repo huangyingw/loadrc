ccccddef0e3acc2935b4fc91087fe767d9f1cbcb
Ying Huang <ying.huang@cohesity.com>
2023-01-04 16:12:18 -0800

    n

--- gitrc/gres.sh
+++ gitrc/gres.sh
@@ -1,10 +1,8 @@
 #!/bin/zsh
 
 conflictArr=($(git status | awk '/both .+:/{print $3}'))
-#       git status | sed -e '/both modified:/!d;s/both modified://;s/^[\s\t]*//g;s/\ /\\ /g'
-#       both modified:   Solutions/CohesitySecurity/Data Connectors/Helios2Sentinel/IncidentProducer/IncidentProducer.cs
-#       both modified:   Solutions/CohesitySecurity/Data Connectors/Helios2Sentinel/IncidentProducer/local.settings.json
-#       both added:      Solutions/CohesitySecurity/Data Connectors/Helios2Sentinel/azuredeploy.json
+IFS=$'\n'
+
 for conflict in "${conflictArr[@]}"
 do
     git add $conflict
