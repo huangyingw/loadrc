6ab7f16359c6f94902c7a7091da22e3a26300e1a
Ying Huang <huangyingw@gmail.com>
2023-01-06 13:18:29 -0800

    n

--- bashrc/fvideos.sh
+++ bashrc/fvideos.sh
@@ -8,10 +8,11 @@ if [ -n "$2" ]
 then
     SIZE_OPTION="+$2M"
 else
-    SIZE_OPTION="+100M"
+    SIZE_OPTION="+10M"
 fi
 
 cd "$1"
+echo "SIZE_OPTION --> $SIZE_OPTION"
 find . -type f -size "$SIZE_OPTION" -exec du -h {} + | sort -r -h | cut -f 2 | sed 's/\(["\]\)/\\\1/g;s/.*/"&"/' > "$FAVLOG" && \
     cp -fv "$FAVLOG" fav.log && \
     find . -type f -size "$SIZE_OPTION" -exec ls -t {} \+ | sed 's/\(["\]\)/\\\1/g;s/.*/"&"/' > "$FAVLOGSORT" && \
