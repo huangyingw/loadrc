f52c0aa005a71d364148b3daa1c58c8e5b96a302
Ying Huang <huangyingw@gmail.com>
2023-01-08 08:05:43 +0300

    n

--- zerotierrc/install.sh
+++ zerotierrc/install.sh
@@ -7,5 +7,7 @@ if [ $(uname) != "Darwin" ]
 then
     curl -s https://install.zerotier.com/ | bash
     pacman --noconfirm -Sy zerotier-one
+    systemctl enable zerotier-one.service
+    systemctl start zerotier-one.service
     zerotier-cli join 93afae5963560e41
 fi
