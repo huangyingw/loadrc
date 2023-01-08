a53e465653c170c0017df6fe65d6f2b0d0197b2b
Ying Huang <huangyingw@gmail.com>
2023-01-08 05:56:03 +0300

    n

--- zerotierrc/install.sh
+++ zerotierrc/install.sh
@@ -6,5 +6,6 @@ cd "$SCRIPTPATH"
 if [ $(uname) != "Darwin" ]
 then
     curl -s https://install.zerotier.com/ | bash
+    pacman --noconfirm -Sy zerotier-one
     zerotier-cli join 93afae5963560e41
 fi
