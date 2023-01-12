6f18cc6c8c1d5e36a7378ac31d82fe5803b61c93
Ying Huang <huangyingw@gmail.com>
2023-01-11 21:57:00 -0800

    n

--- bashrc/findmv.sh
+++ bashrc/findmv.sh
@@ -6,4 +6,6 @@ file="/media/mapper/115_disk/av/av91/av/zunsafe/旬果/ssis-506-4k/7 ssis-506-4k
 
 echo "file --> $file"
 echo "target --> $target"
-echo "file --> $file" | sed -e "s#$source#$target#g"
+new_file=$(echo "$file" | sed -e "s#$source#$target#g")
+mkdir -p "$(dirname "$new_file")"
+mv -v "$file" "$new_file"
