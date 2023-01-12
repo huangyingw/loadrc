27c7c0081013931b014bd32585c272caded8cc5a
Ying Huang <huangyingw@gmail.com>
2023-01-11 19:27:06 -0800

    n

--- bashrc/findmv.sh
+++ bashrc/findmv.sh
@@ -1,1 +1,9 @@
 #!/bin/zsh
+
+source="/media/mapper/115_disk/av/av91/av"
+target="/media/mapper/115_disk/av"
+file="/media/mapper/115_disk/av/av91/av/zunsafe/旬果/ssis-506-4k/7 ssis-506-4k.mp4"
+
+echo "file --> $file"
+echo "target --> $target"
+echo "file --> $file" | sed -e "s#$source#$target#g"
