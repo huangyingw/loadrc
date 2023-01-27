a8fbbf0c8c6674b5f9a1db7dd06fbf2e5e5f8eaa
Ying Huang <ying.huang@cohesity.com>
2023-01-27 11:14:16 -0800

    n

--- vishrc/vrun.sh
+++ vishrc/vrun.sh
@@ -18,7 +18,7 @@ extension=${file##*.}
 host=$(git config deploy.host)
 rpath=$(git config deploy.path)
 rootFolder=$(~/loadrc/bashrc/find_up_folder.sh "$file" "files.proj")
-rfile=$(realpath --relative-to="$rootFolder" "$file")
+rfile=$(realpath --relative-to="$rootFolder" "$file" | sed 's/ /\\ /g')
 rfolder=$(realpath --relative-to="$rootFolder" $(dirname "$file"))
 
 if [[ "$file" = *'_test.py' ]]
