72d80ded708f96082ed0fc719b176bfe695cdb7f
Ying Huang <huangyingw@gmail.com>
2023-02-02 22:15:45 -0800

    n

--- gitrc/submodule_update.sh
+++ gitrc/submodule_update.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 git submodule init
 git submodule sync
-git submodule foreach ~/loadrc/gitrc/gps.sh || : 
+git submodule foreach ~/loadrc/gitrc/gps.sh || :
 git submodule foreach ~/loadrc/gitrc/gsync.sh || :
--- mdadmrc/reactivate.sh
+++ mdadmrc/reactivate.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh 
+#!/bin/zsh
 #reactivate array
 mdadm --stop /dev/md0
 mdadm --assemble --force /dev/md0 /dev/sd[b-e]1
