84a1ca09bff906a0ccc71063e17af4676be0f3b2
Ying Huang <huangyingw@gmail.com>
2023-02-17 09:53:01 -0800

    n

--- pythonrc/vlc.py
+++ pythonrc/vlc.py
@@ -13,7 +13,7 @@ def callvlc(targetFile, start=0):
     if targetFile:
         if platform.system() == 'Darwin':
             if start == 0:
-                subprocess.check_call(['/Applications/VLC.app/Contents/MacOS/VLC', '--sub-language', 'Chinese', '--sub-autodetect-file', '-f', '--macosx-continue-playback=2', '--rate=2.0', targetFile])
+                subprocess.check_call(['/Applications/VLC.app/Contents/MacOS/VLC', '--sub-language', 'Chinese', '--sub-autodetect-file', '-f', '--macosx-continue-playback=2', '--rate=2.0', '--no-volume-save', '--volume', '0', targetFile])
             else:
                 subprocess.check_call(['/Applications/VLC.app/Contents/MacOS/VLC', '--sub-language', 'Chinese', '--sub-autodetect-file', '-f', '--macosx-continue-playback=2', '--start-time=' + str(start), targetFile])
         else:
