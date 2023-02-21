9ad9ee16c386eeee2ae8fee1c740a50c6aba3eeb
Ying Huang <ying.huang@cohesity.com>
2023-02-21 15:33:24 -0800

    n

--- gitrc/gdit.sh
+++ gitrc/gdit.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-# output="$1"
+output="$1"
 current_branch="$(~/loadrc/gitrc/get_current_branch.sh)"
 target_branch=$(echo "$current_branch" | sed 's/\.fix$//g')
 remote="$(git config gsync.remote)"
