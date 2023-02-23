334ca883846d52e3799a1acf77f153dc45c6c1b3
Ying Huang <huangyingw@gmail.com>
2023-02-23 13:54:06 -0800

    n

--- bashrc/ssh.sh
+++ bashrc/ssh.sh
@@ -2,7 +2,4 @@
 target=$(echo "$1" | sed  -e "s/\/$//g")
 tmuxAction="$(ssh "$target" ". ~/loadrc/.pathrc ; which tmux") new-session -A -s ssh"
 
-while true
-do
-    ssh -Y "$target" -t -- ${tmuxAction}
-done
+ssh -Y "$target" -t -- ${tmuxAction}
