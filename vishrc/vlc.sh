b08b819f93071aca23f837d7ee9121a54b0480be
Ying Huang <huangyingw@gmail.com>
2023-02-24 17:03:05 -0800

    n

--- vishrc/vlc.sh
+++ vishrc/vlc.sh
@@ -7,7 +7,6 @@ fi
 
 if [ $(uname) = "Darwin" ]
 then
-    osascript -e 'set volume 0'
     /Applications/VLC.app/Contents/MacOS/VLC --sub-language Chinese --sub-autodetect-file -f --macosx-continue-playback=2 --rate=2.0 "$1"
 else
     #smplayer "$1"
