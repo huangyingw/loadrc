2a4c82e4cb85ded4fec3b00add9f43e55505e27b
Ying Huang <ying.huang@cohesity.com>
2023-03-02 08:49:59 -0800

    n

--- gitrc/gdi.sh
+++ gitrc/gdi.sh
@@ -9,9 +9,18 @@ else
     COMMAND="$COMMAND $1 $2"
     IFS=$'\n'
 
-    for ss in $(git config --get-all gdif.ignore)
+    for ss in $(git config --get-all gdio.ignore)
     do
         COMMAND="$COMMAND  ':(exclude)$ss'"
     done
 fi
+
+if [[ -n "$3" ]] && [[ "$3" = "gdit" ]]
+then
+    for ss in $(git config --get-all gdit.ignore)
+    do
+        COMMAND="$COMMAND  ':(exclude)$ss'"
+    done
+fi
+
 eval "$COMMAND" | sed 's/^--- a\//--- \.\//g;s/^+++ b\//+++ \.\//g;/^index [0-9a-f]*[0-9a-f]*/d'
