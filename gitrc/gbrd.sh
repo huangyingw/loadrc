ea94b5a56ebae1d8677f3993be624dde76bd1e25
Ying Huang <ying.huang@cohesity.com>
2023-03-02 08:46:58 -0800

    n

--- gitrc/gbrd.sh
+++ gitrc/gbrd.sh
@@ -1,11 +1,7 @@
 #!/bin/zsh
 git branch -d "$1"
-retVal=$?
 
-if [ $retVal -eq 0 ]
-then
-    for ss in $(git remote)
-    do
-        git push "$ss" --delete "$1" || true
-    done
-fi
+for ss in $(git remote)
+do
+    git push "$ss" --delete "$1" || true
+done
