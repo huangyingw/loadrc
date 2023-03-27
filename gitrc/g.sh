d33189a5d25575834f610b37b3a8564090e3ec57
Ying Huang <ying.huang@cohesity.com>
2023-03-27 14:19:25 -0700

    n

--- gitrc/g.sh
+++ gitrc/g.sh
@@ -1,14 +1,14 @@
 #!/bin/zsh
 ~/loadrc/gitrc/include_gitconfig.sh
 
-git_dir=$(git rev-parse --git-dir)
+git_directory=$(git rev-parse --git-dir)
 
-if [ -f "$git_dir/MERGE_MSG" ] && [ -s "$git_dir/MERGE_MSG" ]
-then
-    commit_message=$(cat "$git_dir/MERGE_MSG")
-elif [ -f "$git_dir/COMMIT_EDITMSG" ]
-then
-    commit_message=$(cat "$git_dir/COMMIT_EDITMSG")
+commit_message=""
+
+if [ -f "$git_directory/MERGE_MSG" ] && [ -s "$git_directory/MERGE_MSG" ]; then
+    commit_message=$(cat "$git_directory/MERGE_MSG")
+elif [ -f "$git_directory/COMMIT_EDITMSG" ]; then
+    commit_message=$(cat "$git_directory/COMMIT_EDITMSG")
 fi
 
 if [ -n "$1" ]
