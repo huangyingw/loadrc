6b151606619333aacc5792f02db92bb20c5d233d
Ying Huang <huangyingw@gmail.com>
2023-04-04 11:15:23 -0700

    n

--- osxrc/restart_bluetooth.sh
+++ osxrc/restart_bluetooth.sh
@@ -1,18 +1,30 @@
 #!/bin/zsh
-KeepConnect() {
-    ID="$1"
-    while [[ $(blueutil --is-connected "$ID") != 1 ]]
-    do
+
+TIMEOUT=180
+
+keep_connect() {
+    local id="$1"
+    local elapsed_time=0
+
+    while ! blueutil --is-connected "$id" && [[ $elapsed_time -lt $TIMEOUT ]]; do
         blueutil -p 1
-        blueutil --connect "$ID"
+        blueutil --connect "$id"
+        sleep 1
+        ((elapsed_time++))
     done
 }
 
+ensure_power_on() {
+    local elapsed_time=0
+
+    while ! blueutil -p 1 && [[ $elapsed_time -lt $TIMEOUT ]]; do
+        sleep 1
+        ((elapsed_time++))
+    done
+}
+
+DEVICE_ID="c8-7b-23-cd-44-ba" # Bose QC Earbuds II
+
 blueutil -p 0
-
-while [[ $(blueutil -p) != 1 ]]
-do
-    blueutil -p 1
-done
-
-KeepConnect c8-7b-23-cd-44-ba & # Bose QC Earbuds II
+ensure_power_on
+keep_connect "$DEVICE_ID" &
