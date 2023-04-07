5184fe940a6d09f7bd9960d1f8a393ebd69bcceb
Ying Huang <ying.huang@cohesity.com>
2023-04-07 12:55:02 -0700

    Remove empty submodule vimrc/.vim/bundle/vim-gitgutter

--- gitrc/git-submodule.sh
+++ gitrc/git-submodule.sh
@@ -1,17 +1,43 @@
 #!/bin/sh
 
+# Initialize the git submodules
 git submodule init
+
+# Get submodule path information from the .gitmodules file
 git config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
-    while read path_key path
-    do
-        name=$(echo $path_key | sed 's/\submodule\.\(.*\)\.path/\1/')
-        url_key=$(echo $path_key | sed 's/\.path/.url/')
-        branch_key=$(echo $path_key | sed 's/\.path/.branch/')
-        url=$(git config -f .gitmodules --get "$url_key")
-        branch=$(git config -f .gitmodules --get "$branch_key" || echo "master")
-        if [ -d "$path" ] && [ ! $(git config --get "$url_key") ]
-        then
-            mv "$path" "$path""_backup_""$(date +'%Y%m%d%H%M%S')";
-        fi
-        git submodule add -b $branch $url $path || continue
-    done
+
+# Process each submodule
+while read submodule_path_key submodule_path; do
+    # Extract the submodule name from the submodule path key
+    submodule_name=$(echo $submodule_path_key | sed 's/\submodule\.\(.*\)\.path/\1/')
+
+    # Create submodule URL and branch keys based on the submodule name
+    submodule_url_key=$(echo $submodule_path_key | sed 's/\.path/.url/')
+    submodule_branch_key=$(echo $submodule_path_key | sed 's/\.path/.branch/')
+
+    # Get submodule URL and branch values
+    submodule_url=$(git config -f .gitmodules --get "$submodule_url_key")
+    submodule_branch=$(git config -f .gitmodules --get "$submodule_branch_key" || echo "master")
+
+    # Check if the submodule directory is empty (0B)
+    submodule_directory_size=$(du -s "$submodule_path" | cut -f1)
+
+    if [ "$submodule_directory_size" -eq 0 ]; then
+        # Remove the empty submodule directory
+        rm -rf "$submodule_path"
+
+        # Remove the submodule from the index
+        git rm --cached "$submodule_path"
+
+        # Commit the removal of the submodule
+        git commit -m "Remove empty submodule $submodule_name"
+    fi
+
+    # Check if the submodule is already in the index
+    submodule_index_status=$(git ls-files --stage -- "$submodule_path" | grep -E '^160000')
+
+    if [ -z "$submodule_index_status" ]; then
+        # Add the submodule with the specified branch, URL, and path
+        git submodule add -b $submodule_branch $submodule_url $submodule_path || continue
+    fi
+done
