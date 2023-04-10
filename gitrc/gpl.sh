25dc86c35a5c17dedccfab81dcf0fc3ccaeafce0
Ying Huang <huangyingw@gmail.com>
2023-04-10 13:59:22 -0700

    n

--- gitrc/gpl.sh
+++ gitrc/gpl.sh
@@ -1,30 +1,47 @@
 #!/bin/zsh
 
+# Discard unnecessary files
 ~/loadrc/gitrc/discard_unnecessaries.sh
 
-if [ -n "$(git status --untracked-files=no --porcelain --ignore-submodules)" ]
-then
+# Check for changes in the working directory
+changes=$(git status --untracked-files=no --porcelain --ignore-submodules)
+
+if [ -n "$changes" ]; then
     return
 fi
 
+# Include git configuration
 ~/loadrc/gitrc/include_gitconfig.sh
+
+# Update remote references
 git remote update
+
+# Get the current branch name
 currentBranch=$(~/loadrc/gitrc/get_current_branch.sh)
 
-for remote in $(git remote)
-do
-    git pull $remote $currentBranch
-    git remote prune $remote
+# Function to pull and prune a remote
+pull_and_prune() {
+    git pull $1 $2
+    git remote prune $1
+}
+
+# Iterate through remotes and perform pull and prune operations in parallel
+for remote in $(git remote); do
+    pull_and_prune $remote $currentBranch &
 done
 
-for ss in $(git config --get-all merge.from)
-do
+# Wait for all background processes to finish
+wait
+
+# Merge branches specified in the 'merge.from' configuration
+for ss in $(git config --get-all merge.from); do
     git merge "$ss"
 done
 
+# Get the 'gsync.target' configuration value
 target=$(git config gsync.target)
 
-if [ -n "$target" ]
-then
+# Merge the target branch if specified
+if [ -n "$target" ]; then
     git merge "$target"
 fi
