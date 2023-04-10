09d122167e7fd8bf42fe2d3f551023dea6ba3e19
Ying Huang <huangyingw@gmail.com>
2023-04-10 13:52:59 -0700

    n

--- gitrc/gsync.sh
+++ gitrc/gsync.sh
@@ -1,3 +1,13 @@
 #!/bin/zsh
-~/loadrc/gitrc/dogsync.sh
-git submodule status | awk '{print $2}' | parallel --jobs 0 'cd {}; ~/loadrc/gitrc/dogsync.sh; cd -'
+
+# Define variables
+dogsync_script="$HOME/loadrc/gitrc/dogsync.sh"
+git_submodule_status_cmd="git submodule status"
+awk_cmd="awk '{print $2}'"
+parallel_cmd="parallel --jobs 0 --ungroup"
+
+# Execute the dogsync script
+source "$dogsync_script"
+
+# Execute the dogsync script for each submodule
+$git_submodule_status_cmd | $awk_cmd | $parallel_cmd "cd {}; source $dogsync_script; cd -"
