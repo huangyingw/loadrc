96860d492fa1ee0310050e6064567c7f51cd3534
Ying Huang <huangyingw@gmail.com>
2023-04-10 14:04:20 -0700

    n

--- bashrc/do_update_proj.sh
+++ bashrc/do_update_proj.sh
@@ -1,9 +1,14 @@
 #!/bin/zsh
 
-parallel --jobs 10 -k ::: \
-    ~/loadrc/bashrc/generate_files_proj.sh \
-    ~/loadrc/bashrc/generate_rsync_files.sh \
-    ~/loadrc/bashrc/fvideos.sh \
-    ~/loadrc/bashrc/fdocs.sh \
-    ~/loadrc/bashrc/cscope.sh \
-    'git remote update'
+# Define the list of commands to run in parallel
+commands=(
+    "~/loadrc/bashrc/generate_files_proj.sh"
+    "~/loadrc/bashrc/generate_rsync_files.sh"
+    "~/loadrc/bashrc/fvideos.sh"
+    "~/loadrc/bashrc/fdocs.sh"
+    "~/loadrc/bashrc/cscope.sh"
+    "git remote update"
+)
+
+# Run the commands in parallel with 10 jobs and keep the output in order
+parallel --jobs 10 -k -- ::: "${commands[@]}"
