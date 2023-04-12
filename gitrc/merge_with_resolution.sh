99f45726b7c0d6078e617a29a209b6b9f72dce52
Ying Huang <ying.huang@cohesity.com>
2023-04-12 15:51:34 -0700

    n

--- gitrc/merge_with_resolution.sh
+++ gitrc/merge_with_resolution.sh
@@ -10,18 +10,36 @@ BRANCH_TO_MERGE="$1"
 # Attempt to merge
 MERGE_OUTPUT=$(git merge "$BRANCH_TO_MERGE" --strategy ort 2>&1)
 
-# Check if merge failed due to conflicts
+# Check if merge failed due to conflicts or untracked files
 if [[ $MERGE_OUTPUT == *"error:"* ]]; then
-    # Parse conflicting file names from merge output
-    CONFLICTING_FILES=$(echo "$MERGE_OUTPUT" | grep -oP '(?<=        ).*(?=\n)')
+    if [[ $MERGE_OUTPUT == *"untracked working tree files would be overwritten by merge"* ]]; then
+        echo "Untracked working tree files would be overwritten by merge. Renaming them to *.bak."
 
-    # Rename conflicting files to *.bak
-    for FILE in ${(f)CONFLICTING_FILES}; do
-        mv "$FILE" "${FILE}.bak"
-    done
+        # Parse untracked file names from merge output
+        UNTRACKED_FILES=$(echo "$MERGE_OUTPUT" | awk '/^    / {print substr($0, 9)}')
 
-    # Merge again
-    git merge "$BRANCH_TO_MERGE" --strategy ort
+        # Rename untracked files to *.bak
+        for FILE in ${(f)UNTRACKED_FILES}; do
+            mv "$FILE" "${FILE}.bak"
+        done
+
+        # Attempt the merge again
+        MERGE_OUTPUT=$(git merge "$BRANCH_TO_MERGE" --strategy ort 2>&1)
+    fi
+
+    # Check if merge failed due to conflicts
+    if [[ $MERGE_OUTPUT == *"error:"* ]]; then
+        # Parse conflicting file names from merge output
+        CONFLICTING_FILES=$(echo "$MERGE_OUTPUT" | awk '/^        / {print substr($0, 9)}')
+
+        # Rename conflicting files to *.bak
+        for FILE in ${(f)CONFLICTING_FILES}; do
+            mv "$FILE" "${FILE}.bak"
+        done
+
+        # Merge again
+        git merge "$BRANCH_TO_MERGE" --strategy ort
+    fi
 else
     echo "Merge completed without conflicts."
 fi
