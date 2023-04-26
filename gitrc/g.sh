395a38d2f880e307f725e2ed6d6075e912181351
Ying Huang <huangyingw@gmail.com>
2023-04-26 12:51:34 -0700

    restore the previous g.sh, and empty COMMIT_EDITMSG and MERGE_MSG

--- gitrc/g.sh
+++ gitrc/g.sh
@@ -12,12 +12,38 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+# Initialize commit message variable
+commit_message=""
+
+# Check if there is an ongoing merge
+if [ -f "$git_directory/MERGE_MSG" ] && [ -s "$git_directory/MERGE_MSG" ]; then
+    commit_message=$(cat "$git_directory/MERGE_MSG")
+    # Check if there is a COMMIT_EDITMSG file
+elif [ -f "$git_directory/COMMIT_EDITMSG" ]; then
+    commit_message=$(cat "$git_directory/COMMIT_EDITMSG")
+fi
+
 # Use the first argument as the commit message if provided
 if [ -n "$1" ]; then
-    git commit -m "$1"
-else
-    git commit --no-edit || git commit -m "fallback commit message"
+    commit_message="$1"
 fi
+
+# Use a default commit message if none is set
+if [ -z "$commit_message" ]; then
+    commit_message="n"
+fi
+
+# Try committing without editing the message or use the provided message
+git commit --no-edit || git commit -m "$commit_message"
 
 # Run post-commit script
 ~/loadrc/gitrc/postCommit.sh
+
+# Empty the COMMIT_EDITMSG and MERGE_MSG files after a successful commit
+if [ -f "$git_directory/COMMIT_EDITMSG" ]; then
+    echo "" > "$git_directory/COMMIT_EDITMSG"
+fi
+
+if [ -f "$git_directory/MERGE_MSG" ]; then
+    echo "" > "$git_directory/MERGE_MSG"
+fi
