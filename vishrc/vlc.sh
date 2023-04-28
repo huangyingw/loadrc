3e878d1bd1e66a13c6b4869ea69fb0ca76a9ecbb
Ying Huang <huangyingw@gmail.com>
2023-04-27 21:22:33 -0700

    n

--- vishrc/vlc.sh
+++ vishrc/vlc.sh
@@ -3,8 +3,8 @@
 line="./mapper/usb_backup_crypt_8T_1/115download/031215_043-1pon-1080p.mp4/9 uncensored 031215_043-1pon-1080p:02:17:17"
 
 # Use regex to match the timestamp at the end of the string
-file_path=$(echo "$line" | sed 's/\(:[0-9][0-9]:[0-9][0-9]:[0-9][0-9]\)$//')
-timestamp=$(echo "$line" | sed 's/.*\(:[0-9][0-9]:[0-9][0-9]:[0-9][0-9]\)$/\1/' | sed 's/^://')
+file_path=$(echo "$line" | perl -pe 's/:[0-9]{1,2}:[0-9]{2}$//')
+timestamp=$(echo "$line" | perl -pe 's/.*:([0-9]{1,2}:[0-9]{2})$/\1/')
 
 echo "file_path --> $file_path"
 echo "timestamp --> $timestamp"
