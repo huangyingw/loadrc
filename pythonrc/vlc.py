e5c0f6c0a10a23031946ffd75c4b0720af37cc6c
Ying Huang <huangyingw@gmail.com>
2023-04-29 00:06:32 -0700

    n

--- pythonrc/vlc.py
+++ pythonrc/vlc.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python3
-# vlc.py
 import re
+import sys
+import subprocess
+import os
+
+
+def time_to_seconds(time_string):
+    time_parts = time_string.split(":")
+    if len(time_parts) == 3:
+        hours, minutes, seconds = map(int, time_parts)
+        total_seconds = hours * 3600 + minutes * 60 + seconds
+    elif len(time_parts) == 2:
+        minutes, seconds = map(int, time_parts)
+        total_seconds = minutes * 60 + seconds
+    else:
+        total_seconds = None
+    return total_seconds
 
 
 def split_string(string):
@@ -8,24 +23,38 @@ def split_string(string):
     pattern = r"^(.*?)(?::(\d{1,2}:\d{1,2}(?::\d{1,2})?))$"
     match = re.match(pattern, string)
     if match:
-        path = match.group(1)
-        time = match.group(2)
-        return path, time
+        # Calculate time in seconds from the matched time string
+        time_seconds = time_to_seconds(match.group(2))
+        return time_seconds
     else:
         return None
 
 
-# Example usage
-string = "./mapper/usb_backup_crypt_8T_1/115download/[7sht.me]MKBD-S118-C/9 MKBD-S118-C.mp4:32:57"
-path, time = split_string(string)
-print(
-    path
-)  # ./mapper/usb_backup_crypt_8T_1/115download/[7sht.me]MKBD-S118-C/9 MKBD-S118-C.mp4
-print(time)  # 32:57
+def open_in_vlc(file_path, cur_line):
+    time = split_string(cur_line)
+    print("Opening in VLC: " + cur_line + " at " + str(time) + "")
 
-string = "./mapper/usb_backup_crypt_8T_1/115download/031215_043-1pon-1080p.mp4/9 uncensored 031215_043-1pon-1080p:02:17:17"
-path, time = split_string(string)
-print(
-    path
-)  # ./mapper/usb_backup_crypt_8T_1/115download/031215_043-1pon-1080p.mp4/9 uncensored 031215_043-1pon-1080p
-print(time)  # 02:17:17
+    if time:
+        # Change the working directory to file_path
+        os.chdir(os.path.dirname(file_path))
+
+        subprocess.check_call(
+            [
+                "/Applications/VLC.app/Contents/MacOS/VLC",
+                "--sub-language",
+                "Chinese",
+                "--sub-autodetect-file",
+                "-f",
+                "--macosx-continue-playback=2",
+                "--start-time=" + str(time),
+                cur_line,
+            ]
+        )
+
+
+if __name__ == "__main__":
+    # Use the command line arguments to get the file path and line
+    file_path = sys.argv[1]
+    cur_line = sys.argv[2]
+
+    open_in_vlc(file_path, cur_line)
