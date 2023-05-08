1f1fad5bce14cf80e1057d400e90c8074fc063c5
Ying Huang <huangyingw@gmail.com>
2023-05-08 16:23:12 -0700

    n

--- pythonrc/find_similar_filenames.py
+++ pythonrc/find_similar_filenames.py
@@ -1,4 +1,13 @@
 import difflib
+import os
+import re
+import nltk
+
+def get_file_size(file_path, default_size=0):
+    try:
+        return os.path.getsize(file_path)
+    except FileNotFoundError:
+        return default_size
 
 file_paths = [
     "./mapper/usb_backup_crypt_8T_1/115download/abp-566-4K.mp4",
@@ -13,18 +22,38 @@ def extract_file_name(file_path):
 
 file_names = [extract_file_name(fp) for fp in file_paths]
 
-def find_close_files(file_names, threshold=0.8):
-    close_files = []
+def extract_keywords(file_names):
+    pattern = re.compile(r'\b(?:\d+|[a-z]+(?:\d+[a-z]*)?)\b', re.IGNORECASE)
+    keywords = set()
+    for file_name in file_names:
+        # Remove file extension
+        name_without_ext = os.path.splitext(file_name)[0]
+        matches = pattern.findall(name_without_ext)
+        keywords.update(matches)
+    return list(keywords)
+
+def find_close_files(file_names, keywords, threshold=0.8):
+    close_files = {kw: [] for kw in keywords}
 
     for i, file_name_1 in enumerate(file_names):
         for j, file_name_2 in enumerate(file_names):
             if i != j:
                 similarity = difflib.SequenceMatcher(None, file_name_1, file_name_2).ratio()
                 if similarity >= threshold:
-                    close_files.append((file_name_1, file_name_2))
+                    for kw in keywords:
+                        if kw in file_name_1 and kw in file_name_2:
+                            close_files[kw].extend([file_paths[i], file_paths[j]])
 
     return close_files
 
+keywords = extract_keywords(file_names)
 threshold = 0.8
-close_files = find_close_files(file_names, threshold)
-print(close_files)
+close_files = find_close_files(file_names, keywords, threshold)
+
+for keyword in keywords:
+    keyword_files = list(set(close_files[keyword]))
+    keyword_files.sort(key=lambda x: get_file_size(x))
+    print(f"{keyword}:")
+    for file_path in keyword_files:
+        print(f'"{file_path}",')
+    print()
