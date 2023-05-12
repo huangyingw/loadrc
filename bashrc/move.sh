b121b00a0bb6473298053fcb852ccc2f806dc290
Ying Huang <huangyingw@gmail.com>
2023-05-12 06:07:01 +0000

    n

--- bashrc/move.sh
+++ bashrc/move.sh
@@ -1,40 +1,15 @@
 #!/bin/zsh
 
 # Script Name: move.sh
-
-# Check if both source and target parameters are provided
-if [ -z "$1" ] || [ -z "$2" ]; then
-    echo -e "${red}Please provide both source and target parameters... ${NC}"
-    exit 1
-fi
+# Description: This script is designed to move files and directories from a source location to a target location.
+# It uses the rsync_content_migrator.zsh script to perform the move operation.
 
 # Get the real paths for source and target
 source="$1"
-realpath "$1" && source=$(realpath "$1")
-
 target="$2"
-realpath "$2" && target=$(realpath "$2")
 
-# Only proceed if source and target are not the same
-if [ -n "$source" ] && [ -n "$target" ] && [ "$source" != "$target" ]; then
-    # Get necessary parameters for rsync
-    iconvs=$(~/loadrc/bashrc/get_iconvs.sh "$source" "$target")
-    rsyncpath=$(~/loadrc/bashrc/get_rsyncpath.sh "$source" "$target")
-    rsync_basic_options=($(< ~/loadrc/bashrc/rsync_basic_options))
+# Call the rsync_content_migrator.zsh script with source and target
+~/loadrc/zshrc/rsync_content_migrator.zsh "$source" "$target"
 
-    # Perform rsync with appropriate options
-    rsync \
-        --remove-source-files \
-        "${rsync_basic_options[@]}" \
-        "$iconvs" \
-        "$rsyncpath" \
-        "$source/" "$target/"
-
-    # Remove empty directories from the source
-    ~/loadrc/bashrc/rmEmpty.sh "$source"
-else
-    # Print error message and exit if source and target are the same
-    echo -e "${red}same dir --> ${source} ... ${NC}"
-    echo -e "${red}please choose a different dir! ... ${NC}"
-    exit 1
-fi
+# Remove empty directories from the source
+~/loadrc/bashrc/rmEmpty.sh "$source"
