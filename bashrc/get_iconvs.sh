1838e3065aae246353ba3c35010b3ef5d13b8d44
Ying Huang <huangyingw@gmail.com>
2023-05-12 17:08:33 -0700

    n

--- bashrc/get_iconvs.sh
+++ bashrc/get_iconvs.sh
@@ -1,13 +1,6 @@
 #!/bin/zsh
 
-source=$1
-target=$2
-
-shost=$(~/loadrc/bashrc/parse_host.sh "$source")
-thost=$(~/loadrc/bashrc/parse_host.sh "$target")
-
-function get_utf_8_str()
-{
+function get_utf_8_str() {
     host=$1
 
     if ( ssh -q "$host" "uname" | grep -q Darwin )
@@ -18,16 +11,30 @@ function get_utf_8_str()
     fi
 }
 
-local_utf_8_str=$(get_utf_8_str "localhost")
+function main() {
+    source=$1
+    target=$2
 
-if [ "$shost" != "localhost" ]
-then
-    remote_utf_8_str=$(get_utf_8_str $shost)
-    echo "--iconv=$local_utf_8_str,$remote_utf_8_str"
-elif [ "$thost" != "localhost" ]
-then
-    remote_utf_8_str=$(get_utf_8_str $thost)
-    echo "--iconv=$local_utf_8_str,$remote_utf_8_str"
-else
-    echo "--iconv=$local_utf_8_str,$local_utf_8_str"
-fi
+    shost=$(~/loadrc/bashrc/parse_host.sh "$source")
+    thost=$(~/loadrc/bashrc/parse_host.sh "$target")
+
+    local_utf_8_str=$(get_utf_8_str "localhost")
+
+    if [ "$shost" != "localhost" ]
+    then
+        remote_utf_8_str=$(get_utf_8_str $shost)
+        echo "--iconv=$local_utf_8_str,$remote_utf_8_str"
+    elif [ "$thost" != "localhost" ]
+    then
+        remote_utf_8_str=$(get_utf_8_str $thost)
+        echo "--iconv=$local_utf_8_str,$remote_utf_8_str"
+    else
+        echo "--iconv=$local_utf_8_str,$local_utf_8_str"
+    fi
+}
+
+main "$@"
+#main ~/loadrc/ mini:~/loadrc/
+#main mini:~/loadrc/ ~/loadrc/ 
+#main ~/loadrc/ ~/loadrc/ 
+#main mini:~/loadrc/ mini:~/loadrc/ 
