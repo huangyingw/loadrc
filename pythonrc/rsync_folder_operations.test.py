44849ff486667c830ed8e88ea83026c7d3b0bbd5
Ying Huang <huangyingw@gmail.com>
2023-05-13 18:52:54 -0700

    n

--- pythonrc/rsync_folder_operations.test.py
+++ pythonrc/rsync_folder_operations.test.py
@@ -5,6 +5,7 @@ A script to test the functionality of rsync_folder_operations.py by creating sam
 
 import os
 import shutil
+from unittest import TestCase, main as unittest_main
 from rsync_folder_operations import rsync_operations
 
 
@@ -16,10 +17,14 @@ def create_sample_folders(base_folder):
     with open(os.path.join(base_folder, "source_folder/file1.txt"), "w") as f:
         f.write("This is file1 in source_folder.")
 
-    with open(os.path.join(base_folder, "source_folder/subfolder1/file2.txt"), "w") as f:
+    with open(
+        os.path.join(base_folder, "source_folder/subfolder1/file2.txt"), "w"
+    ) as f:
         f.write("This is file2 in subfolder1 of source_folder.")
 
-    with open(os.path.join(base_folder, "source_folder/subfolder2/file3.txt"), "w") as f:
+    with open(
+        os.path.join(base_folder, "source_folder/subfolder2/file3.txt"), "w"
+    ) as f:
         f.write("This is file3 in subfolder2 of source_folder.")
 
 
@@ -28,23 +33,36 @@ def cleanup_sample_folders(base_folder):
     shutil.rmtree(os.path.join(base_folder, "target_folder"))
 
 
-def test_rsync_operations():
-    base_folder = "test_folders"
+class TestRsyncFolderOperations(TestCase):
+    def setUp(self):
+        self.base_folder = "test_folders"
+        create_sample_folders(self.base_folder)
+        self.source_folder = os.path.join(self.base_folder, "source_folder")
+        self.target_folder = os.path.join(self.base_folder, "target_folder")
 
-    create_sample_folders(base_folder)
+    def tearDown(self):
+        cleanup_sample_folders(self.base_folder)
 
-    source_folder = os.path.join(base_folder, "source_folder")
-    target_folder = os.path.join(base_folder, "target_folder")
+    def test_rsync_move(self):
+        print("\nTesting mode: move")
+        rsync_operations(self.source_folder, self.target_folder, "move")
+        # Add assertions for the move mode here.
 
-    # Test different modes
-    modes = ["move", "copy", "mirror", "tmirror"]
-    for mode in modes:
-        print(f"\nTesting mode: {mode}")
-        rsync_operations(source_folder, target_folder, mode)
+    def test_rsync_copy(self):
+        print("\nTesting mode: copy")
+        rsync_operations(self.source_folder, self.target_folder, "copy")
+        # Add assertions for the copy mode here.
 
-    # Clean up the sample folders
-    cleanup_sample_folders(base_folder)
+    def test_rsync_mirror(self):
+        print("\nTesting mode: mirror")
+        rsync_operations(self.source_folder, self.target_folder, "mirror")
+        # Add assertions for the mirror mode here.
+
+    def test_rsync_tmirror(self):
+        print("\nTesting mode: tmirror")
+        rsync_operations(self.source_folder, self.target_folder, "tmirror")
+        # Add assertions for the tmirror mode here.
 
 
 if __name__ == "__main__":
-    test_rsync_operations()
+    unittest_main()
