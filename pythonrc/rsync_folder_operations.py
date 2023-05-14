bf4541f1ff64817e5b884d0489bb74c435165e81
Ying Huang <huangyingw@gmail.com>
2023-05-13 18:29:14 -0700

    n

---
+++ pythonrc/rsync_folder_operations.py
@@ -0,0 +1,88 @@
+"""
+rsync_folder_operations.py
+A script to move, copy, or mirror the contents of one folder into another using rsync while maintaining the tree structure.
+"""
+
+import os
+import sys
+import subprocess
+from pathlib import Path
+from rsync_path_resolver import resolve_rsync_path
+
+
+def rsync_operations(source_folder, target_folder, mode):
+    # Get the real paths for source and target
+    source_folder = os.path.realpath(source_folder)
+    target_folder = os.path.realpath(target_folder)
+
+    if os.path.samefile(source_folder, target_folder):
+        print(
+            "Source and target folders are identical or just soft links to each other. Aborting."
+        )
+        return 1
+
+    # Get necessary parameters for rsync
+    iconvs = subprocess.check_output(
+        ["./rsync_iconv_options.zsh", source_folder, target_folder], text=True
+    ).strip()
+    rsync_path_option = resolve_rsync_path(source_folder, target_folder)
+
+    with open("./rsync_basic_options", "r") as file:
+        rsync_basic_options = file.read().split()
+
+    # Set rsync options based on the mode (move, copy, mirror, or tmirror)
+    rsync_options = rsync_basic_options + [iconvs, rsync_path_option]
+    if mode == "move":
+        rsync_options.append("--remove-source-files")
+    elif mode == "mirror":
+        rsync_options.append("--delete-before")
+    elif mode == "tmirror":
+        rsync_options.extend(["-in", "--delete-before"])
+
+    # Perform rsync with --mkpath option first
+    rsync_options.append("--mkpath")
+    print(f"rsync options: {rsync_options}")
+
+    if mode == "tmirror":
+        ready_file = os.path.join(source_folder, "tmirror.ready")
+        with open(ready_file, "w") as outfile:
+            subprocess.run(
+                ["rsync"]
+                + rsync_options
+                + [f"{source_folder}/", f"{target_folder}/"],
+                stdout=outfile,
+            )
+    else:
+        result = subprocess.run(
+            ["rsync"]
+            + rsync_options
+            + [f"{source_folder}/", f"{target_folder}/"]
+        )
+        if result.returncode != 0:
+            # If rsync with --mkpath option fails, try again without the option
+            rsync_options = rsync_options[:-1]  # remove --mkpath from options
+            print(f"rsync options (without --mkpath): {rsync_options}")
+            subprocess.run(
+                ["rsync"]
+                + rsync_options
+                + [f"{source_folder}/", f"{target_folder}/"]
+            )
+
+
+def main():
+    if len(sys.argv) != 4:
+        print(
+            "Usage: python rsync_folder_operations.py source_folder target_folder mode"
+        )
+        print("mode: 'move', 'copy', 'mirror', or 'tmirror'")
+        sys.exit(1)
+
+    source_folder = sys.argv[1]
+    target_folder = sys.argv[2]
+    mode = sys.argv[3]
+
+    rsync_operations(source_folder, target_folder, mode)
+
+
+if __name__ == "__main__":
+    main()
