de0aa807be416469acfe2487c36f29dda58eaa5e
Ying Huang <huangyingw@gmail.com>
2023-05-15 11:51:51 -0700

    n

--- bashrc/tmirror.sh
+++ bashrc/tmirror.sh
@@ -6,4 +6,4 @@ source=$1
 target=$2
 
 # Call the rsync_folder_operations.zsh script with source, target, and mode set to 'tmirror'
-~/loadrc/zshrc/rsync_folder_operations.zsh "$source" "$target" "tmirror"
+~/loadrc/pythonrc/rsync_folder_operations.py "$source" "$target" "tmirror"
