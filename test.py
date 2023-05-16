d3bfe42c151fabed8035bccbbe05c3b7789cd8aa
Ying Huang <huangyingw@gmail.com>
2023-05-16 12:15:24 -0700

    n

--- test.py
+++ test.py
@@ -15,12 +15,15 @@ test_scripts = glob.glob("./pythonrc/*.test.py")
 
 # Execute each test
 for script in test_scripts:
+    print(f"Starting test: {script}")
     result = subprocess.run(["python3", script])
 
     # If the test script fails (non-zero exit status), abort immediately
     if result.returncode != 0:
         print(f"Test {script} failed. Aborting.")
         sys.exit(1)
+    else:
+        print(f"Finished test: {script}")
 
 # If all test scripts pass (the script has not yet exited), print a success message
 print("All test scripts passed successfully.")
