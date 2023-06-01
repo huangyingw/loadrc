5470edf0e9082593ac726b265eb1d8104a66bcdb
Ying Huang <ying.huang@cohesity.com>
2023-06-01 10:25:41 -0700

    n

--- gitrc/gdit.sh
+++ gitrc/gdit.sh
@@ -7,15 +7,28 @@ remote="$(git config gsync.remote)"
 target="$(git config gsync.target)"
 
 git branch "$target_branch" "$target"
-git fetch "$remote" "$target_branch":"$target_branch"
+
+# Switch to the target branch
+git checkout "$target_branch"
+
+# Update all branches from all remotes
+git remote update
+
+# Merge the first branch
+git merge "$remote/$target_branch"
+
+# Merge the second branch
 remote_branch=$(echo "$target" | sed "s/^$remote\///g")
-git fetch "$remote" "$remote_branch":"$target_branch"
+git merge "$remote/$remote_branch"
+
+# Switch back to the current branch
+git checkout "$current_branch"
 
 ~/loadrc/gitrc/gdi.sh "$target_branch " "$current_branch" "gdit" > "$output" 2>&1
 
 if [ $? -ne 0 ]
 then
-    echo -e "${red}most probably sth wrong in git merge... ${NC}" > "$output" 2>&1
+    echo -e "${red}most probably something went wrong in git merge... ${NC}" > "$output" 2>&1
     exit 1
 fi
 
