e95b7d61f0eeaadc7d60108654624f9d92f0ce47
Ying Huang <huangyingw@gmail.com>
2023-06-02 17:45:24 -0700

    n

--- pythonrc/line_video_player.py
+++ pythonrc/line_video_player.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python3
 import sys
 import subprocess
-from shlex import quote
+import json
 
 
 def main():
@@ -8,15 +9,15 @@ def main():
         print(f"Usage: {sys.argv[0]} <filename>")
         return 1
 
-    filename = sys.argv[1]
+    filename = sys.argv[1]  # sys.argv[1] is already a Python string
     with open(filename, "r") as f:
         for line in f:
-            line = line.strip()
+            line = line.strip()  # strip() removes leading/trailing white space
             if line:  # skip empty lines
                 print(f"Playing: {line}")  # echo the current playing line
                 # Call vlc.py script for each line
                 subprocess.run(
-                    f'~/loadrc/pythonrc/vlc.py "{quote(filename)}" "{quote(line)}"',
+                    f"~/loadrc/pythonrc/vlc.py {json.dumps(filename)} {json.dumps(line)}",
                     shell=True,
                 )
     return 0
