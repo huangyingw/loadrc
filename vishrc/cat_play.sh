cf9698ac620d676f78922dd6cfba1c626d2dc03c
Ying Huang <huangyingw@gmail.com>
2023-07-12 14:40:17 -0700

    n

--- vishrc/cat_play.sh
+++ vishrc/cat_play.sh
@@ -1,14 +1,10 @@
 #!/bin/zsh
 
-volume=$(osascript -e 'output volume of (get volume settings)')
-osascript -e 'set volume 0' &
 
 while read ss
 do
     ss=$(echo "$ss" | sed 's/"//g')
     echo "now playing"
     echo "$ss"
-    ~/loadrc/vishrc/vlc.sh "$ss"
+    ~/loadrc/pythonrc/vlc.py "$1" "$ss"
 done < "$1"
-
-osascript -e "set volume $volume" &
