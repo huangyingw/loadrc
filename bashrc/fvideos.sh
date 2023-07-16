d22655337010a45dae64f6c7316c2bc23f426c99
Ying Huang <huangyingw@gmail.com>
2023-07-16 13:42:09 -0700

    ../.gitconfig ../.gitconfig ../.gitconfig ../.gitconfig n

--- bashrc/fvideos.sh
+++ bashrc/fvideos.sh
@@ -18,10 +18,15 @@ function main() {
 
     # Build the find command with exclusion patterns from the EXCLUDE_FILE
     find_cmd="find . -type f -size \"${SIZE_OPTION}\""
-    while IFS= read -r line
-    do
-        find_cmd+=" ! -path \"${line}\""
-    done < "${EXCLUDE_FILE}"
+
+    # If the exclude file exists, then process it
+    if [ -f "${EXCLUDE_FILE}" ]
+    then
+        while IFS= read -r line
+        do
+            find_cmd+=" ! -path \"${line}\""
+        done < "${EXCLUDE_FILE}"
+    fi
 
     eval "${find_cmd} -exec du -h {} + | sort -r -h | cut -f 2 | sed 's/\([\"\\]\)/\\\1/g;s/.*/\"&\"/' > \"${FAVLOG}\"" && \
         cp -fv "${FAVLOG}" fav.log && \
