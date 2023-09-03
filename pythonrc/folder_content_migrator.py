5fffea093e52b0b061b6be0ba0fd480bbc464d59
Ying Huang <huangyingw@gmail.com>
2023-09-03 16:01:07 -0700

    n

--- pythonrc/folder_content_migrator.py
+++ pythonrc/folder_content_migrator.py
@@ -12,13 +12,19 @@ from pathlib import Path
 
 
 def same_partition(source_folder, target_folder):
+    print(f"Checking if {source_folder} and {target_folder} are on the same partition...")
     return os.stat(source_folder).st_dev == os.stat(target_folder).st_dev
 
 
 def move_folders(source_folder, target_folder):
-    if os.path.isdir(source_folder) and os.path.isdir(target_folder):
+    print(f"Moving contents from {source_folder} to {target_folder}...")
+    if os.path.isdir(source_folder):
+        if not os.path.isdir(target_folder):
+            print(f"Target folder {target_folder} does not exist. Creating it...")
+            os.makedirs(target_folder, exist_ok=True)
+
         if same_partition(source_folder, target_folder):
-            # Use 'shutil.move' if the source and target folders are on the same partition.
+            print("Source and target folders are on the same partition. Using 'shutil.move'...")
             for root, _, files in os.walk(source_folder):
                 for file in files:
                     src = os.path.join(root, file)
@@ -26,7 +32,9 @@ def move_folders(source_folder, target_folder):
                         target_folder, os.path.relpath(src, source_folder)
                     )
                     try:
+                        print(f"Creating directories for {tgt} if they don't exist...")
                         os.makedirs(os.path.dirname(tgt), exist_ok=True)
+                        print(f"Moving {src} to {tgt}...")
                         shutil.move(src, tgt) if not os.path.exists(
                             tgt
                         ) else print(
@@ -36,6 +44,7 @@ def move_folders(source_folder, target_folder):
                         print(f"Cannot create directory {os.path.dirname(tgt)}: {e}")
 
         # Call the rsync_folder_operations script
+        print("Source and target folders are on different partitions. Using 'rsync'...")
         rsync_script_path = os.path.join(os.path.expanduser('~'), "loadrc/pythonrc/rsync_folder_operations.py")
         call(
             [
@@ -47,15 +56,20 @@ def move_folders(source_folder, target_folder):
         )
 
         # Remove empty directories after the move
+        print("Removing empty directories...")
         for root, dirs, _ in os.walk(source_folder, topdown=False):
             for directory in dirs:
                 dir_path = os.path.join(root, directory)
                 if os.path.isdir(dir_path) and not os.listdir(dir_path):
+                    print(f"Removing empty directory {dir_path}...")
                     os.rmdir(dir_path)
 
         # Check if source_folder is empty and remove it
         if not os.listdir(source_folder):
+            print(f"Removing empty source folder {source_folder}...")
             os.rmdir(source_folder)
+    else:
+        print("Source folder does not exist. Exiting...")
 
 
 def main():
