f0730542bd8abdb4877a13283d4e61f390b5078d
Ying Huang <huangyingw@gmail.com>
2023-09-14 11:12:13 -0700

    n

--- leetcoderc/post_submit.sh
+++ leetcoderc/post_submit.sh
@@ -2,4 +2,4 @@
 FILE="$1"
 NUM=$(echo "$FILE" | sed 's/^\([0-9]*\)\..*/\1/g')
 
-~/loadrc/bashrc/check_running.sh ~/loadrc/leetcoderc/do_post_submit.sh "$NUM"
+~/loadrc/bashrc/run_once_script_checker.sh ~/loadrc/leetcoderc/do_post_submit.sh "$NUM"
