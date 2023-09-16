5ab1fb33a8e73608561a2b6d2b5691a138c7a660
Ying Huang <huangyingw@gmail.com>
2023-09-15 17:19:21 -0700

    n

--- jaccard_file_sorter/file_group_sorter_jaccard.cpp
+++ jaccard_file_sorter/file_group_sorter_jaccard.cpp
@@ -2,26 +2,32 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <sstream>
 #include <unordered_map>
 #include <unordered_set>
 #include <algorithm>
 
 // Function to calculate Jaccard similarity between two strings
-double jaccard_similarity(const std::string& a, const std::string& b) {
+double jaccard_similarity(const std::string& a, const std::string& b)
+{
+    std::cout << "Calculating Jaccard similarity between: " << a << " and " << b << std::endl;  // Log output
     std::unordered_set<char> setA(a.begin(), a.end());
     std::unordered_set<char> setB(b.begin(), b.end());
 
     std::unordered_set<char> intersection;
     std::unordered_set<char> unionSet;
 
-    for (char c : setA) {
-        if (setB.find(c) != setB.end()) {
+    for (char c : setA)
+    {
+        if (setB.find(c) != setB.end())
+        {
             intersection.insert(c);
         }
         unionSet.insert(c);
     }
 
-    for (char c : setB) {
+    for (char c : setB)
+    {
         unionSet.insert(c);
     }
 
@@ -29,76 +35,122 @@ double jaccard_similarity(const std::string& a, const std::string& b) {
 }
 
 // Function to group files by similarity
-std::unordered_map<std::string, std::vector<std::pair<int, std::string>>> group_files_by_similarity(const std::vector<std::pair<int, std::string>>& file_list) {
+std::unordered_map<std::string, std::vector<std::pair<int, std::string>>> group_files_by_similarity(const std::vector<std::pair<int, std::string>>& file_list)
+{
+    std::cout << "Grouping files by similarity..." << std::endl;  // Log output
     std::unordered_map<std::string, std::vector<std::pair<int, std::string>>> groups;
-    
-    for (const auto& [size, path] : file_list) {
+
+    for (const auto& [size, path] : file_list)
+    {
         std::string filename = path.substr(path.find_last_of("/") + 1);
         bool added = false;
-        
-        for (const auto& [key, _] : groups) {
-            if (jaccard_similarity(key, filename) > 80) {
+
+        for (const auto& [key, _] : groups)
+        {
+            if (jaccard_similarity(key, filename) > 80)
+            {
                 groups[key].push_back({size, path});
                 added = true;
                 break;
             }
         }
-        
-        if (!added) {
+
+        if (!added)
+        {
             groups[filename].push_back({size, path});
         }
     }
-    
+
     return groups;
 }
 
 // Main function
-int main(int argc, char* argv[]) {
-    if (argc != 3) {
+int main(int argc, char* argv[])
+{
+    std::cout << "Program started." << std::endl;  // Log output
+
+    if (argc != 3)
+    {
         std::cout << "Usage: ./FileGroupSorter_Jaccard <input_filename> <output_filename>" << std::endl;
         return 1;
     }
-    
+
     std::string input_filename = argv[1];
     std::string output_filename = argv[2];
-    
+
+    std::cout << "Reading from input file: " << input_filename << std::endl;  // Log output
+
     std::ifstream infile(input_filename);
-    std::ofstream outfile(output_filename);
-    
-    std::vector<std::pair<int, std::string>> file_list;
-    int size;
-    std::string path;
-    
-    while (infile >> size >> path) {
-        file_list.push_back({size, path});
+    if (!infile.is_open())
+    {
+        std::cerr << "Could not open input file." << std::endl;
+        return 1;
     }
-    
+
+    std::ofstream outfile(output_filename);
+    if (!outfile.is_open())
+    {
+        std::cerr << "Could not open output file." << std::endl;
+        return 1;
+    }
+
+    std::cout << "Reading file content..." << std::endl;  // Log output
+
+    std::vector<std::pair<int, std::string>> file_list;
+    std::string line, size_str, path_str;
+    long long int size;  // Use long long int for large file sizes
+
+    // New file reading logic
+    while (getline(infile, line))
+    {
+        std::stringstream ss(line);
+        getline(ss, size_str, ',');
+        getline(ss, path_str, ',');
+        path_str = path_str.substr(1, path_str.length() - 2);  // Remove the double quotes
+        size = std::stoll(size_str);  // Convert string to long long int
+        file_list.push_back({size, path_str});
+    }
+
+    if (file_list.empty())
+    {
+        std::cerr << "No data read from input file. Exiting." << std::endl;
+        return 1;
+    }
+
     auto groups = group_files_by_similarity(file_list);
 
     // Sort files within each group by size
-    for (auto& [key, group] : groups) {
-        std::sort(group.begin(), group.end(), [](const auto& a, const auto& b) {
+    for (auto& [key, group] : groups)
+    {
+        std::sort(group.begin(), group.end(), [](const std::pair<int, std::string>& a, const std::pair<int, std::string>& b)
+        {
             return a.first > b.first;
         });
     }
 
     // Sort groups by the largest file in each group
     std::vector<std::vector<std::pair<int, std::string>>> sorted_groups;
-    for (const auto& [_, group] : groups) {
+    for (const auto& [_, group] : groups)
+    {
         sorted_groups.push_back(group);
     }
 
-    std::sort(sorted_groups.begin(), sorted_groups.end(), [](const auto& a, const auto& b) {
+    std::sort(sorted_groups.begin(), sorted_groups.end(), [](const auto & a, const auto & b)
+    {
         return a[0].first > b[0].first;
     });
 
     // Write to output file
-    for (const auto& group : sorted_groups) {
-        for (const auto& [size, path] : group) {
+    for (const auto& group : sorted_groups)
+    {
+        for (const auto& [size, path] : group)
+        {
             outfile << size << " " << path << std::endl;
         }
         outfile << std::endl;  // Add an empty line to separate groups
     }
+
+    std::cout << "Program finished successfully." << std::endl;  // Log output
 
     return 0;
 }
