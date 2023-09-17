b8c817a411a35bf164c7fe14f8e1133330bb813c
Ying Huang <huangyingw@gmail.com>
2023-09-16 21:51:59 -0700

    n

--- jaccard_file_sorter/file_group_sorter_jaccard.cpp
+++ jaccard_file_sorter/file_group_sorter_jaccard.cpp
@@ -13,6 +13,16 @@
 #include <iomanip>
 #include <spdlog/spdlog.h>  // Include spdlog header
 #include <spdlog/sinks/basic_file_sink.h>  // Include file sink header
+#include <leveldb/cache.h>  // Include LevelDB cache header
+#include <atomic>  // Include atomic header
+
+// Initialize LevelDB LRU Cache
+leveldb::Cache* cache = leveldb::NewLRUCache(1000000000);  // 1GB cache
+
+// 初始化LevelDB LRU缓存
+leveldb::Cache* groups_cache = leveldb::NewLRUCache(1000000000);  // 1GB cache for groups
+
+std::unordered_set<std::string> cache_keys;  // to store all keys inserted into the cache
 
 // 在main函数或程序初始化部分
 void setup_logger(const std::string& output_filename)
@@ -39,13 +49,11 @@ void setup_logger(const std::string& output_filename)
     }
 }
 
-std::mutex jaccard_cache_mutex; // Mutex for jaccard_cache
-std::mutex groups_mutex;  // Mutex for groups
 std::mutex progress_mutex;  // Mutex for progress
 
 // Progress variables
 int total_files = 0;
-int processed_files = 0;
+std::atomic<int> processed_files(0);  // Make processed_files atomic
 
 // Define a custom hash for std::pair<std::string, std::string>
 struct pair_hash
@@ -67,59 +75,63 @@ std::unordered_map<std::pair<std::string, std::string>, double, pair_hash> jacca
 // Function to calculate Jaccard similarity between two strings
 double jaccard_similarity(const std::string& a, const std::string& b)
 {
-    std::lock_guard<std::mutex> lock(jaccard_cache_mutex); // Lock the mutex
+    // Check if result is in cache
+    // Generate a cache key
+    std::string cache_key = a + "_" + b;
+
+    // Try to find the value in the cache
+    leveldb::Cache::Handle* handle = cache->Lookup(cache_key);
+    if (handle != nullptr)
     {
-        // Check if result is in cache
-        auto it = jaccard_cache.find({a, b});
-        if (it != jaccard_cache.end())
-        {
-            return it->second;
-        }
-
-        std::cout << "Calculating Jaccard similarity between: " << a << " and " << b << std::endl;  // Log output
-        std::unordered_set<char> setA(a.begin(), a.end());
-        std::unordered_set<char> setB(b.begin(), b.end());
-
-        std::unordered_set<char> intersection;
-        std::unordered_set<char> unionSet;
-
-        for (char c : setA)
-        {
-            if (setB.find(c) != setB.end())
-            {
-                intersection.insert(c);
-            }
-            unionSet.insert(c);
-        }
-
-        for (char c : setB)
-        {
-            unionSet.insert(c);
-        }
-
-        double result = static_cast<double>(intersection.size()) / unionSet.size() * 100;
-
-        // Store result in cache
-        jaccard_cache[ {a, b}] = result;
-        jaccard_cache[ {b, a}] = result; // Jaccard similarity is commutative
-
-        return result;
+        double* cached_value = reinterpret_cast<double*>(cache->Value(handle));
+        cache->Release(handle);
+        return *cached_value;
     }
+
+    std::cout << "Calculating Jaccard similarity between: " << a << " and " << b << std::endl;  // Log output
+    std::unordered_set<char> setA(a.begin(), a.end());
+    std::unordered_set<char> setB(b.begin(), b.end());
+
+    std::unordered_set<char> intersection;
+    std::unordered_set<char> unionSet;
+
+    for (char c : setA)
+    {
+        if (setB.find(c) != setB.end())
+        {
+            intersection.insert(c);
+        }
+        unionSet.insert(c);
+    }
+
+    for (char c : setB)
+    {
+        unionSet.insert(c);
+    }
+
+    double result = static_cast<double>(intersection.size()) / unionSet.size() * 100;
+
+    // Store the result in the cache
+    double* heap_value = new double(result);
+    cache->Insert(cache_key, heap_value, sizeof(double), [](const leveldb::Slice & key, void* value)
+    {
+        delete reinterpret_cast<double*>(value);
+    });
+
+    return result;
 }
 
 // Function to update and print progress
 void update_progress()
 {
-    std::lock_guard<std::mutex> lock(progress_mutex);
-    ++processed_files;
-    double progress = (static_cast<double>(processed_files) / total_files) * 100;
+    ++processed_files;  // Atomic operation
+    double progress = (static_cast<double>(processed_files.load()) / total_files) * 100;  // Atomic read
     auto logger = spdlog::get("file_logger");  // Get logger
     logger->info("Progress: {:.2f}%", progress);  // Use file_logger for logging
 }
 
 // Function to group files by similarity
 void group_files_by_similarity_threaded(const std::vector<std::pair<long long int, std::string>>& file_list,
-                                        std::unordered_map<std::string, std::vector<std::pair<long long int, std::string>>>& groups,
                                         int start, int end)
 {
     spdlog::info("Grouping files by similarity...");  // Use spdlog for logging
@@ -129,20 +141,33 @@ void group_files_by_similarity_threaded(const std::vector<std::pair<long long in
         std::string filename = path.substr(path.find_last_of("/") + 1);
         bool added = false;
 
-        std::lock_guard<std::mutex> lock(groups_mutex); // Lock the mutex for groups
-        for (const auto& [key, _] : groups)
+        // Try to find a similar key in the cache
+        for (const auto& key : cache_keys)
         {
             if (jaccard_similarity(key, filename) > 90)
             {
-                groups[key].push_back({size, path});
-                added = true;
-                break;
+                leveldb::Cache::Handle* handle = groups_cache->Lookup(key);
+                if (handle != nullptr)
+                {
+                    auto* similar_group = reinterpret_cast<std::vector<std::pair<long long int, std::string>>*>(groups_cache->Value(handle));
+                    similar_group->push_back({size, path});
+                    groups_cache->Release(handle);
+                    added = true;
+                    break;
+                }
             }
         }
 
         if (!added)
         {
-            groups[filename].push_back({size, path});
+            // Create a new group if not found in cache
+            auto* new_group = new std::vector<std::pair<long long int, std::string>>();
+            new_group->push_back({size, path});
+            groups_cache->Insert(filename, new_group, sizeof(new_group), [](const leveldb::Slice & key, void* value)
+            {
+                delete reinterpret_cast<std::vector<std::pair<long long int, std::string>>*>(value);
+            });
+            cache_keys.insert(filename);  // Add the new key to our set of keys
         }
         update_progress();
     }
@@ -186,6 +211,7 @@ int main(int argc, char* argv[])
     spdlog::info("Reading file content...");  // Use spdlog for logging
 
     std::vector<std::pair<long long int, std::string>> file_list;
+    file_list.reserve(20000);  // 预分配内存
     std::string line, size_str, path_str;
     long long int size;  // Use long long int for large file sizes
 
@@ -207,6 +233,7 @@ int main(int argc, char* argv[])
     }
 
     std::unordered_map<std::string, std::vector<std::pair<long long int, std::string>>> groups;
+    groups.reserve(20000);  // 预分配内存
 
     // Get the number of hardware threads available on the system
     int num_threads = static_cast<int>(std::thread::hardware_concurrency());
@@ -227,13 +254,17 @@ int main(int argc, char* argv[])
     {
         int start = i * chunk_size;
         int end = (i == num_threads - 1) ? file_list.size() : start + chunk_size;
-        threads.push_back(std::thread(group_files_by_similarity_threaded, std::ref(file_list), std::ref(groups), start, end));
+        threads.push_back(std::thread(group_files_by_similarity_threaded, std::ref(file_list), start, end));
     }
 
     for (auto& t : threads)
     {
         t.join();
     }
+
+    // 释放内存
+    file_list.clear();
+    std::vector<std::pair<long long int, std::string>>().swap(file_list);
 
     // Sort groups by the largest file in each group
     std::vector<std::vector<std::pair<long long int, std::string>>> sorted_groups;
@@ -242,6 +273,10 @@ int main(int argc, char* argv[])
     {
         sorted_groups.push_back(group);
     }
+
+    groups.clear();
+    std::unordered_map<std::string, std::vector<std::pair<long long int, std::string>>>().swap(groups);
+
 
     std::sort(sorted_groups.begin(), sorted_groups.end(), [](const auto & a, const auto & b)
     {
@@ -259,6 +294,12 @@ int main(int argc, char* argv[])
     }
 
     logger->info("Program finished successfully.");  // Use spdlog for logging
+
+    // Release LevelDB LRU Cache
+    delete cache;
+
+    // Release LevelDB LRU Cache
+    delete groups_cache;
 
     return 0;
 }
