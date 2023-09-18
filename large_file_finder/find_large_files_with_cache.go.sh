fd960ee5d20717cc5003bb52ec36bda6d97641d2
Ying Huang <huangyingw@gmail.com>
2023-09-18 16:01:51 -0700

    n

--- large_file_finder/find_large_files_with_cache.go.sh
+++ large_file_finder/find_large_files_with_cache.go.sh
@@ -3,5 +3,5 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-go build find_large_files_with_cache.go
-./find_large_files_with_cache
+go build find_large_files_with_cache.go && \
+    ./find_large_files_with_cache /media/115_disk/av/kyler/
