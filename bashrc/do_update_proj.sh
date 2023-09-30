9c8d35e01456455f2ad8cd2339f0378b97bd822a
Ying Huang <huangyingw@gmail.com>
2023-09-29 23:40:52 -0700

    n

--- bashrc/do_update_proj.sh
+++ bashrc/do_update_proj.sh
@@ -4,5 +4,4 @@ parallel --jobs 10 -k ::: \
     ~/loadrc/bashrc/generate_file_index.sh \
     ~/loadrc/bashrc/generate_rsync_files.sh \
     ~/loadrc/bashrc/cscope.sh \
-    ~/loadrc/bashrc/find_large_files_with_exclusions.sh \
     'git remote update'
