dfec580701f766bf26b455d70373901cd3b6b5e3
Ying Huang <huangyingw@gmail.com>
2023-12-02 21:43:12 -0800

    n

--- large_file_finder/find_large_files_with_cache.go.sh
+++ large_file_finder/find_large_files_with_cache.go.sh
@@ -6,7 +6,13 @@ cd "$SCRIPTPATH"
 go mod init github.com/huangyingw/FileSorter
 go get -u github.com/allegro/bigcache
 go get -u github.com/go-redis/redis/v8
-docker run --name my-redis -d -p 6379:6379 redis
+go get -u github.com/mattn/go-zglob/fastwalk
+go get -u github.com/karrick/godirwalk
+
+docker-compose down -v && docker-compose up -d
+
+#go build find_large_files_with_cache.go && \
+#    sudo ./find_large_files_with_cache /media/usb_backup_crypt_5T_1
 
 go build find_large_files_with_cache.go && \
-    ./find_large_files_with_cache /media/115_disk/av/kyler/
+    sudo ./find_large_files_with_cache /media
