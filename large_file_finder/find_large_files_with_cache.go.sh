0c4c1d92ed92690bea95f12b78ca4c5fa96d810f
Ying Huang <huangyingw@gmail.com>
2023-12-02 22:17:12 -0800

    n

--- large_file_finder/find_large_files_with_cache.go.sh
+++ large_file_finder/find_large_files_with_cache.go.sh
@@ -9,7 +9,8 @@ go get -u github.com/go-redis/redis/v8
 go get -u github.com/mattn/go-zglob/fastwalk
 go get -u github.com/karrick/godirwalk
 
-docker-compose down -v && docker-compose up -d
+#docker-compose down -v && docker-compose up -d
+docker-compose restart
 
 go build find_large_files_with_cache.go && \
     sudo ./find_large_files_with_cache /media
