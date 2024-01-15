cba5fae089cd6781e69305f73a9ce806a96b056a
Ying Huang <huangyingw@gmail.com>
2024-01-14 16:54:04 -0800

    n

--- macosrc/restart_ssh.sh
+++ macosrc/restart_ssh.sh
@@ -1,6 +1,4 @@
 #!/bin/zsh
 
-sudo launchctl unload /System/Library/LaunchDaemons/ssh.plist
-sudo launchctl load -w /System/Library/LaunchDaemons/ssh.plist
 sudo launchctl stop com.openssh.sshd
 sudo launchctl start com.openssh.sshd
