aa0638f01e98e8efb117dd8bc73f3f328005f9e3
Ying Huang <huangyingw@gmail.com>
2024-01-25 12:43:00 -0800

    n

--- zshrc/capture_chrome_window_screenshot.zsh
+++ zshrc/capture_chrome_window_screenshot.zsh
@@ -1,7 +1,10 @@
 # 使用osascript运行AppleScript并捕获输出
-read x y width height <<< $(osascript -e 'tell application "Google Chrome" to get bounds of first window whose title contains "daily"')
+window_bounds=$(osascript -e 'tell application "Google Chrome" to get bounds of first window whose title contains "daily"')
 
-# 计算窗口的宽度和高度
+# 将输出分解为单独的变量
+read -r x y width height <<< $(echo $window_bounds | tr -d ',')
+
+# 计算窗口的实际宽度和高度
 width=$(($width - $x))
 height=$(($height - $y))
 
