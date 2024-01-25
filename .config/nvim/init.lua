c1e47e31c17d970029a9951c67443b760df8434d
Ying Huang <huangyingw@gmail.com>
2024-01-25 12:36:33 -0800

    n

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,11 @@
 -- init.lua
 vim.cmd 'set runtimepath^=~/.vim runtimepath+=~/.vim/after'
 vim.cmd 'let &packpath = &runtimepath'
+vim.cmd('colorscheme vim')
+
 vim.cmd 'source ~/.vimrc'
 require('plugins')
-require('secret_config')
+-- require('secret_config')
 
 require("chatgpt").setup()
 
@@ -23,4 +25,4 @@ wk.register({
 }, {
     prefix = "<leader>",
     mode = "v",
-})
+})                    
