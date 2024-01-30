48101576c1cd91aab6e2affae7115a747582a8b0
Ying Huang <huangyingw@gmail.com>
2024-01-29 21:48:10 -0800

    n

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,17 @@
 -- init.lua
 vim.cmd 'set runtimepath^=~/.vim runtimepath+=~/.vim/after'
 vim.cmd 'let &packpath = &runtimepath'
+
+-- 禁用终端的 GUI 颜色支持
+vim.o.termguicolors = false
+
+-- 设置当应用特定颜色方案时改变状态行的颜色
+vim.api.nvim_create_autocmd("ColorScheme", {
+    pattern = "vim",
+    command = "hi StatusLine ctermbg=blue ctermfg=yellow cterm=NONE"
+})
+
+-- 设置当前的颜色方案为 'vim'
 vim.cmd('colorscheme vim')
 
 vim.cmd 'source ~/.vimrc'
