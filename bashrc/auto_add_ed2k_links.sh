29f7949257a41a965828429805d7b677b306f964
Ying Huang <huangyingw@gmail.com>
2024-02-20 13:46:58 -0800

    n

--- bashrc/auto_add_ed2k_links.sh
+++ bashrc/auto_add_ed2k_links.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/bin/zsh
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd "$SCRIPTPATH"
 
 # 直接指定ED2K链接文件的路径
 FILE="ed2k_links.txt"
@@ -12,7 +15,7 @@ fi
 # 读取文件中的每一行
 while IFS= read -r line; do
     # 使用amulecmd添加ED2K链接
-    echo -e "Add $line\nexit" | amulecmd -P your_password
+    echo -e "Add $line\nexit" | amulecmd -P amulecmd
 done < "$FILE"
 
 echo "所有链接已添加到aMule。"
