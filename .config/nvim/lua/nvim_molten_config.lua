229823c678d41be735782da1d63eb7710870814a
Ying Huang <huangyingw@gmail.com>
2024-03-27 13:43:58 -0700

    n

---
+++ .config/nvim/lua/nvim_molten_config.lua
@@ -0,0 +1,6 @@
+vim.g.molten_auto_open_output = false
+vim.g.molten_auto_image_popup = true
+vim.g.molten_image_provider = "image.nvim"
+vim.g.molten_wrap_output = true
+vim.g.molten_virt_text_output = true
+vim.g.molten_virt_lines_off_by_1 = true
